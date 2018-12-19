147139b4e66351b4904c6856ac5eb943a7e6ebd6
WLM1ke <wlmike@gmail.com>
2018-12-19 12:51:32 +0300

    Исправлены тесты

--- poptimizer/finder/tests/test_momentum.py
+++ poptimizer/finder/tests/test_momentum.py
@@ -11,9 +11,9 @@ def test_find_momentum(capsys):
     find_momentum(port, 0.02)
     captured = capsys.readouterr()
     assert "Выведены 2% - 5 акций" in captured.out
-    assert "TATN   0.509073  0.244496  1.000000  2.082128     " in captured.out
-    assert "BANEP  0.412600  0.205033  1.000000  2.012363  ADD" in captured.out
-    assert "NVTK   0.536663  0.275343  1.000000  1.949073  ADD" in captured.out
-    assert "KZOS   0.385195  0.199113  0.999934  1.934423     " in captured.out
-    assert "SIBN   0.418839  0.240944  1.000000  1.738324  ADD" in captured.out
+    assert "TATN   0.509073  0.244496       1.0  2.082128     " in captured.out
+    assert "BANEP  0.412600  0.205033       1.0  2.012363  ADD" in captured.out
+    assert "NVTK   0.536663  0.275343       1.0  1.949073  ADD" in captured.out
+    assert "KZOS   0.385195  0.199113       1.0  1.934551     " in captured.out
+    assert "SIBN   0.418839  0.240944       1.0  1.738324  ADD" in captured.out
     assert "LKOH" not in captured.out
