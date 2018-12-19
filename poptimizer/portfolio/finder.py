50403eeb08835e19eb77093b9de750ab1b1be2d8
WLM1ke <wlmike@gmail.com>
2018-12-19 15:05:43 +0300

    Тесты для дивидендных бумаг

--- poptimizer/portfolio/finder.py
+++ poptimizer/portfolio/finder.py
@@ -4,6 +4,7 @@ import pandas as pd
 from poptimizer import ml, data, portfolio
 from poptimizer.config import ML_PARAMS
 from poptimizer.ml import feature
+from poptimizer.portfolio.portfolio import CASH
 
 
 def feature_days(feat):
@@ -12,11 +13,16 @@ def feature_days(feat):
     return ML_PARAMS[0][loc][1]["days"]
 
 
-def get_turnover(tickers, date):
+def get_turnover(port, tickers):
     """Получает фактор оборота для тикеров."""
-    positions = {ticker: 1 for ticker in tickers}
-    all_tickers_port = portfolio.Portfolio(date, 0, positions)
-    return all_tickers_port.turnover_factor
+    date = port.date
+    cash = port.shares[CASH]
+    positions = {ticker: port.shares[ticker] for ticker in port.index[:-2]}
+    for ticker in tickers:
+        if ticker not in positions:
+            positions[ticker] = 0
+    all_tickers_port = portfolio.Portfolio(date, cash, positions)
+    return all_tickers_port.turnover_factor[:-2]
 
 
 def mark_not_portfolio(df, column_name, choose, current_port):
@@ -30,7 +36,7 @@ def mark_not_portfolio(df, column_name, choose, current_port):
     return df
 
 
-def find_momentum(current_port: portfolio.Portfolio, part: float = 0.1):
+def find_momentum(current_port: portfolio.Portfolio, part: float = 0.1) -> pd.DataFrame:
     """Поиск бумаг с устойчивым ростом и низкой волатильностью.
 
     Печатает, какие из входящих в лучшую часть бумаг не содержатся в текущем портфеле и могут быть
@@ -40,6 +46,8 @@ def find_momentum(current_port: portfolio.Portfolio, part: float = 0.1):
         Текущий портфель.
     :param part:
         Доля всех котирующихся бумаг, которые должны быть выведены.
+    :return:
+        Сводная информация по лучшим акциям.
     """
     all_tickers = data.securities_with_reg_number()
     mean_days = feature_days(feature.Mean)
@@ -49,7 +57,7 @@ def find_momentum(current_port: portfolio.Portfolio, part: float = 0.1):
     std = feature.STD(tuple(all_tickers), date).get(date, days=mean_days)
     std *= mean_days ** 0.5
     df = pd.concat([mean, std], axis=1)
-    df["TURNOVER"] = get_turnover(all_tickers, date)
+    df["TURNOVER"] = get_turnover(current_port, all_tickers)
     df["T_SCORE"] = df.iloc[:, 0] / df.iloc[:, 1] * df.iloc[:, 2]
     choose = int(part * all_tickers.size)
     df = mark_not_portfolio(df, "T_SCORE", choose, current_port)
@@ -62,9 +70,12 @@ def find_momentum(current_port: portfolio.Portfolio, part: float = 0.1):
         f"\n{df}"
     )
     print(text)
+    return df
 
 
-def find_dividends(current_port: portfolio.Portfolio, part: float = 0.1):
+def find_dividends(
+    current_port: portfolio.Portfolio, part: float = 0.1
+) -> pd.DataFrame:
     """Поиск бумаг с максимальными дивидендами.
 
     Печатает, какие из входящих в лучшую часть бумаг не содержатся в текущем портфеле и могут быть
@@ -74,17 +85,18 @@ def find_dividends(current_port: portfolio.Portfolio, part: float = 0.1):
         Текущий портфель.
     :param part:
         Доля всех котирующихся бумаг, которые должны быть выведены.
+    :return:
+        Сводная информация по лучшим акциям.
     """
     all_tickers = data.securities_with_reg_number()
     div_days = feature_days(feature.Dividends)
     date = current_port.date
     div = feature.Dividends(tuple(all_tickers), date).get(date, days=div_days)
     div = div.to_frame()
-    div["TURNOVER"] = get_turnover(all_tickers, date)
-    div["SCORE"] = div.iloc[:, 0] / div.iloc[:, 1]
+    div["TURNOVER"] = get_turnover(current_port, all_tickers)
+    div["SCORE"] = div.iloc[:, 0] * div.iloc[:, 1]
     choose = int(part * all_tickers.size)
     div = mark_not_portfolio(div, "SCORE", choose, current_port)
-
     text = (
         f"\nПОИСК ДИВИДЕНДНЫХ ТИКЕРОВ"
         f"\n"
@@ -94,3 +106,4 @@ def find_dividends(current_port: portfolio.Portfolio, part: float = 0.1):
         f"\n{div}"
     )
     print(text)
+    return div
--- poptimizer/portfolio/tests/test_finder.py
+++ poptimizer/portfolio/tests/test_finder.py
@@ -1,19 +1,49 @@
 import pandas as pd
+import pytest
 
 from poptimizer import portfolio
-from poptimizer.portfolio.finder import find_momentum
+from poptimizer.ml import feature
+from poptimizer.portfolio import finder
+
+
+def test_feature_days():
+    assert finder.feature_days(feature.Label) == 21
+
+
+def test_get_turnover():
+    date = pd.Timestamp("2018-12-18")
+    positions = dict(TATN=20000, KZOS=20000, LKOH=20000)
+    port = portfolio.Portfolio(date, 0, positions)
+    df = finder.get_turnover(port, ("KZOS", "AKRN"))
+    assert isinstance(df, pd.Series)
+    assert df.size == 4
+    assert df["KZOS"] == pytest.approx(0.986873)
 
 
 def test_find_momentum(capsys):
     date = pd.Timestamp("2018-12-18")
-    positions = dict(TATN=1, KZOS=1, LKOH=1)
+    positions = dict(TATN=20000, KZOS=20000, LKOH=20000)
     port = portfolio.Portfolio(date, 0, positions)
-    find_momentum(port, 0.02)
+    finder.find_momentum(port, 0.02)
     captured = capsys.readouterr()
     assert "Выведены 2% - 5 акций" in captured.out
-    assert "TATN   0.509073  0.244496       1.0  2.082128     " in captured.out
-    assert "BANEP  0.412600  0.205033       1.0  2.012363  ADD" in captured.out
-    assert "NVTK   0.536663  0.275343       1.0  1.949073  ADD" in captured.out
-    assert "KZOS   0.385195  0.199113       1.0  1.934551     " in captured.out
-    assert "SIBN   0.418839  0.240944       1.0  1.738324  ADD" in captured.out
+    assert "TATN   0.509073  0.244496  1.000000  2.082128     " in captured.out
+    assert "BANEP  0.412600  0.205033  0.999980  2.012322  ADD" in captured.out
+    assert "NVTK   0.536663  0.275343  1.000000  1.949073  ADD" in captured.out
+    assert "KZOS   0.385195  0.199113  0.986873  1.909157     " in captured.out
+    assert "SIBN   0.418839  0.240944  0.999999  1.738323  ADD" in captured.out
     assert "LKOH" not in captured.out
+
+
+def test_find_dividends(capsys):
+    date = pd.Timestamp("2018-12-18")
+    positions = dict(CHMF=20000, TATN=20000, KZOS=20000, LKOH=20000)
+    port = portfolio.Portfolio(date, 0, positions)
+    df = finder.find_dividends(port, 0.02)
+    captured = capsys.readouterr()
+    assert "CHMF    0.142110  1.000000  0.142110     " in captured.out
+    assert "MTLRP   0.142030  0.999910  0.142018  ADD" in captured.out
+    assert isinstance(df, pd.DataFrame)
+    assert df.shape == (5, 4)
+    assert list(df.columns) == ["Dividends", "TURNOVER", "SCORE", "ADD"]
+    assert list(df.index) == ["CHMF", "MTLRP", "LSNGP", "ENRU", "TATNP"]
