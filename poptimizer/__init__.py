f4d3c598cac17edcb04ad26347a64490b77897a2
WLM1ke <wlmike@gmail.com>
2018-12-23 11:10:18 +0300

    Тесты для отчета

--- poptimizer/__init__.py
+++ poptimizer/__init__.py
@@ -1,6 +1,6 @@
 """Оптимизация долгосрочного портфеля акций."""
 from poptimizer.ml import find_better_model
 from poptimizer.portfolio import *
-from poptimizer.reporter import income_report
+from poptimizer.reports import income
 
 __version__ = "0.1.0"
--- poptimizer/reporter/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Подготовка отчетов по фактической доходности портфеля."""
-from poptimizer.reporter.income import income_report
