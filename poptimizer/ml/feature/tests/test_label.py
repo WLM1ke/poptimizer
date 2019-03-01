4032b5a7028aba79f86f8e58401109d9b749c6e5
WLM1ke <wlmike@gmail.com>
2019-03-01 16:42:22 +0300

    Обновление тестов для векторных меток

--- poptimizer/ml/feature/tests/test_label.py
+++ poptimizer/ml/feature/tests/test_label.py
@@ -2,13 +2,15 @@ import pandas as pd
 import pytest
 from hyperopt.pyll import Apply
 
-from poptimizer.ml.feature import label_old, YEAR_IN_TRADING_DAYS
+from poptimizer.ml.feature import label, YEAR_IN_TRADING_DAYS
 
 
+# noinspection PyUnresolvedReferences
 @pytest.fixture(scope="module", name="feat")
 def make_feature():
-    # noinspection PyTypeChecker
-    return label_old.Label(("AKRN", "SNGSP", "MSTT"), pd.Timestamp("2018-12-11"))
+    return label.Label(
+        ("AKRN", "SNGSP", "MSTT"), pd.Timestamp("2018-12-11"), {"days": 21}
+    )
 
 
 def test_is_categorical(feat):
@@ -22,11 +24,14 @@ def test_get_params_space(feat):
     assert isinstance(space["days"], Apply)
 
 
+# noinspection PyUnresolvedReferences
 def test_get(feat):
-    df = feat.get(pd.Timestamp("2018-11-12"), days=21)
+    df = feat.get()
     assert isinstance(df, pd.Series)
-    assert df.size == 3
-    assert df.at["SNGSP"] == pytest.approx(0.0995856337763434 / YEAR_IN_TRADING_DAYS)
+    assert df[(pd.Timestamp("2018-11-12"), "SNGSP")] == pytest.approx(
+        0.0995856337763434 / YEAR_IN_TRADING_DAYS
+    )
 
-    df = feat.get(pd.Timestamp("2018-05-17"), days=21)
-    assert df.at["AKRN"] == pytest.approx(0.114643927228733 / YEAR_IN_TRADING_DAYS)
+    assert df[(pd.Timestamp("2018-05-17"), "AKRN")] == pytest.approx(
+        0.114643927228733 / YEAR_IN_TRADING_DAYS
+    )
