e9de690bc0d71503b735ca799d5967004c630370
WLM1ke <wlmike@gmail.com>
2019-03-01 16:34:37 +0300

    Обновление тестов для векторного признака дивидендов

--- poptimizer/ml/feature/tests/test_divyield.py
+++ poptimizer/ml/feature/tests/test_divyield.py
@@ -3,12 +3,14 @@ import pytest
 from hyperopt.pyll import Apply
 
 from poptimizer.config import AFTER_TAX
-from poptimizer.ml.feature import divyield_old
+from poptimizer.ml.feature import divyield
 
 
 @pytest.fixture(scope="module", name="feat")
 def test_divyield_feature():
-    return divyield_old.DivYield(("PHOR", "TATN", "DSKY"), pd.Timestamp("2018-12-12"))
+    return divyield.DivYield(
+        ("PHOR", "TATN", "DSKY"), pd.Timestamp("2018-12-12"), {"days": 47}
+    )
 
 
 def test_is_categorical(feat):
@@ -22,51 +24,27 @@ def test_get_params_space(feat):
     assert isinstance(space["days"], Apply)
 
 
-def test_check_bounds_middle(feat, capsys):
-    lower, upper = divyield_old.DivYield.RANGE
-    feat.check_bounds(days=(lower + upper) / 2)
-    captured = capsys.readouterr()
-    assert captured.out == ""
-
-
-def test_check_bounds_lower(feat, capsys):
-    lower, upper = divyield_old.DivYield.RANGE
-    feat.check_bounds(days=int(lower * 1.05))
-    captured = capsys.readouterr()
-    assert "Необходимо расширить" in captured.out
-    assert str(upper) in captured.out
-
-
-def test_check_bounds_upper(feat, capsys):
-    lower, upper = divyield_old.DivYield.RANGE
-    feat.check_bounds(days=int(upper / 1.05))
-    captured = capsys.readouterr()
-    assert "Необходимо расширить" in captured.out
-    assert str(lower) in captured.out
-
-
 def test_get(feat):
-    df = feat.get(pd.Timestamp("2010-01-26"), days=13)
-
+    df = feat.get({"days": 13})
     assert isinstance(df, pd.Series)
-    assert df.size == 3
-    assert df.isna().all()
-    assert df.name == "DivYield"
 
-    df = feat.get(pd.Timestamp("2010-01-26"), days=12)
-    assert not df.isna().all()
+    assert pd.Timestamp("2018-12-12") in df.index
+    assert pd.Timestamp("2018-12-13") not in df.index
+    assert pd.Timestamp("2010-01-26") not in df.index
 
-    df = feat.get(pd.Timestamp("2018-06-25"), days=9)
-    assert df["PHOR"] == pytest.approx(AFTER_TAX * 15 / 2303)
+    df = feat.get(dict(days=9))
+    assert df[(pd.Timestamp("2018-06-22"), "PHOR")] == pytest.approx(
+        AFTER_TAX * 15 / 2291
+    )
+    assert df[(pd.Timestamp("2018-06-25"), "PHOR")] == pytest.approx(0)
 
-    df = feat.get(pd.Timestamp("2018-06-26"), days=9)
-    assert df["PHOR"] == pytest.approx(0)
+    df = feat.get(dict(days=20))
+    assert df[(pd.Timestamp("2018-06-11"), "PHOR")] == pytest.approx(
+        AFTER_TAX * 15 / 2315
+    )
 
-    df = feat.get(pd.Timestamp("2018-06-13"), days=20)
-    assert df["PHOR"] == pytest.approx(AFTER_TAX * 15 / 2322)
-
-    df = feat.get(pd.Timestamp("2018-10-12"), days=30)
-    assert df["TATN"] == pytest.approx(30.27 * AFTER_TAX / 790)
-
-    df = feat.get(pd.Timestamp("2018-10-11"), days=30)
-    assert df["TATN"] == pytest.approx(0)
+    df = feat.get(dict(days=30))
+    assert df[(pd.Timestamp("2018-10-11"), "TATN")] == pytest.approx(
+        30.27 * AFTER_TAX / 778.3
+    )
+    assert df[(pd.Timestamp("2018-10-10"), "TATN")] == pytest.approx(0)
