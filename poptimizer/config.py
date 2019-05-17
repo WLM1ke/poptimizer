faffec956765351172ad1d3b9e93e836d4caf91e
WLM1ke <wlmike@gmail.com>
2019-05-17 15:53:41 +0300

    Обновлены данные по дивидендам

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -36,26 +36,26 @@ MAX_TRADE = 0.015
 TURNOVER_PERIOD = 21 * 2
 
 # Минимальный оборот - преимущества акции снижаются при приближении медианного оборота к данному уровню
-TURNOVER_CUT_OFF = 4.1 * MAX_TRADE
+TURNOVER_CUT_OFF = 4.0 * MAX_TRADE
 
 # Параметры ML-модели
 ML_PARAMS = {
     "data": (
-        ("Label", {"days": 41, "on_off": True}),
-        ("STD", {"days": 25, "on_off": True}),
+        ("Label", {"days": 44, "on_off": True}),
+        ("STD", {"days": 26, "on_off": True}),
         ("Ticker", {"on_off": True}),
-        ("Mom12m", {"days": 269, "on_off": True}),
-        ("DivYield", {"days": 278, "on_off": True, "periods": 1}),
-        ("Mom1m", {"days": 26, "on_off": True}),
-        ("RetMax", {"days": 39, "on_off": True}),
+        ("Mom12m", {"days": 255, "on_off": True}),
+        ("DivYield", {"days": 304, "on_off": True, "periods": 1}),
+        ("Mom1m", {"days": 29, "on_off": True}),
+        ("RetMax", {"days": 40, "on_off": True}),
         ("ChMom6m", {"days": 114, "on_off": True}),
     ),
     "model": {
-        "bagging_temperature": 0.6544649873900201,
+        "bagging_temperature": 1.0517537606447385,
         "depth": 9,
-        "l2_leaf_reg": 2.0554933787566427,
-        "learning_rate": 0.0038270934866279915,
+        "l2_leaf_reg": 3.470705441531027,
+        "learning_rate": 0.006413498364479064,
         "one_hot_max_size": 2,
-        "random_strength": 1.0150904877556164,
+        "random_strength": 1.4033180342553484,
     },
 }
