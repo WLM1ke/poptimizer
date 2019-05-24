2b11ce3867ad2e7be1b314535b0ea9212261fd48
WLM1ke <wlmike@gmail.com>
2019-05-24 13:43:38 +0300

    Новый вариант поиска тикеров

--- poptimizer/portfolio/finder.py
+++ poptimizer/portfolio/finder.py
@@ -2,7 +2,6 @@
 import pandas as pd
 
 from poptimizer import data, portfolio, config
-from poptimizer.ml import feature
 from poptimizer.portfolio import optimizer
 from poptimizer.portfolio.portfolio import CASH, Portfolio
 
@@ -26,10 +25,11 @@ def get_turnover(port, tickers):
     return all_tickers_port.turnover_factor[:-2]
 
 
-def mark_not_portfolio(df, column_name, choose, current_port):
+def mark_not_portfolio(df, column_name, current_port, choose=None):
     """Выбирает лучшие и помечает бумаги не в портфеле."""
     df = df.sort_values(column_name, ascending=False)
-    df = df.head(choose)
+    if choose:
+        df = df.head(choose)
     df["ADD"] = "ADD"
     for ticker in current_port.index:
         if ticker in df.index:
@@ -37,62 +37,22 @@ def mark_not_portfolio(df, column_name, choose, current_port):
     return df
 
 
-def find_momentum(current_port: Portfolio, part: float = 0.1) -> pd.DataFrame:
-    """Поиск бумаг с устойчивым ростом и низкой волатильностью.
+def find_good_volume(current_port: Portfolio) -> pd.DataFrame:
+    """Поиск бумаг с не нулевым фактором объема.
 
-    Печатает, какие из входящих в лучшую часть бумаг не содержатся в текущем портфеле и могут быть
-    в него добавлены.
+    Печатает, какие не содержатся в текущем портфеле и могут быть в него добавлены.
 
     :param current_port:
         Текущий портфель.
-    :param part:
-        Доля всех котирующихся бумаг, которые должны быть выведены.
     :return:
         Сводная информация по лучшим акциям.
     """
     all_tickers = data.securities_with_reg_number()
-    params = feature_params("Mom12m")
-    params["periods"] = 1
-    date = current_port.date
-    mean = feature.Mom12m(tuple(all_tickers), date, params).get()
-    mean *= params["days"]
-    mean = mean.loc[mean.index.get_level_values(0) == date]
-    mean.index = mean.index.get_level_values(1)
-    std = feature.STD(tuple(all_tickers), date, params).get()
-    std *= params["days"] ** 0.5
-    std = std.loc[std.index.get_level_values(0) == date]
-    std.index = std.index.get_level_values(1)
-    df = pd.concat([mean, std], axis=1, sort=True)
-    df["TURNOVER"] = get_turnover(current_port, all_tickers)
-    df["_DRAW_DOWN"] = df.iloc[:, 0] * df.iloc[:, 2] / df.iloc[:, 1] ** 2
-    choose = int(part * all_tickers.size)
-    return mark_not_portfolio(df, "_DRAW_DOWN", choose, current_port)
-
-
-def find_dividends(current_port: Portfolio, part: float = 0.1) -> pd.DataFrame:
-    """Поиск бумаг с максимальными дивидендами.
-
-    Печатает, какие из входящих в лучшую часть бумаг не содержатся в текущем портфеле и могут быть
-    в него добавлены.
-
-    :param current_port:
-        Текущий портфель.
-    :param part:
-        Доля всех котирующихся бумаг, которые должны быть выведены.
-    :return:
-        Сводная информация по лучшим акциям.
-    """
-    all_tickers = data.securities_with_reg_number()
-    params = feature_params("DivYield")
-    params["periods"] = 1
-    date = current_port.date
-    div = feature.DivYield(tuple(all_tickers), date, params).get()
-    div = div.loc[div.index.get_level_values(0) == date]
-    div.index = div.index.get_level_values(1)
-    div["TURNOVER"] = get_turnover(current_port, all_tickers)
-    div["SCORE"] = div.iloc[:, 0] * div.iloc[:, 1]
-    choose = int(part * all_tickers.size)
-    return mark_not_portfolio(div, "SCORE", choose, current_port)
+    df = get_turnover(current_port, all_tickers)
+    df = df[df > 0]
+    df.sort_values(ascending=False, inplace=True)
+    df = df.to_frame("VOLUME")
+    return mark_not_portfolio(df, "VOLUME", current_port)
 
 
 def find_zero_turnover_and_weight(current_port: Portfolio):
@@ -102,46 +62,15 @@ def find_zero_turnover_and_weight(current_port: Portfolio):
     return list(current_port.index[zero_weight & zero_turnover])
 
 
-def find_low_gradient(opt: optimizer.Optimizer):
-    """Находит бумаги с градиентом ниже лучшей продажи.
-
-    Исключаются бумаги, которые входят в состав лучших дивидендных и моментум, чтобы постоянно не
-    включать, а потом исключать их.
-    """
-    sell_ticker = opt.best_sell
-    gradient = opt.metrics.gradient
-    low_gradient = gradient.index[gradient < gradient[sell_ticker]]
-    momentum = find_momentum(opt.portfolio).index
-    dividends = find_dividends(opt.portfolio).index
-    result = filter(lambda x: x not in momentum and x not in dividends, low_gradient)
-    return list(result)
-
-
-def add_tickers(current_port: Portfolio, part: float = 0.1):
+def add_tickers(current_port: Portfolio):
     """Претенденты для добавления."""
-    momentum = find_momentum(current_port, part)
-    dividends = find_dividends(current_port, part)
-    print(
-        f"\nМОМЕНТУМ ТИКЕРЫ"
-        f"\n"
-        f"\n{momentum}"
-        f"\n"
-        f"\nДИВИДЕНДНЫЕ ТИКЕРЫ"
-        f"\n"
-        f"\n{dividends}"
-    )
+    print(f"\nМОМЕНТУМ ТИКЕРЫ\n\n{find_good_volume(current_port)}")
 
 
 def remove_tickers(opt: optimizer.Optimizer):
     """Претенденты на удаление."""
-    zero_turnover_and_weight = find_zero_turnover_and_weight(opt.portfolio)
-    low_gradient = find_low_gradient(opt)
     print(
         f"\nБУМАГИ С НУЛЕВЫМ ОБОРОТОМ И ВЕСОМ"
         f"\n"
-        f"{zero_turnover_and_weight}"
-        f"\n"
-        f"\nБУМАГИ С НИЗКИМ ГРАДИЕНТОМ"
-        f"\n"
-        f"{low_gradient}"
+        f"\n{find_zero_turnover_and_weight(opt.portfolio)}"
     )
