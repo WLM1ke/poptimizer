f75dd3a27a011bea0afa10450b91282654761d55
WLM1ke <wlmike@gmail.com>
2019-09-16 16:03:12 +0300

    Менеджер дивидендов на основе MongoDB

---
+++ poptimizer/store/dividends_new.py
@@ -0,0 +1,44 @@
+"""Менеджер данных для дивидендов."""
+from typing import Optional, Any, List, Dict
+
+import pymongo
+
+from poptimizer.store import manager_new
+from poptimizer.store.manager_new import AbstractManager
+from poptimizer.store.utils_new import DATE, DB
+
+# База и коллекция с источником данных по дивидендам
+SOURCE_DB = "source"
+
+# Наименование коллекции в источнике и основной базе
+COLLECTION = "dividends"
+
+
+class Dividends(AbstractManager):
+    """Дивиденды и время закрытия реестра для акций.
+
+    Данные создаются с нуля, так как могут быть ретроспективные исправления ошибок.
+    """
+
+    CREATE_FROM_SCRATCH = True
+
+    def __init__(self, db=DB) -> None:
+        super().__init__(collection=COLLECTION, db=db, create_from_scratch=True)
+
+    def _download(self, item: str, last_index: Optional[Any]) -> List[Dict[str, Any]]:
+        """Загружает полностью данные по дивидендам.
+
+        Загрузка осуществляется из обновляемой в ручную MongoDB базы данных по дивидендам."""
+        source = self._collection.database.client[SOURCE_DB][COLLECTION]
+        data = list(
+            source.aggregate(
+                [
+                    {"$match": {"ticker": item}},
+                    {"$project": {"_id": False, "date": True, "dividends": True}},
+                    {"$group": {"_id": "$date", item: {"$sum": "$dividends"}}},
+                    {"$sort": {"_id": pymongo.ASCENDING}},
+                ]
+            )
+        )
+        formatter = dict(_id=lambda x: (DATE, x))
+        return manager_new.data_formatter(data, formatter)
