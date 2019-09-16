e0240354469f919b1a8c43d8cc81a706b85ec69e
WLM1ke <wlmike@gmail.com>
2019-09-16 09:10:31 +0300

    Ускорен запуск/остановка клиентов/серверов MongoDB + тесты

--- poptimizer/store/mongo.py
+++ poptimizer/store/mongo.py
@@ -5,12 +5,12 @@
 import atexit
 import functools
 import logging
+import signal
 import subprocess
 
 import psutil
 import pymongo
 import requests
-from pymongo.errors import AutoReconnect, ServerSelectionTimeoutError
 from requests import adapters
 
 from poptimizer import config
@@ -20,33 +20,29 @@ HTTPS_MAX_POOL_SIZE = 20
 MAX_RETRIES = 3
 
 
+def start_mongo_server() -> psutil.Process:
+    """Запуск сервера MongoDB."""
+    for process in psutil.process_iter(attrs=["name"]):
+        if "mongod" in process.info["name"]:
+            logging.info("Локальный сервер MongoDB уже работает")
+            return process
+    logging.info("Запускается локальный сервер MongoDB")
+    mongo_server = [
+        "mongod",
+        "--dbpath",
+        config.MONGO_PATH,
+        "--directoryperdb",
+        "--bind_ip",
+        "localhost",
+    ]
+    return psutil.Popen(mongo_server, stdout=subprocess.DEVNULL)
+
+
 def start_mongo_client() -> pymongo.MongoClient:
     """Открытие клиентского соединения с MongoDB."""
     logging.info("Создается клиент MongoDB")
-    client = pymongo.MongoClient(
-        "localhost", 27017, tz_aware=False, serverSelectionTimeoutMS=3000
-    )
+    client = pymongo.MongoClient("localhost", 27017, tz_aware=False)
     return client
-
-
-def start_mongo_server(mongo_client: pymongo.MongoClient) -> psutil.Process:
-    """Запуск сервера MongoDB."""
-    try:
-        pid = mongo_client["admin"].command("serverStatus")["pid"]
-    except ServerSelectionTimeoutError:
-        logging.info("Запускается локальный сервер MongoDB")
-        mongo_server = [
-            "mongod",
-            "--dbpath",
-            config.MONGO_PATH,
-            "--directoryperdb",
-            "--bind_ip",
-            "localhost",
-        ]
-        return psutil.Popen(mongo_server, stdout=subprocess.DEVNULL)
-    else:
-        logging.info("Локальный сервер MongoDB уже работает")
-        return psutil.Process(pid)
 
 
 def start_http_session() -> requests.Session:
@@ -62,19 +58,18 @@ def start_http_session() -> requests.Session:
 
 def clean_up(mongo_process: psutil.Process) -> None:
     """Отключение сервера и закрытие соединений."""
-    try:
-        MONGO_CLIENT["admin"].command("shutdown")
-    except AutoReconnect:
-        pass
-    status = mongo_process.wait()
-    logging.info(f"Локальный сервер MongoDB остановлен со статусом {status}")
     MONGO_CLIENT.close()
     logging.info("Подключение клиента MongoDB закрыто")
+
+    mongo_process.send_signal(signal.SIGTERM)
+    status = mongo_process.wait()
+    logging.info(f"Локальный сервер MongoDB остановлен со статусом {status}")
+
     HTTP_SESSION.close()
     logging.info("Сессия для обновления данных по интернет закрыта")
 
 
+MONGO_PROCESS = start_mongo_server()
 MONGO_CLIENT = start_mongo_client()
-MONGO_PROCESS = start_mongo_server(MONGO_CLIENT)
 HTTP_SESSION = start_http_session()
 atexit.register(functools.partial(clean_up, MONGO_PROCESS))
--- poptimizer/store/tests/test_mongo.py
+++ poptimizer/store/tests/test_mongo.py
@@ -1,12 +1,21 @@
 import logging
+import signal
 import time
 
 import pymongo
 import pytest
 import requests
-from pymongo.errors import AutoReconnect, ServerSelectionTimeoutError
 
 from poptimizer.store import mongo
+
+
+def test_no_start_mongo_server(caplog):
+    mongo_pid = mongo.MONGO_PROCESS.pid
+    with caplog.at_level(logging.INFO):
+        process = mongo.start_mongo_server()
+    assert "Локальный сервер MongoDB уже работает" in caplog.text
+    assert process.pid == mongo_pid
+    assert process.is_running()
 
 
 def test_start_mongo_client():
@@ -14,40 +23,24 @@ def test_start_mongo_client():
         assert isinstance(client, pymongo.MongoClient)
         assert client.address == ("localhost", 27017)
         assert client.codec_options.tz_aware is False
-        assert client.server_selection_timeout == 3.0
-
-
-def test_no_start_mongo_server(caplog):
-    client = mongo.MONGO_CLIENT
-    mongo_pid = mongo.MONGO_PROCESS.pid
-    with caplog.at_level(logging.INFO):
-        process = mongo.start_mongo_server(client)
-    assert "Локальный сервер MongoDB уже работает" in caplog.text
-    assert process.pid == mongo_pid
 
 
 @pytest.fixture("function")
 def shutdown_mongo_server():
-    client = mongo.MONGO_CLIENT
-    try:
-        client["admin"].command("shutdown")
-    except AutoReconnect:
-        pass
+    mongo_process = mongo.MONGO_PROCESS
+    mongo_process.send_signal(signal.SIGTERM)
+    mongo_process.wait()
 
 
 @pytest.mark.usefixtures("shutdown_mongo_server")
 def test_start_mongo_server(caplog):
-    client = mongo.MONGO_CLIENT
-    with pytest.raises(ServerSelectionTimeoutError) as error:
-        client["admin"].command("serverStatus")
-    assert error.type == ServerSelectionTimeoutError
-    assert "localhost:27017: [Errno 61] Connection refused" in str(error.value)
+    assert not mongo.MONGO_PROCESS.is_running()
 
     with caplog.at_level(logging.INFO):
-        mongo.MONGO_PROCESS = mongo.start_mongo_server(client)
+        mongo.MONGO_PROCESS = mongo.start_mongo_server()
     assert "Запускается локальный сервер MongoDB" in caplog.text
     time.sleep(1)
-    assert client.address == ("localhost", 27017)
+    assert mongo.MONGO_CLIENT.address == ("localhost", 27017)
 
 
 def test_start_http_session():
@@ -62,10 +55,10 @@ def test_clean_up(caplog):
     with caplog.at_level(logging.INFO):
         mongo.clean_up(mongo.MONGO_PROCESS)
 
-    assert "Локальный сервер MongoDB остановлен со статусом 0" in caplog.text
-    assert not mongo.MONGO_PROCESS.is_running()
-
     assert "Подключение клиента MongoDB закрыто" in caplog.text
     assert mongo.MONGO_CLIENT.nodes == frozenset()
+
+    assert "Локальный сервер MongoDB остановлен со статусом 0" in caplog.text
+    assert not mongo.MONGO_PROCESS.is_running()
 
     assert "Сессия для обновления данных по интернет закрыта" in caplog.text
