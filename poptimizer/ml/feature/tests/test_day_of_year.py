97dfbd1cbe090d7f2ca9079f852eb224f0581594
WLM1ke <wlmike@gmail.com>
2019-09-16 14:36:35 +0300

    Исправлены тесты дня года с учетом обрезки данных

--- poptimizer/ml/feature/tests/test_day_of_year.py
+++ poptimizer/ml/feature/tests/test_day_of_year.py
@@ -1,7 +1,15 @@
 import pandas as pd
+import pytest
 from hyperopt.pyll import Apply
 
+from poptimizer.data import div
 from poptimizer.ml.feature import day_of_year, ON_OFF
+
+
+@pytest.fixture(scope="function", autouse=True)
+def set_stats_start(monkeypatch):
+    monkeypatch.setattr(div, "STATS_START", pd.Timestamp("2010-07-19"))
+    yield
 
 
 def test_day_of_year():
@@ -17,7 +25,9 @@ def test_day_of_year():
 
     df = feature.get()
     assert isinstance(df, pd.Series)
+    assert df.index[0] == (pd.Timestamp("2010-07-20"), "GMKN")
+    assert df.index[-1] == (pd.Timestamp("2019-08-30"), "SIBN")
 
-    assert df[(pd.Timestamp("2007-01-26"), "GMKN")] == 26
+    assert df[(pd.Timestamp("2011-01-26"), "GMKN")] == 26
     assert df[(pd.Timestamp("2019-08-30"), "NMTP")] == 242
-    assert df[(pd.Timestamp("2003-07-11"), "SIBN")] == 192
+    assert df[(pd.Timestamp("2013-07-11"), "SIBN")] == 192
