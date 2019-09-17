c37ca77ded1d3bee9b9d0e3f4fb1d3a08afcbd61
WLM1ke <wlmike@gmail.com>
2019-09-17 12:40:13 +0300

    Тесты для обновленного smart_lab менеджера

--- poptimizer/store/tests/test_smart_lab.py
+++ poptimizer/store/tests/test_smart_lab.py
@@ -1,32 +1,37 @@
 import pandas as pd
 import pytest
 
-from poptimizer import config
 from poptimizer.config import POptimizerError
-from poptimizer.store import client, smart_lab, DATE, TICKER, DIVIDENDS
+from poptimizer.store import smart_lab, DATE, TICKER, DIVIDENDS
+from poptimizer.store.mongo import MONGO_CLIENT
+from poptimizer.store.smart_lab import SMART_LAB
 
 
-@pytest.fixture(autouse=True)
-@pytest.mark.asyncio
-async def create_client(tmpdir_factory, monkeypatch):
-    temp_dir = tmpdir_factory.mktemp("smart_lab")
-    monkeypatch.setattr(config, "DATA_PATH", temp_dir)
-    async with client.Client():
-        yield
+@pytest.fixture(name="manager")
+def manager_in_clean_test_db():
+    MONGO_CLIENT.drop_database("test")
+    yield smart_lab.SmartLab(db="test")
+    MONGO_CLIENT.drop_database("test")
 
 
-@pytest.mark.asyncio
-async def test_smart_lab():
-    df = await smart_lab.SmartLab().get()
+def test_smart_lab(manager):
+    df = manager[SMART_LAB]
     assert isinstance(df, pd.DataFrame)
     assert df.index.name == DATE
     assert list(df.columns) == [TICKER, DIVIDENDS]
     assert df.dtypes[DIVIDENDS] == float
 
 
-@pytest.mark.asyncio
-async def test_smart_lab_raise(monkeypatch):
+def test_smart_lab_bad_url(monkeypatch, manager):
     monkeypatch.setattr(smart_lab, "URL", "https://smart-lab.ru/dividends12")
     with pytest.raises(POptimizerError) as error:
-        await smart_lab.SmartLab().get()
+        # noinspection PyStatementEffect
+        manager[SMART_LAB]
     assert str(error.value) == "Данные https://smart-lab.ru/dividends12 не загружены"
+
+
+def test_smart_lab_bad_collection(manager):
+    with pytest.raises(POptimizerError) as error:
+        # noinspection PyStatementEffect
+        manager["QQQ"]
+    assert "Отсутствуют данные test.misc.QQQ" == str(error.value)
