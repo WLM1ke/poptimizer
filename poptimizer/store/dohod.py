9b7498f117c702536beed2e48e29f321f6a9ed3c
WLM1ke <wlmike@gmail.com>
2019-09-17 13:16:51 +0300

    Загрузка данных с dohod.ru с помощью нового менеджера и парсера

--- poptimizer/store/dohod.py
+++ poptimizer/store/dohod.py
@@ -1,22 +1,18 @@
 """Менеджер данных по дивидендам с https://dohod.ru"""
-from typing import Union, Tuple
+from typing import Optional, Any, List, Dict
 
-import aiohttp
+import requests
 
 from poptimizer.config import POptimizerError
 from poptimizer.store import parser
-from poptimizer.store.manager import AbstractManager
-from poptimizer.store.utils_new import DATE
+from poptimizer.store.manager_new import AbstractManager
+from poptimizer.store.utils_new import DATE, DB
 
-# Данные  хранятся в отдельной базе
-CATEGORY_DOHOD = "dohod"
+# Наименование коллекции с данными
+DOHOD = "dohod"
 
 TABLE_INDEX = 2
 HEADER_SIZE = 1
-
-DATE_COLUMN = parser.DataColumn(0, {0: "Дата закрытия реестра"}, parser.date_parser)
-
-DIVIDENDS_COLUMN = parser.DataColumn(2, {0: "Дивиденд (руб.)"}, parser.div_parser)
 
 
 class Dohod(AbstractManager):
@@ -25,26 +21,33 @@ class Dohod(AbstractManager):
     Каждый раз обновляется с нуля.
     """
 
-    CREATE_FROM_SCRATCH = True
+    def __init__(self, db=DB) -> None:
+        super().__init__(collection=DOHOD, db=db, create_from_scratch=True)
 
-    def __init__(self, ticker: Union[str, Tuple[str, ...]]):
-        super().__init__(ticker, CATEGORY_DOHOD)
-
-    async def _download(self, name: str):
-        url = f"https://www.dohod.ru/ik/analytics/dividend/{name.lower()}"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                try:
-                    resp.raise_for_status()
-                except aiohttp.ClientResponseError:
-                    raise POptimizerError(f"Данные {url} не загружены")
-                else:
-                    html = await resp.text()
+    def _download(self, item: str, last_index: Optional[Any]) -> List[Dict[str, Any]]:
+        url = f"https://www.dohod.ru/ik/analytics/dividend/{item.lower()}"
+        with self._session.get(url) as respond:
+            try:
+                respond.raise_for_status()
+            except requests.HTTPError:
+                raise POptimizerError(f"Данные {url} не загружены")
+            else:
+                html = respond.text
         table = parser.HTMLTableParser(html, TABLE_INDEX)
-        columns = [DATE_COLUMN, DIVIDENDS_COLUMN]
-        df = table.make_df(columns, HEADER_SIZE)
-        df.columns = [DATE, name]
-        df = df.groupby(DATE, as_index=False).sum()
-        df.set_index(DATE, inplace=True)
-        df.sort_index(inplace=True)
-        return df[name]
+        date_col = parser.DataColumn(
+            DATE, 0, {0: "Дата закрытия реестра"}, parser.date_parser
+        )
+        div_col = parser.DataColumn(item, 2, {0: "Дивиденд (руб.)"}, parser.div_parser)
+        columns = [date_col, div_col]
+        data = table.get_formatted_data(columns, HEADER_SIZE)
+        return self._sort_and_group(item, data)
+
+    @staticmethod
+    def _sort_and_group(item: str, data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        rez = []
+        for row in sorted(data, key=lambda x: x[DATE]):
+            if not rez or row[DATE] != rez[-1][DATE]:
+                rez.append(row)
+            else:
+                rez[-1][item] += row[item]
+        return rez
