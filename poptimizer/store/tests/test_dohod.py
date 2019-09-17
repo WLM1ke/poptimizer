4206f612119f1fc6dd1ff621f6c397316dcbd130
WLM1ke <wlmike@gmail.com>
2019-09-17 15:11:44 +0300

    Тесты для dohod.ru под новый менеджер

--- poptimizer/store/tests/test_dohod.py
+++ poptimizer/store/tests/test_dohod.py
@@ -1,43 +1,54 @@
 import pandas as pd
 import pytest
 
-from poptimizer import config
 from poptimizer.config import POptimizerError
-from poptimizer.store import client, dohod
+from poptimizer.store import dohod
+from poptimizer.store.mongo import MONGO_CLIENT
 
 
-@pytest.fixture(autouse=True)
-@pytest.mark.asyncio
-async def create_client(tmpdir_factory, monkeypatch):
-    temp_dir = tmpdir_factory.mktemp("dohod")
-    monkeypatch.setattr(config, "DATA_PATH", temp_dir)
-    async with client.Client():
-        yield
+@pytest.fixture(name="manager")
+def manager_in_clean_test_db():
+    MONGO_CLIENT.drop_database("test")
+    yield dohod.Dohod(db="test")
+    MONGO_CLIENT.drop_database("test")
 
 
-@pytest.mark.asyncio
-async def test_dohod():
-    df = await dohod.Dohod(("VSMO",)).get()
-    assert isinstance(df, pd.Series)
+def test_dohod(manager):
+    df = manager["VSMO"]
+    assert isinstance(df, pd.DataFrame)
+    assert list(df.columns) == ["VSMO"]
+    df = df["VSMO"]
     assert len(df) >= 20
     assert df["2017-10-19"] == 762.68
     assert df["2004-03-29"] == 11
 
 
-@pytest.mark.asyncio
-async def test_dohod2():
-    df = await dohod.Dohod(("GAZP",)).get()
-    assert isinstance(df, pd.Series)
+def test_dohod2(manager):
+    df = manager["GAZP"]
+    assert isinstance(df, pd.DataFrame)
+    assert list(df.columns) == ["GAZP"]
+    df = df["GAZP"]
     assert len(df) >= 16
     assert df["2004-05-07"] == 0.69
     assert df["2017-07-20"] == 8.04
     assert df["2008-05-08"] == 2.66
 
 
-@pytest.mark.asyncio
-async def test_no_html():
+def test_dohod3(manager):
+    df = manager["CHMF"]
+    assert isinstance(df, pd.DataFrame)
+    assert list(df.columns) == ["CHMF"]
+    df = df["CHMF"]
+    assert len(df) >= 47
+    assert df["2018-06-19"] == 38.32 + 27.72
+    assert df["2016-07-05"] == 8.25 + 20.27
+    assert df["2019-06-18"] == 35.43
+
+
+def test_no_html(manager):
     with pytest.raises(POptimizerError) as error:
-        await dohod.Dohod(("TEST",)).get()
+        # noinspection PyStatementEffect
+        manager["TEST"]
     assert "Данные https://www.dohod.ru/ik/analytics/dividend/test не загружены" == str(
         error.value
     )
