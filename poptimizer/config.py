40a3e530302219b0eccaff7b5332c0567d2dd681
WLM1ke <wlmike@gmail.com>
2019-09-20 20:46:11 +0300

    Добавлены дивиденды ISKJ

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -33,35 +33,35 @@ T_SCORE = 2.0
 MAX_TRADE = 1 / 100
 
 # Период в торговых днях, за который медианный оборот торгов
-TURNOVER_PERIOD = 54
+TURNOVER_PERIOD = 48
 
 # Минимальный оборот - преимущества акции снижаются при приближении медианного оборота к данному уровню
 TURNOVER_CUT_OFF = 1 / 30
 
 # База дивидендов содержит данные с начала 2010 года
 # Постепенно срок будет сдвигаться к началу режима TQBR для корректного учета сдвига T+2
-STATS_START = pd.Timestamp("2010-03-01")
+STATS_START = pd.Timestamp("2010-04-01")
 
 # Параметры ML-модели
 ML_PARAMS = {
     "data": (
-        ("Label", {"days": 79, "div_share": 0.3, "on_off": True}),
-        ("Scaler", {"days": 225, "on_off": True}),
+        ("Label", {"days": 89, "div_share": 0.3, "on_off": True}),
+        ("Scaler", {"days": 238, "on_off": True}),
         ("Ticker", {"on_off": True}),
-        ("Mom12m", {"days": 178, "on_off": True, "periods": 2}),
-        ("DivYield", {"days": 239, "on_off": True, "periods": 1}),
-        ("Mom1m", {"days": 41, "on_off": False}),
-        ("RetMax", {"days": 54, "on_off": True}),
-        ("ChMom6m", {"days": 90, "on_off": True}),
-        ("STD", {"days": 27, "on_off": True}),
+        ("Mom12m", {"days": 187, "on_off": True, "periods": 2}),
+        ("DivYield", {"days": 263, "on_off": True, "periods": 2}),
+        ("Mom1m", {"days": 36, "on_off": False}),
+        ("RetMax", {"days": 48, "on_off": True}),
+        ("ChMom6m", {"days": 99, "on_off": True}),
+        ("STD", {"days": 24, "on_off": True}),
         ("DayOfYear", {"on_off": False}),
     ),
     "model": {
-        "bagging_temperature": 0.8741295882011074,
-        "depth": 13,
-        "l2_leaf_reg": 1.4138240966644873,
-        "learning_rate": 0.005390648531709855,
-        "one_hot_max_size": 1000,
-        "random_strength": 1.44386445646246,
+        "bagging_temperature": 0.49153923340279754,
+        "depth": 16,
+        "l2_leaf_reg": 0.5880940835637545,
+        "learning_rate": 0.005422182747620653,
+        "one_hot_max_size": 2,
+        "random_strength": 1.0632185857721845,
     },
 }
