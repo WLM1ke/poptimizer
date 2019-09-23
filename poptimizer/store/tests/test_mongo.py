b9988f54f8f7a1c0f09d7ce06fe1b87dff946d27
WLM1ke <wlmike@gmail.com>
2019-09-23 11:30:46 +0300

    Изменен порядок тестов
    После их завершения все сессии продолжают работать, так что последующие
    тесты не проваливаются

--- poptimizer/store/tests/test_mongo.py
+++ poptimizer/store/tests/test_mongo.py
@@ -1,51 +1,10 @@
 import logging
-import signal
 import time
 
 import pymongo
-import pytest
 import requests
 
 from poptimizer.store import mongo
-
-
-def test_no_start_mongo_server(caplog):
-    mongo_pid = mongo.MONGO_PROCESS.pid
-    with caplog.at_level(logging.INFO):
-        process = mongo.start_mongo_server()
-    assert "Локальный сервер MongoDB уже работает" in caplog.text
-    assert process.pid == mongo_pid
-    assert process.is_running()
-
-
-def test_start_mongo_client():
-    with mongo.start_mongo_client() as client:
-        assert isinstance(client, pymongo.MongoClient)
-        assert client.address == ("localhost", 27017)
-        assert client.codec_options.tz_aware is False
-
-
-@pytest.fixture("function")
-def shutdown_mongo_server():
-    mongo_process = mongo.MONGO_PROCESS
-    mongo_process.send_signal(signal.SIGTERM)
-    mongo_process.wait()
-
-
-@pytest.mark.usefixtures("shutdown_mongo_server")
-def test_start_mongo_server(caplog):
-    assert not mongo.MONGO_PROCESS.is_running()
-
-    with caplog.at_level(logging.INFO):
-        mongo.MONGO_PROCESS = mongo.start_mongo_server()
-    assert "Запускается локальный сервер MongoDB" in caplog.text
-    time.sleep(1)
-    assert mongo.MONGO_CLIENT.address == ("localhost", 27017)
-
-
-def test_start_http_session():
-    with mongo.start_http_session() as session:
-        assert isinstance(session, requests.Session)
 
 
 def test_clean_up(caplog):
@@ -62,3 +21,34 @@ def test_clean_up(caplog):
     assert not mongo.MONGO_PROCESS.is_running()
 
     assert "Сессия для обновления данных по интернет закрыта" in caplog.text
+
+
+def test_start_mongo_server(caplog):
+    assert not mongo.MONGO_PROCESS.is_running()
+
+    with caplog.at_level(logging.INFO):
+        mongo.MONGO_PROCESS = mongo.start_mongo_server()
+    assert "Запускается локальный сервер MongoDB" in caplog.text
+    time.sleep(1)
+    assert mongo.MONGO_CLIENT.address == ("localhost", 27017)
+
+
+def test_no_start_mongo_server(caplog):
+    mongo_pid = mongo.MONGO_PROCESS.pid
+    with caplog.at_level(logging.INFO):
+        process = mongo.start_mongo_server()
+    assert "Локальный сервер MongoDB уже работает" in caplog.text
+    assert process.pid == mongo_pid
+    assert process.is_running()
+
+
+def test_start_mongo_client():
+    mongo.MONGO_CLIENT = mongo.start_mongo_client()
+    assert isinstance(mongo.MONGO_CLIENT, pymongo.MongoClient)
+    assert mongo.MONGO_CLIENT.address == ("localhost", 27017)
+    assert mongo.MONGO_CLIENT.codec_options.tz_aware is False
+
+
+def test_start_http_session():
+    mongo.HTTP_SESSION = mongo.start_http_session()
+    assert isinstance(mongo.HTTP_SESSION, requests.Session)
