c881d392b06c9d79b2f39b762d2ff7afcaaaaa9e
WLM1ke <wlmike@gmail.com>
2019-09-27 16:00:06 +0300

    ML-модель с учетом нового фактора

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -31,7 +31,7 @@ AFTER_TAX = 1 - 0.13
 T_SCORE = 2.0
 
 # Максимальный объем одной торговой операции в долях портфеля
-MAX_TRADE = 1 / 100
+MAX_TRADE = 1 / 99
 
 # Минимальный оборот в виде доли портфеля
 # Преимущества акции снижаются при приближении медианного оборота к данному уровню
@@ -40,28 +40,29 @@ TURNOVER_FACTOR = 32
 
 # База дивидендов содержит данные с начала 2010 года
 # Постепенно срок будет сдвигаться к началу режима TQBR для корректного учета сдвига T+2
-STATS_START = pd.Timestamp("2010-05-01")
+STATS_START = pd.Timestamp("2010-06-01")
 
 # Параметры ML-модели
 ML_PARAMS = {
     "data": (
-        ("Label", {"days": 100, "div_share": 0.3, "on_off": True}),
-        ("Scaler", {"days": 243, "on_off": True}),
+        ("Label", {"days": 111, "div_share": 0.3, "on_off": True}),
+        ("Scaler", {"days": 220, "on_off": True}),
         ("Ticker", {"on_off": True}),
-        ("Mom12m", {"days": 176, "on_off": True, "periods": 2}),
+        ("Mom12m", {"days": 187, "on_off": True, "periods": 1}),
         ("DivYield", {"days": 256, "on_off": True, "periods": 1}),
-        ("Mom1m", {"days": 36, "on_off": False}),
-        ("RetMax", {"days": 51, "on_off": True}),
-        ("ChMom6m", {"days": 106, "on_off": True}),
+        ("Mom1m", {"days": 32, "on_off": True}),
+        ("RetMax", {"days": 48, "on_off": True}),
+        ("ChMom6m", {"days": 97, "on_off": True}),
         ("STD", {"days": 24, "on_off": True}),
         ("DayOfYear", {"on_off": False}),
+        ("TurnOver", {"days": 242, "normalize": False, "on_off": True}),
     ),
     "model": {
-        "bagging_temperature": 0.980263392152567,
+        "bagging_temperature": 0.7086647045742429,
         "depth": 14,
-        "l2_leaf_reg": 0.6506004886756825,
-        "learning_rate": 0.007674177708594184,
-        "one_hot_max_size": 2,
-        "random_strength": 0.4970136876264279,
+        "l2_leaf_reg": 1.6621105324472478,
+        "learning_rate": 0.0037710058578652875,
+        "one_hot_max_size": 1000,
+        "random_strength": 1.8447224379576972,
     },
 }
