b7f2908c0406d98301b497cd9e8b756b8daad765
WLM1ke <wlmike@gmail.com>
2020-01-05 12:46:44 +0300

    v 0.7

--- poptimizer/__init__.py
+++ poptimizer/__init__.py
@@ -4,4 +4,4 @@ from poptimizer.ml import find_better_model, partial_dependence_curve
 from poptimizer.portfolio import *
 from poptimizer.reports import history, report
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
