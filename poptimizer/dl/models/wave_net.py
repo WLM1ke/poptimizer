5ce28f4ec402bf1a4c44cd68e4008d98fa6af843
WLM1ke <wlmike@gmail.com>
2020-03-22 17:56:48 +0300

    Рефакторинг WaveNet

--- poptimizer/dl/models/wave_net.py
+++ poptimizer/dl/models/wave_net.py
@@ -1,4 +1,4 @@
-"""Базовая LSTM-модель"""
+"""Модель на основе WaveNet."""
 from typing import Dict, Union, List
 
 import numpy as np
@@ -8,11 +8,19 @@ from torch.utils.data import DataLoader
 
 
 class SubBlock(nn.Module):
-    """Блок с гейтами."""
+    """Блок с гейтом и остаточным соединением."""
 
     def __init__(
-        self, kernels: int = 2, gate_channels: int = 32, residual_channels: int = 32
+        self, kernels: int, gate_channels: int, residual_channels: int
     ) -> None:
+        """
+        :param kernels:
+            Размер сверток в signal и gate сверточных слоях.
+        :param gate_channels:
+            Количество каналов в signal и gate сверточных слоях.
+        :param residual_channels:
+            Количество каналов на входе и по обходному пути.
+        """
         super().__init__()
         self.signal_gate_pad = nn.ConstantPad1d(padding=(kernels - 1, 0), value=0.0)
         self.signal_conv = nn.Conv1d(
@@ -27,43 +35,63 @@ class SubBlock(nn.Module):
             kernel_size=kernels,
             stride=1,
         )
-        self.residual_conv = nn.Conv1d(
+        self.output_conv = nn.Conv1d(
             in_channels=gate_channels, out_channels=residual_channels, kernel_size=1
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """         |----------------------------------------------|  *residual*
-                    |                                              |
-                    |    |-- signal 1x1 -- relu  --|               |
-           -> ------|----|                         * ------ 1x1 -- + -->
-                         |-- gate 1x1   -- sigma --|
-         """
+        """
+           |-------------------------|
+           | |-signal-relu-|         |
+        ->-|-|             *-output->+->
+             |-gate--sigma-|
+        """
         y = self.signal_gate_pad(x)
+
         y_signal = self.signal_conv(y)
         y_signal = torch.relu(y_signal)
+
         y_gate = self.gate_conv(y)
         y_gate = torch.sigmoid(y_gate)
+
         y = y_signal * y_gate
-        y = self.residual_conv(y)
+        y = self.output_conv(y)
+
         return y + x
 
 
 class Block(nn.Module):
-    """Блок, состоящий из нескольких маленьких блоков скипом и уменьшением размера последовательности
-    в конце."""
+    """Блок, состоящий из нескольких маленьких блоков и последующим уменьшением размерности.
+
+     Имеет два выхода:
+     - Основной с уменьшенной в два раза размерностью;
+     - Скип для суммирования с остальными скипами и расчета общего выходного значения.
+     """
 
     def __init__(
         self,
-        kernels: int = 2,
-        sub_blocks: int = 2,
-        gate_channels: int = 32,
-        residual_channels: int = 32,
-        skip_channels: int = 256,
+        sub_blocks: int,
+        kernels: int,
+        gate_channels: int,
+        residual_channels: int,
+        skip_channels: int,
     ) -> None:
+        """
+        :param sub_blocks:
+            Количество маленьких блоков в блоке.
+        :param kernels:
+            Размер сверток в signal и gate сверточных слоях.
+        :param gate_channels:
+            Количество каналов в signal и gate сверточных слоях маленьких блоков.
+        :param residual_channels:
+            Количество каналов на входе и по обходному пути маленьких блоков.
+        :param skip_channels:
+            Количество каналов у скипа.
+        """
         super().__init__()
-        self.gated_sub_blocks = nn.ModuleList()
+        self.sub_blocks = nn.ModuleList()
         for i in range(sub_blocks):
-            self.gated_sub_blocks.append(
+            self.sub_blocks.append(
                 SubBlock(
                     kernels=kernels,
                     gate_channels=gate_channels,
@@ -82,41 +110,53 @@ class Block(nn.Module):
         )
 
     def forward(self, x: torch.Tensor) -> (torch.Tensor, torch.Tensor):
-        """Большой блок."""
+        """
+                        |-dilated->
+        ->n x SubBlock -|
+                        |-skip---->
+        """
         y = x
-        for sub_block in self.gated_sub_blocks:
+        for sub_block in self.sub_blocks:
             y = sub_block(y)
+
         skip = self.skip_convs(y)
         skip = skip[:, :, -1:]
+
         y = self.dilated_pad(y)
         y = self.dilated_convs(y)
+
         return y, skip
 
 
 class WaveNet(nn.Module):
-    """Реализация WaveNet для прогнозирования котировок.
+    """За основу взята WaveNet https://arxiv.org/abs/1609.03499
 
-    https://arxiv.org/abs/1609.03499
+    Сверточная сеть с большим полем восприятия - удваивается при увеличении глубины на один уровень,
+    что позволяет анализировать длинные последовательности котировок.
     """
 
     def __init__(
         self,
         data_loader: DataLoader,
-        start_bn: bool = True,
-        kernels: int = 2,
-        sub_blocks: int = 2,
-        gate_channels: int = 32,
-        residual_channels: int = 32,
-        skip_channels: int = 256,
-        end_channels: int = 256,
+        start_bn: bool,
+        sub_blocks: int,
+        kernels: int,
+        gate_channels: int,
+        residual_channels: int,
+        skip_channels: int,
+        end_channels: int,
     ) -> None:
         super().__init__()
+
         batch = next(iter(data_loader))
         in_sequences = len(batch["Sequence"])
         history_days = batch["Sequence"][0].shape[1]
 
-        self.skip_channels = skip_channels
-        self.bn = start_bn and nn.BatchNorm1d(in_sequences)
+        if start_bn:
+            self.bn = nn.BatchNorm1d(in_sequences)
+        else:
+            self.bn = nn.Identity()
+
         self.start_conv = nn.Conv1d(
             in_channels=in_sequences, out_channels=residual_channels, kernel_size=1
         )
@@ -126,16 +166,19 @@ class WaveNet(nn.Module):
         for block in range(blocks):
             self.blocks.append(
                 Block(
-                    kernels=kernels,
                     sub_blocks=sub_blocks,
+                    kernels=kernels,
                     gate_channels=gate_channels,
                     residual_channels=residual_channels,
                     skip_channels=skip_channels,
                 )
             )
+
+        self.skip_channels = skip_channels
         self.final_skip_conv = nn.Conv1d(
             in_channels=residual_channels, out_channels=skip_channels, kernel_size=1
         )
+
         self.end_conv = nn.Conv1d(
             in_channels=skip_channels, out_channels=end_channels, kernel_size=1
         )
@@ -146,13 +189,12 @@ class WaveNet(nn.Module):
     def forward(
         self, batch: Dict[str, Union[torch.Tensor, List[torch.Tensor]]]
     ) -> torch.Tensor:
-        """Реализация WaveNet для прогнозирования котировок.
-
-        https://github.com/vincentherrmann/pytorch-wavenet/blob/master/wavenet_model.py
+        """
+        ->nb-start-Block-|-Block-|-...-Block-|-final_skip-|
+                         |-skips-+-...-skips-+-skips------+-relu-end-relu-output->
         """
         y = torch.stack(batch["Sequence"], dim=1)
-        if self.bn:
-            y = self.bn(y)
+        y = self.bn(y)
         y = self.start_conv(y)
 
         skips = torch.zeros((y.shape[0], self.skip_channels, 1))
