67a427f2bc932bacbe262d6db94630532359c2b2
WLM1ke <wlmike@gmail.com>
2020-03-31 11:13:45 +0300

    Дополнены гены оптимизатора и скорректированы гены политики

--- poptimizer/evolve/chromosomes/optimizer.py
+++ poptimizer/evolve/chromosomes/optimizer.py
@@ -1,16 +1,42 @@
 """Гены и хромосома ответственные за параметры оптимизации."""
-from poptimizer.evolve.chromosomes.chromosome import GeneParams, Chromosome
+from poptimizer.evolve.chromosomes import chromosome
 
-WEIGHT_DECAY = GeneParams(
-    path=["optimizer", "weight_decay"],
-    default_value=0.01,
+BETA2 = chromosome.GeneParams(
+    path=["optimizer", "betas"],
+    default_value=0.999,
+    phenotype_function=lambda x: (0.9, x),
+    lower_bound=0.0,
+    upper_bound=1.0,
+)
+EPS = chromosome.GeneParams(
+    path=["optimizer", "eps"],
+    default_value=1e-8,
     phenotype_function=float,
     lower_bound=0.0,
     upper_bound=None,
 )
+WEIGHT_DECAY = chromosome.GeneParams(
+    path=["optimizer", "weight_decay"],
+    default_value=1e-2,
+    phenotype_function=float,
+    lower_bound=0.0,
+    upper_bound=None,
+)
+AMSGRAD = chromosome.GeneParams(
+    path=["optimizer", "amsgrad"],
+    default_value=0.9,
+    phenotype_function=lambda x: bool(int(x)),
+    lower_bound=0.0,
+    upper_bound=1.99,
+)
 
 
-class Optimizer(Chromosome):
-    """Хромосома ответственная за параметры обучения."""
+class Optimizer(chromosome.Chromosome):
+    """Хромосома ответственная за параметры оптимизации с помощью AdamW."""
 
-    _GENES = [WEIGHT_DECAY]
+    _GENES = [
+        BETA2,  # Бета2 - значение Бета1 переписывается One cycle learning rate policy
+        EPS,  # Корректировка для численной стабильности
+        WEIGHT_DECAY,  # L2 регуляризация
+        AMSGRAD,  # Нужно ли использовать AMSGrad
+    ]
--- poptimizer/evolve/chromosomes/scheduler.py
+++ poptimizer/evolve/chromosomes/scheduler.py
@@ -1,82 +1,74 @@
-"""Гены и хромосома ответственные за параметры обучения."""
-from poptimizer.evolve.chromosomes.chromosome import GeneParams, Chromosome
+"""Гены и хромосома ответственные за политику обучения."""
+from poptimizer.evolve.chromosomes import chromosome
 
-MAX_LR = GeneParams(
+MAX_LR = chromosome.GeneParams(
     path=["scheduler", "max_lr"],
     default_value=0.01,
     phenotype_function=float,
     lower_bound=0.0,
     upper_bound=None,
 )
-EPOCHS = GeneParams(
+EPOCHS = chromosome.GeneParams(
     path=["scheduler", "epochs"],
     default_value=1.8,
     phenotype_function=int,
     lower_bound=1.0,
     upper_bound=None,
 )
-PCT_START = GeneParams(
+PCT_START = chromosome.GeneParams(
     path=["scheduler", "pct_start"],
     default_value=0.3,
     phenotype_function=float,
     lower_bound=0.0,
     upper_bound=1.0,
 )
-ANNEAL_STRATEGY = GeneParams(
+ANNEAL_STRATEGY = chromosome.GeneParams(
     path=["scheduler", "anneal_strategy"],
-    default_value=0.8,
-    phenotype_function=lambda x: {0: "cos", 1: "liner"}[int(x)],
+    default_value=0.9,
+    phenotype_function=lambda x: {0: "cos", 1: "linear"}[int(x)],
     lower_bound=0.0,
     upper_bound=1.99,
 )
-CYCLE_MOMENTUM = GeneParams(
-    path=["scheduler", "cycle_momentum"],
-    default_value=1.2,
-    phenotype_function=lambda x: bool(int(x)),
-    lower_bound=0.0,
-    upper_bound=1.99,
-)
-BASE_MOMENTUM = GeneParams(
+BASE_MOMENTUM = chromosome.GeneParams(
     path=["scheduler", "base_momentum"],
     default_value=0.85,
     phenotype_function=float,
     lower_bound=0.0,
     upper_bound=1.0,
 )
-MAX_MOMENTUM = GeneParams(
+MAX_MOMENTUM = chromosome.GeneParams(
     path=["scheduler", "max_momentum"],
     default_value=0.95,
     phenotype_function=float,
     lower_bound=0.0,
     upper_bound=1.0,
 )
-DIV_FACTOR = GeneParams(
+DIV_FACTOR = chromosome.GeneParams(
     path=["scheduler", "div_factor"],
     default_value=25.0,
     phenotype_function=float,
-    lower_bound=0.0,
+    lower_bound=1.0,
     upper_bound=None,
 )
-FINAL_DIV_FACTOR = GeneParams(
+FINAL_DIV_FACTOR = chromosome.GeneParams(
     path=["scheduler", "final_div_factor"],
     default_value=1e4,
     phenotype_function=float,
-    lower_bound=0.0,
+    lower_bound=1.0,
     upper_bound=None,
 )
 
 
-class Scheduler(Chromosome):
-    """Хромосома ответственная за параметры обучения."""
+class Scheduler(chromosome.Chromosome):
+    """Хромосома ответственная за параметры One cycle learning rate policy."""
 
     _GENES = [
-        MAX_LR,
-        EPOCHS,
-        PCT_START,
-        ANNEAL_STRATEGY,
-        CYCLE_MOMENTUM,
-        BASE_MOMENTUM,
-        MAX_MOMENTUM,
-        DIV_FACTOR,
-        FINAL_DIV_FACTOR,
+        MAX_LR,  # Максимальная скорость обучения
+        EPOCHS,  # Количество эпох обучения
+        PCT_START,  # Доля шагов разогрева
+        ANNEAL_STRATEGY,  # Стратегия снижения скорости обучения
+        BASE_MOMENTUM,  # Базовый моментум
+        MAX_MOMENTUM,  # Максимальный моментум
+        DIV_FACTOR,  # Понижающий коэффициент скорости обучения для периода разогрева
+        FINAL_DIV_FACTOR,  # Понижающий коэффициент для скорости обучения в конце цикла понижения
     ]
