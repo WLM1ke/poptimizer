ab4227514410a5efe056404b975e057e68566c00
WLM1ke <wlmike@gmail.com>
2020-04-04 15:32:31 +0300

    Функции для получения и создания организмов

--- poptimizer/evolve/population.py
+++ poptimizer/evolve/population.py
@@ -33,49 +33,52 @@ class Organism:
     ):
         self.collection = collection
 
-        self.id = _id or bson.ObjectId()
-        self.genotype = Genotype(genotype)
+        self._id = _id or bson.ObjectId()
+        if not isinstance(genotype, Genotype):
+            genotype = Genotype(genotype)
+        self._genotype = genotype
+
         if _id is None:
-            organism = {ID: self.id, GENOTYPE: self.genotype.as_dict}
+            organism = {ID: self._id, GENOTYPE: self._genotype.to_dict()}
             self.collection.insert_one(organism)
 
-        self.model = model
-        self.sharpe = sharpe
+        self._model = model
+        self._sharpe = sharpe
         self.sharpe_date = sharpe_date
 
     def __str__(self):
-        return str(self.genotype)
+        return str(self._genotype)
 
-    def fitness(self, tickers, end):
+    def sharpe(self, tickers, end):
         """Вычисляет коэффициент Шарпа."""
         if self.sharpe_date == end:
-            return self.sharpe
+            return self._sharpe
 
-        trainer = Trainer(tickers, end, self.genotype.phenotype, self.model)
+        trainer = Trainer(tickers, end, self._genotype.phenotype, self._model)
         sharpe = trainer.sharpe
-        self.sharpe = sharpe
+        self._sharpe = sharpe
         self.sharpe_date = end
         update = {SHARPE: sharpe, SHARPE_DATE: end}
 
-        if self.model is None:
-            self.model = trainer.model
-            update[MODEL] = self.model
+        if self._model is None:
+            self._model = trainer.model
+            update[MODEL] = self._model
 
-        self.collection.update_one({ID: self.id}, {"$set": update})
+        self.collection.update_one({ID: self._id}, {"$set": update})
         return sharpe
 
     def kill(self):
         """Убивает организм."""
-        self.collection.delete_one({ID: self.id})
+        self.collection.delete_one({ID: self._id})
 
     def make_child(self):
         """Осуществляет одну мутацию и сохраняет полученный генотип в MongoDB."""
-        genotypes = [organism.genotype for organism in sample_organism(3)]
-        child_genotype = self.genotype.make_child(*genotypes)
+        genotypes = [organism._genotype for organism in _sample_organism(3)]
+        child_genotype = self._genotype.make_child(*genotypes)
         return Organism(genotype=child_genotype)
 
 
-def sample_organism(num: int, collection=COLLECTION) -> Iterable["Organism"]:
+def _sample_organism(num: int, collection=COLLECTION) -> Iterable["Organism"]:
     """Выбирает несколько случайных организмов.
 
     Необходимо для реализации размножения и отбора.
@@ -87,6 +90,17 @@ def sample_organism(num: int, collection=COLLECTION) -> Iterable["Organism"]:
 def count(collection=COLLECTION) -> int:
     """Количество документов в коллекции."""
     return collection.count_documents({})
+
+
+def create_new_organism() -> Organism:
+    """Создает новый организм с пустым генотипом."""
+    return Organism()
+
+
+def get_random_organism() -> Organism:
+    """Получить случайный организм из популяции."""
+    organism, *_ = _sample_organism(1)
+    return organism
 
 
 def print_stat(collection=COLLECTION) -> None:
