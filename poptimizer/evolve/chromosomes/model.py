c201298181afde557ef888d4bce5d655a3e167b8
WLM1ke <wlmike@gmail.com>
2020-04-06 15:15:37 +0300

    Рефакторинг хромосомы модели

--- poptimizer/evolve/chromosomes/model.py
+++ poptimizer/evolve/chromosomes/model.py
@@ -2,60 +2,60 @@
 from poptimizer.evolve.chromosomes.chromosome import GeneParams, Chromosome
 
 START_BN = GeneParams(
-    path=["model", "start_bn"],
-    default_value=(1.1, 1.2),
-    phenotype_function=lambda x: bool(int(x)),
+    path=("model", "start_bn"),
+    default_range=(1.1, 1.2),
     lower_bound=0.0,
     upper_bound=1.99,
+    phenotype_function=lambda x: bool(int(x)),
 )
 KERNELS = GeneParams(
-    path=["model", "kernels"],
-    default_value=(2.1, 2.9),
-    phenotype_function=int,
+    path=("model", "kernels"),
+    default_range=(2.1, 2.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 SUB_BLOCKS = GeneParams(
-    path=["model", "sub_blocks"],
-    default_value=(1.1, 1.9),
-    phenotype_function=int,
+    path=("model", "sub_blocks"),
+    default_range=(1.1, 1.2),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 GATE_CHANNELS = GeneParams(
-    path=["model", "gate_channels"],
-    default_value=(4.1, 4.9),
-    phenotype_function=int,
+    path=("model", "gate_channels"),
+    default_range=(2.1, 2.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 RESIDUAL_CHANNELS = GeneParams(
-    path=["model", "residual_channels"],
-    default_value=(4.1, 4.9),
-    phenotype_function=int,
+    path=("model", "residual_channels"),
+    default_range=(2.1, 2.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 SKIP_CHANNELS = GeneParams(
-    path=["model", "skip_channels"],
-    default_value=(4.1, 4.9),
-    phenotype_function=int,
+    path=("model", "skip_channels"),
+    default_range=(2.1, 2.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 END_CHANNELS = GeneParams(
-    path=["model", "end_channels"],
-    default_value=(4.1, 4.9),
-    phenotype_function=int,
+    path=("model", "end_channels"),
+    default_range=(2.1, 2.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 
 
 class Model(Chromosome):
-    """Хромосома ответственная за параметры обучения."""
+    """Хромосома ответственная за параметры модели."""
 
-    _GENES = [
+    _GENES = (
         START_BN,
         KERNELS,
         SUB_BLOCKS,
@@ -63,4 +63,4 @@ class Model(Chromosome):
         RESIDUAL_CHANNELS,
         SKIP_CHANNELS,
         END_CHANNELS,
-    ]
+    )
