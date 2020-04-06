b8b53caa9612f0871245f5c62687e3968378a868
WLM1ke <wlmike@gmail.com>
2020-04-06 13:40:54 +0300

    Рефакторинг хромосомы данных

--- poptimizer/evolve/chromosomes/data.py
+++ poptimizer/evolve/chromosomes/data.py
@@ -2,29 +2,29 @@
 from poptimizer.evolve.chromosomes.chromosome import GeneParams, Chromosome
 
 BATCH_SIZE = GeneParams(
-    path=["data", "batch_size"],
-    default_value=(127.0, 128.0),
-    phenotype_function=int,
+    path=("data", "batch_size"),
+    default_range=(128.1, 128.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 HISTORY_DAYS = GeneParams(
-    path=["data", "history_days"],
-    default_value=(21.0, 252.0),
-    phenotype_function=int,
+    path=("data", "history_days"),
+    default_range=(252.1, 252.9),
     lower_bound=2.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 FORECAST_DAYS = GeneParams(
-    path=["data", "forecast_days"],
-    default_value=(1.1, 21.0),
-    phenotype_function=int,
+    path=("data", "forecast_days"),
+    default_range=(196.1, 196.9),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 
 
 class Data(Chromosome):
-    """Хромосома ответственная за параметры обучения."""
+    """Хромосома ответственная за параметры данных."""
 
-    _GENES = [BATCH_SIZE, HISTORY_DAYS, FORECAST_DAYS]
+    _GENES = (BATCH_SIZE, HISTORY_DAYS, FORECAST_DAYS)
