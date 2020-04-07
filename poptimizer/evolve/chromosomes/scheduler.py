f5fcff050b5100af989565bd78622b318a601cba
WLM1ke <wlmike@gmail.com>
2020-04-07 15:14:18 +0300

    Тесты для политики обучения

--- poptimizer/evolve/chromosomes/scheduler.py
+++ poptimizer/evolve/chromosomes/scheduler.py
@@ -2,67 +2,67 @@
 from poptimizer.evolve.chromosomes import chromosome
 
 MAX_LR = chromosome.GeneParams(
-    path=["scheduler", "max_lr"],
-    default_value=(0.01, 0.1),
-    phenotype_function=float,
+    path=("scheduler", "max_lr"),
+    default_range=(0.001, 0.01),
     lower_bound=0.0,
     upper_bound=None,
+    phenotype_function=float,
 )
 EPOCHS = chromosome.GeneParams(
-    path=["scheduler", "epochs"],
-    default_value=(1.1, 1.2),
-    phenotype_function=int,
+    path=("scheduler", "epochs"),
+    default_range=(1.1, 1.2),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=int,
 )
 PCT_START = chromosome.GeneParams(
-    path=["scheduler", "pct_start"],
-    default_value=(0.29, 0.31),
-    phenotype_function=float,
+    path=("scheduler", "pct_start"),
+    default_range=(0.299, 0.301),
     lower_bound=0.0,
     upper_bound=1.0,
+    phenotype_function=float,
 )
 ANNEAL_STRATEGY = chromosome.GeneParams(
-    path=["scheduler", "anneal_strategy"],
-    default_value=(0.1, 0.9),
-    phenotype_function=lambda x: {0: "cos", 1: "linear"}[int(x)],
+    path=("scheduler", "anneal_strategy"),
+    default_range=(0.1, 0.9),
     lower_bound=0.0,
     upper_bound=1.99,
+    phenotype_function=lambda x: {0: "cos", 1: "linear"}[int(x)],
 )
 BASE_MOMENTUM = chromosome.GeneParams(
-    path=["scheduler", "base_momentum"],
-    default_value=(0.84, 0.86),
-    phenotype_function=float,
+    path=("scheduler", "base_momentum"),
+    default_range=(0.849, 0.851),
     lower_bound=0.0,
     upper_bound=1.0,
+    phenotype_function=float,
 )
 MAX_MOMENTUM = chromosome.GeneParams(
-    path=["scheduler", "max_momentum"],
-    default_value=(0.94, 0.96),
-    phenotype_function=float,
+    path=("scheduler", "max_momentum"),
+    default_range=(0.949, 0.951),
     lower_bound=0.0,
     upper_bound=1.0,
+    phenotype_function=float,
 )
 DIV_FACTOR = chromosome.GeneParams(
-    path=["scheduler", "div_factor"],
-    default_value=(23.0, 26.0),
-    phenotype_function=float,
+    path=("scheduler", "div_factor"),
+    default_range=(24.9, 25.1),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=float,
 )
 FINAL_DIV_FACTOR = chromosome.GeneParams(
-    path=["scheduler", "final_div_factor"],
-    default_value=(0.9e4, 1.1e4),
-    phenotype_function=float,
+    path=("scheduler", "final_div_factor"),
+    default_range=(0.999e4, 1.01e4),
     lower_bound=1.0,
     upper_bound=None,
+    phenotype_function=float,
 )
 
 
 class Scheduler(chromosome.Chromosome):
     """Хромосома ответственная за параметры One cycle learning rate policy."""
 
-    _GENES = [
+    _GENES = (
         MAX_LR,  # Максимальная скорость обучения
         EPOCHS,  # Количество эпох обучения
         PCT_START,  # Доля шагов разогрева
@@ -71,4 +71,4 @@ class Scheduler(chromosome.Chromosome):
         MAX_MOMENTUM,  # Максимальный моментум
         DIV_FACTOR,  # Понижающий коэффициент скорости обучения для периода разогрева
         FINAL_DIV_FACTOR,  # Понижающий коэффициент для скорости обучения в конце цикла понижения
-    ]
+    )
---
+++ poptimizer/evolve/chromosomes/tests/test_scheduler.py
@@ -0,0 +1,35 @@
+from poptimizer.evolve.chromosomes import scheduler
+
+
+def test_init_no_data():
+    chromo = scheduler.Scheduler({})
+    assert len(chromo.data) == 8
+    assert 0.001 < chromo.data["max_lr"] < 0.01
+    assert 1.1 < chromo.data["epochs"] < 1.2
+    assert 0.299 < chromo.data["pct_start"] < 0.301
+    assert 0.1 < chromo.data["anneal_strategy"] < 0.9
+    assert 0.849 < chromo.data["base_momentum"] < 0.851
+    assert 0.949 < chromo.data["max_momentum"] < 0.951
+    assert 24.9 < chromo.data["div_factor"] < 25.1
+    assert 0.999e4 < chromo.data["final_div_factor"] < 1.01e4
+
+
+def test_setup_phenotype():
+    chromosome_data = dict(
+        max_lr=0.2,
+        epochs=10.1,
+        pct_start=0.4,
+        anneal_strategy=1.5,
+        base_momentum=0.6,
+        max_momentum=0.7,
+        div_factor=1.8,
+        final_div_factor=9.9,
+    )
+    chromo = scheduler.Scheduler(chromosome_data)
+    base_phenotype = dict(type="Test_Model")
+    phenotype_data = dict(type="Test_Model", scheduler=chromosome_data)
+    phenotype_data["scheduler"]["epochs"] = 10
+    # noinspection PyTypeChecker
+    phenotype_data["scheduler"]["anneal_strategy"] = "linear"
+    chromo.change_phenotype(base_phenotype)
+    assert base_phenotype == phenotype_data
