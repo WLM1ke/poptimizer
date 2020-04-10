94b33cadbeda6c03db078ad5dbc957b01b1856f2
WLM1ke <wlmike@gmail.com>
2020-04-10 16:15:44 +0300

    Признак для тикера

--- poptimizer/dl/features/__init__.py
+++ poptimizer/dl/features/__init__.py
@@ -2,4 +2,5 @@
 from poptimizer.dl.features.dividends import Dividends
 from poptimizer.dl.features.label import Label
 from poptimizer.dl.features.prices import Prices
+from poptimizer.dl.features.ticker import Ticker
 from poptimizer.dl.features.weight import Weight
---
+++ poptimizer/dl/features/ticker.py
@@ -0,0 +1,26 @@
+"""Номер тикера данного примера."""
+import torch
+
+from poptimizer.dl.data_params import DataParams
+from poptimizer.dl.features.feature import Feature
+
+
+class Ticker(Feature):
+    """Обратная величина СКО полной доходности обрезанная для низких значений."""
+
+    def __init__(self, ticker: str, params: DataParams):
+        super().__init__(ticker, params)
+        self._idx = torch.tensor([params.tickers.index(ticker)], dtype=torch.float)
+
+    def __getitem__(self, item: int) -> torch.Tensor:
+        return self._idx
+
+    @staticmethod
+    def key() -> str:
+        """Ключ по которому нужно сохранять признак."""
+        return "Embedding"
+
+    @staticmethod
+    def unique() -> bool:
+        """Является ли признак единственным для данного ключа."""
+        return True
