a13bd70f2e527ac028b2693c2736eeb7467f8592
WLM1ke <wlmike@gmail.com>
2020-04-12 12:52:19 +0300

    Тесты для популяции

--- poptimizer/evolve/population.py
+++ poptimizer/evolve/population.py
@@ -5,6 +5,7 @@ import bson
 import pandas as pd
 from pymongo.collection import Collection
 
+from poptimizer.config import POptimizerError
 from poptimizer.dl.trainer import Trainer
 from poptimizer.evolve.genotype import Genotype
 from poptimizer.store.mongo import DB, MONGO_CLIENT
@@ -19,6 +20,11 @@ WINS = "wins"
 MODEL = "model"
 SHARPE = "sharpe"
 SHARPE_DATE = "sharpe_date"
+SHARPE_TICKERS = "tickers"
+
+
+class OrganismIdError(POptimizerError):
+    """Ошибка попытки загрузить организм с отсутствующим в базе ID."""
 
 
 class Organism:
@@ -31,10 +37,12 @@ class Organism:
 
     def __init__(
         self,
+        *,
         _id: Optional[bson.ObjectId] = None,
         genotype: Optional[Genotype] = None,
-        collection: Collection = COLLECTION,
+        collection: Collection = None,
     ):
+        collection = collection or COLLECTION
         self.collection = collection
 
         if _id is None:
@@ -43,11 +51,22 @@ class Organism:
             collection.insert_one(organism)
 
         organism = collection.find_one({ID: _id})
+        if organism is None:
+            raise OrganismIdError(f"В популяции нет организма с ID: {_id}")
         organism[GENOTYPE] = Genotype(organism[GENOTYPE])
         self._data = organism
 
     def __str__(self) -> str:
         return str(self._data[GENOTYPE])
+
+    @property
+    def id(self) -> bson.ObjectId:
+        """ID организма."""
+        return self._data[ID]
+
+    def die(self) -> NoReturn:
+        """Организм удаляется из популяции."""
+        self.collection.delete_one({ID: self._data[ID]})
 
     def _update(self, update: Dict[str, Any]) -> NoReturn:
         """Обновление данных в MongoDB и внутреннего состояния организма."""
@@ -59,25 +78,34 @@ class Organism:
         """Количество побед."""
         return self._data.get(WINS, 0)
 
+    def kill(self, organism: "Organism") -> NoReturn:
+        """Убивает другой организм и ведет счет побед."""
+        update = {WINS: self.wins + 1}
+        self._update(update)
+        organism.die()
+
     def evaluate_fitness(self, tickers: Tuple[str, ...], end: pd.Timestamp) -> float:
         """Вычисляет коэффициент Шарпа."""
         organism = self._data
 
-        if organism.get(SHARPE_DATE) == end:
+        if organism.get(SHARPE_DATE) == end and organism.get(SHARPE_TICKERS) == sorted(
+            tickers
+        ):
             return organism[SHARPE]
 
         trainer = Trainer(tickers, end, organism[GENOTYPE].get_phenotype())
         sharpe = trainer.sharpe
         model = trainer.model
 
-        update = {SHARPE: sharpe, SHARPE_DATE: end, MODEL: model}
+        update = {
+            SHARPE: sharpe,
+            SHARPE_DATE: end,
+            SHARPE_TICKERS: sorted(tickers),
+            MODEL: model,
+        }
         self._update(update)
 
         return sharpe
-
-    def die(self) -> NoReturn:
-        """Организм удаляется из популяции."""
-        self.collection.delete_one({ID: self._data[ID]})
 
     def make_child(self) -> "Organism":
         """Создает новый организм с помощью дифференциальной мутации."""
@@ -85,57 +113,55 @@ class Organism:
         child_genotype = self._data[GENOTYPE].make_child(*genotypes)
         return Organism(genotype=child_genotype)
 
-    def kill(self, organism: "Organism") -> NoReturn:
-        """Убивает другой организм и ведет счет побед."""
-        update = {WINS: self.wins + 1}
-        self._update(update)
-        organism.die()
 
-
-def _sample_organism(
-    num: int, collection: Collection = COLLECTION
-) -> Iterable[Organism]:
+def _sample_organism(num: int, collection: Collection = None) -> Iterable[Organism]:
     """Выбирает несколько случайных организмов.
 
     Необходимо для реализации размножения и отбора.
     """
+    collection = collection or COLLECTION
     pipeline = [{"$sample": {"size": num}}, {"$project": {"_id": True}}]
     organisms = collection.aggregate(pipeline)
-    yield from (Organism(organism[ID]) for organism in organisms)
+    yield from (Organism(**organism) for organism in organisms)
 
 
-def count(collection: Collection = COLLECTION) -> int:
+def count(collection: Collection = None) -> int:
     """Количество организмов в популяции."""
+    collection = collection or COLLECTION
     return collection.count_documents({})
 
 
-def create_new_organism(collection: Collection = COLLECTION) -> Organism:
+def create_new_organism(collection: Collection = None) -> Organism:
     """Создает новый организм с пустым генотипом."""
+    collection = collection or COLLECTION
     return Organism(collection=collection)
 
 
-def get_random_organism(collection: Collection = COLLECTION) -> Organism:
+def get_random_organism(collection: Collection = None) -> Organism:
     """Получить случайный организм из популяции."""
+    collection = collection or COLLECTION
     organism, *_ = tuple(_sample_organism(1, collection))
     return organism
 
 
-def get_all_organisms(collection=COLLECTION) -> Iterable[Organism]:
+def get_all_organisms(collection=None) -> Iterable[Organism]:
     """Получить все имеющиеся организмы."""
+    collection = collection or COLLECTION
     id_dicts = collection.find(filter={}, projection=["_id"], sort=[(SHARPE, 1)])
     for id_dict in id_dicts:
         yield Organism(**id_dict)
 
 
-def print_stat(collection=COLLECTION) -> NoReturn:
+def print_stat(collection=None) -> NoReturn:
     """Статистика - минимальное и максимальное значение коэффициента Шарпа."""
+    collection = collection or COLLECTION
     db_find = collection.find
 
     sort_type = (1, -1)
     params = {"filter": {SHARPE: {"$exists": True}}, "projection": [SHARPE], "limit": 1}
     cursors = (db_find(sort=[(SHARPE, up_down)], **params) for up_down in sort_type)
-    sharpes = [tuple(cursor) for cursor in cursors]
-    sharpes = [f"{sharpe[0][SHARPE]:.4f}" if sharpe else "-" for sharpe in sharpes]
+    cursors = [tuple(cursor) for cursor in cursors]
+    sharpes = [f"{cursor[0][SHARPE]:.4f}" if cursor else "-" for cursor in cursors]
     print(f"Коэффициент Шарпа - ({', '.join(sharpes)})")
 
     params = {
@@ -145,7 +171,6 @@ def print_stat(collection=COLLECTION) -> NoReturn:
         "limit": 1,
     }
     wins = list(db_find(**params))
-    print(wins)
     max_wins = None
     if wins:
         max_wins, *_ = wins
---
+++ poptimizer/evolve/tests/test_population.py
@@ -0,0 +1,205 @@
+from typing import Iterable
+
+import pandas as pd
+import pytest
+
+from poptimizer.evolve import population
+from poptimizer.evolve.genotype import Genotype
+from poptimizer.store.mongo import DB, MONGO_CLIENT
+
+TEST_COLLECTION = MONGO_CLIENT[DB]["Test"]
+
+
+@pytest.fixture(scope="function", autouse=True)
+def test_db(monkeypatch):
+    monkeypatch.setattr(population, "COLLECTION", TEST_COLLECTION)
+    yield
+    TEST_COLLECTION.drop()
+
+
+def test_create_empty_organism():
+    organism = population.Organism()
+    assert len(organism._data) == 2
+    assert isinstance(organism._data[population.GENOTYPE], Genotype)
+    id_ = organism.id
+
+    organism_data = TEST_COLLECTION.find_one({population.ID: id_})
+    assert len(organism_data) == 2
+    assert organism_data[population.ID] == id_
+    assert organism_data[population.GENOTYPE] is None
+
+
+def test_create_organism_from_genotype_and_reload():
+    genotype_data = {"Scheduler": {"max_lr": 7}}
+    organism = population.Organism(genotype=Genotype(genotype_data))
+    assert len(organism._data) == 2
+    assert isinstance(organism._data[population.GENOTYPE], Genotype)
+    id_ = organism.id
+    genotype = organism._data[population.GENOTYPE]
+    assert genotype["Scheduler"]["max_lr"] == 7
+
+    organism_data = TEST_COLLECTION.find_one({population.ID: id_})
+    assert len(organism_data) == 2
+    assert organism_data[population.ID] == id_
+    assert organism_data[population.GENOTYPE] == genotype
+    assert organism_data[population.GENOTYPE]["Scheduler"]["max_lr"] == 7
+
+    organism = population.Organism(_id=id_)
+    assert len(organism._data) == 2
+    assert organism.id == id_
+    assert organism._data[population.GENOTYPE] == genotype
+    assert organism._data[population.GENOTYPE]["Scheduler"]["max_lr"] == 7
+
+
+def test_die():
+    organism = population.Organism()
+    id_ = organism.id
+    assert population.Organism(_id=id_).id == id_
+
+    organism.die()
+
+    with pytest.raises(population.OrganismIdError) as error:
+        population.Organism(_id=id_)
+    assert "В популяции нет организма с ID" in str(error.value)
+
+
+def test_initial_wins():
+    assert population.Organism().wins == 0
+
+
+def test_kill():
+    winner = population.Organism()
+    loser1 = population.Organism()
+    loser2 = population.Organism()
+
+    assert winner.wins == 0
+    assert loser1.wins == 0
+    assert loser2.wins == 0
+
+    winner.kill(loser1)
+    assert winner.wins == 1
+    winner.kill(loser2)
+    assert winner.wins == 2
+
+    assert population.Organism(_id=winner._data[population.ID]).wins == 2
+
+    with pytest.raises(population.OrganismIdError) as error:
+        population.Organism(_id=loser1._data[population.ID])
+    assert "В популяции нет организма с ID" in str(error.value)
+
+    with pytest.raises(population.OrganismIdError) as error:
+        population.Organism(_id=loser2._data[population.ID])
+    assert "В популяции нет организма с ID" in str(error.value)
+
+
+class FakeTrainer:
+    COUNTER = 0
+
+    # noinspection PyUnusedLocal
+    def __init__(self, tickers, end, phenotype):
+        pass
+
+    @property
+    def sharpe(self):
+        self.__class__.COUNTER += 1
+        return 5
+
+    @property
+    def model(self):
+        return 6
+
+
+def test_evaluate_fitness(monkeypatch):
+    monkeypatch.setattr(population, "Trainer", FakeTrainer)
+
+    organism = population.Organism()
+    id_ = organism.id
+
+    assert FakeTrainer.COUNTER == 0
+    sharpe = organism.evaluate_fitness(("GAZP", "AKRN"), pd.Timestamp("2020-04-12"))
+
+    assert sharpe == 5
+    assert FakeTrainer.COUNTER == 1
+
+    organism_reloaded = population.Organism(_id=id_)
+
+    assert organism_reloaded._data[population.SHARPE] == 5
+    assert organism_reloaded._data[population.SHARPE_DATE] == pd.Timestamp("2020-04-12")
+    assert organism_reloaded._data[population.SHARPE_TICKERS] == ["AKRN", "GAZP"]
+    assert organism_reloaded._data[population.MODEL] == 6
+    assert (
+        organism_reloaded.evaluate_fitness(("GAZP", "AKRN"), pd.Timestamp("2020-04-12"))
+        == 5
+    )
+    assert FakeTrainer.COUNTER == 1
+
+    assert (
+        organism_reloaded.evaluate_fitness(("GAZP", "LKOH"), pd.Timestamp("2020-04-12"))
+        == 5
+    )
+    assert FakeTrainer.COUNTER == 2
+
+    assert (
+        organism_reloaded.evaluate_fitness(("GAZP", "LKOH"), pd.Timestamp("2020-04-13"))
+        == 5
+    )
+    assert FakeTrainer.COUNTER == 3
+
+
+def test_make_child():
+    organism = population.Organism()
+    # Нужно минимум три организма для дифференциальной эволюции
+    population.Organism()
+    population.Organism()
+
+    assert isinstance(organism.make_child(), population.Organism)
+
+
+def test_count():
+    assert population.count() == 0
+
+    org1 = population.Organism()
+    assert population.count() == 1
+
+    org2 = population.Organism()
+    assert population.count() == 2
+
+    org1.kill(org2)
+    assert population.count() == 1
+
+    org1.die()
+    assert population.count() == 0
+
+
+def test_create_new_organism():
+    org = population.create_new_organism()
+    assert isinstance(org, population.Organism)
+
+    id_ = org.id
+    organism_data = TEST_COLLECTION.find_one({population.ID: id_})
+    assert len(organism_data) == 2
+    assert organism_data[population.ID] == id_
+    assert organism_data[population.GENOTYPE] is None
+
+
+def test_get_random_organism():
+    ids = set()
+    ids.add(population.Organism().id)
+    ids.add(population.Organism().id)
+    ids.add(population.Organism().id)
+
+    org = population.get_random_organism()
+    assert org.id in ids
+
+
+def test_get_all_organisms():
+    ids = list()
+    ids.append(population.Organism().id)
+    ids.append(population.Organism().id)
+    ids.append(population.Organism().id)
+    ids.sort()
+
+    organisms = population.get_all_organisms()
+    assert isinstance(organisms, Iterable)
+
+    assert sorted(org.id for org in organisms) == ids
