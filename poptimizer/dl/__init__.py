f7ecf5129abce61a99c8b6a9938d813c8d71fdb3
WLM1ke <wlmike@gmail.com>
2020-04-26 12:50:15 +0300

    Misc import error

--- poptimizer/dl/__init__.py
+++ poptimizer/dl/__init__.py
@@ -1,8 +1,3 @@
 """Прогнозирование доходности  с помощью нейронных сетей."""
 from poptimizer.dl.data_loader import PhenotypeData
-from poptimizer.dl.model import (
-    ModelError,
-    Model,
-    DegeneratedForecastError,
-    NotTrainedError,
-)
+from poptimizer.dl.model import ModelError, Model, DegeneratedForecastError
