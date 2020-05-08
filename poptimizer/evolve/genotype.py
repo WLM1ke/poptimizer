a97ae549b56f0ad8666b9fa9db0213bae0fdd0d1
WLM1ke <wlmike@gmail.com>
2020-05-08 12:16:07 +0300

    Уменьшена доля дивидендов и оптимизирована ML-модель

--- poptimizer/evolve/genotype.py
+++ poptimizer/evolve/genotype.py
@@ -11,7 +11,7 @@ BASE_PHENOTYPE = {
     "type": "WaveNet",
     "data": {
         "features": {
-            "Label": {"div_share": 0.6},
+            "Label": {"div_share": 0.5},
             "Prices": {},
             "Dividends": {},
             "Turnover": {},
