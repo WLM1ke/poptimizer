82692938ae2b18396cbfed51f752a8c2b38b550e
WLM1ke <wlmike@gmail.com>
2020-06-02 13:07:45 +0300

    Рефактоинг и тесты для хранилища данных об эволюции

--- poptimizer/evolve/store.py
+++ poptimizer/evolve/store.py
@@ -9,7 +9,7 @@ from poptimizer.evolve.genotype import Genotype
 from poptimizer.store.mongo import DB, MONGO_CLIENT
 
 # Коллекция для хранения моделей
-COLLECTION = MONGO_CLIENT[DB]["models"]
+_COLLECTION = MONGO_CLIENT[DB]["models"]
 
 # Название столбца с индексом
 ID = "_id"
@@ -17,36 +17,52 @@ ID = "_id"
 
 def get_collection() -> Collection:
     """Коллекция для хранения моделей."""
-    return COLLECTION
+    return _COLLECTION
 
 
-class Field:
-    """Дескриптор поля документа."""
+class BaseField:
+    """Базовый дескриптор поля.
+
+    Информация об изменениях значения поля сохраняется в вспомогательную переменную для последующих
+    инкрементальных апдейтов.
+    """
 
     def __init__(self, *, index: bool = False):
         self._name = index and ID
 
     def __set_name__(self, owner: Type, name: str):
         self._name = self._name or name
-        print(self._name)
 
     def __set__(self, instance: Any, value: Any):
         """Сохраняет измененные значение во вспомогательный словарь."""
-        data_dict = instance.__dict__
+        data_dict = vars(instance)
         key = self._name
 
         update = data_dict["_update"]
         update[key] = value
         data_dict[key] = value
 
-    def __get__(self, instance: Any, owner: Type):
+    def __get__(self, instance: Any, owner: Type) -> Any:
         try:
-            instance.__dict__[self._name]
+            data_dict = vars(instance)
+            return data_dict[self._name]
         except KeyError as error:
             raise AttributeError(f"'{owner.__name__}' object has no attribute {error}")
 
 
-class GenotypeField(Field):
+class DefaultField(BaseField):
+    """Дескриптор поля со значением по умолчанию."""
+
+    def __init__(self, default: Optional[Any] = None):
+        super().__init__()
+        self._default = default
+
+    def __get__(self, instance: Any, owner: Type) -> Any:
+        data_dict = vars(instance)
+        return data_dict.get(self._name, self._default)
+
+
+class GenotypeField(BaseField):
     """Дескриптор для генотипа.
 
     При необходимости присваиваемое значение преобразуется к типу генотип.
@@ -66,10 +82,10 @@ class Doc:
     """Документ в базе данных."""
 
     def __init__(
-            self,
-            *,
-            id_: Optional[bson.ObjectId] = None,
-            genotype: Optional[Genotype] = None,
+        self,
+        *,
+        id_: Optional[bson.ObjectId] = None,
+        genotype: Optional[Genotype] = None,
     ):
         self._update = {}
         if id_ is None:
@@ -85,24 +101,26 @@ class Doc:
         if doc is None:
             raise IdError(id_)
 
-        for key, value in doc:
+        for key, value in doc.items():
             setattr(self, key, value)
 
         self._update.clear()
 
     def save(self) -> NoReturn:
-        """Сохраняет измененные значения."""
+        """Сохраняет измененные значения в MongoDB."""
         collection = get_collection()
         update = self._update
 
-        collection.update_one({ID: self.id}, {"$set": update})
+        collection.update_one(
+            filter={ID: self.id}, update={"$set": update}, upsert=True
+        )
         update.clear()
 
-    id = Field(index=True)
+    id = BaseField(index=True)
     genotype = GenotypeField()
-    wins = Field()
-    model = Field()
-    llh = Field()
-    date = Field()
-    timer = Field()
-    tickers = Field()
+    wins = DefaultField(0)
+    model = DefaultField()
+    llh = BaseField()
+    date = DefaultField()
+    timer = DefaultField(0)
+    tickers = DefaultField()
