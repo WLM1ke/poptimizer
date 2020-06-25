ee241e888aedd160d390f1fd9c8a80a1990a7dbc
WLM1ke <wlmike@gmail.com>
2020-06-25 17:40:20 +0300

    Исправлены тесты с учетом отключаемого дня в году

--- poptimizer/evolve/tests/test_genotype.py
+++ poptimizer/evolve/tests/test_genotype.py
@@ -14,7 +14,7 @@ def test_get_phenotype():
             "batch_size": 3,
             "history_days": 4,
             "forecast_days": 5,
-            "features": {"Prices": {}, "Ticker": {"on": True}},
+            "features": {"Prices": {}, "Ticker": {"on": True}, "DayOfYear": {"on": True}},
         },
     }
     assert result == genotype.get_phenotype()
@@ -23,17 +23,49 @@ def test_get_phenotype():
 def test_make_child(monkeypatch):
     chromosomes_types = [chromosomes.Data]
 
-    parent = {"Data": {"batch_size": 3, "history_days": 4, "forecast_days": 5, "ticker_on": 2}}
-    base = {"Data": {"batch_size": 2, "history_days": 5, "forecast_days": 4, "ticker_on": 3}}
-    diff1 = {"Data": {"batch_size": 1, "history_days": 3, "forecast_days": 6, "ticker_on": 3}}
-    diff2 = {"Data": {"batch_size": 4, "history_days": 6, "forecast_days": 3, "ticker_on": 1}}
+    parent = {
+        "Data": {
+            "batch_size": 3,
+            "history_days": 4,
+            "forecast_days": 5,
+            "ticker_on": 2,
+            "day_of_year_on": 1,
+        }
+    }
+    base = {
+        "Data": {
+            "batch_size": 2,
+            "history_days": 5,
+            "forecast_days": 4,
+            "ticker_on": 3,
+            "day_of_year_on": 7,
+        }
+    }
+    diff1 = {
+        "Data": {
+            "batch_size": 1,
+            "history_days": 3,
+            "forecast_days": 6,
+            "ticker_on": 3,
+            "day_of_year_on": 3,
+        }
+    }
+    diff2 = {
+        "Data": {
+            "batch_size": 4,
+            "history_days": 6,
+            "forecast_days": 3,
+            "ticker_on": 1,
+            "day_of_year_on": 8,
+        }
+    }
 
     parent = Genotype(parent, all_chromosome_types=chromosomes_types)
     base = Genotype(base, all_chromosome_types=chromosomes_types)
     diff1 = Genotype(diff1, all_chromosome_types=chromosomes_types)
     diff2 = Genotype(diff2, all_chromosome_types=chromosomes_types)
 
-    monkeypatch.setattr(chromosome.random, "rand", lambda _: (0.89, 0.91, 0.89, 0.89))
+    monkeypatch.setattr(chromosome.random, "rand", lambda _: (0.89, 0.91, 0.89, 0.89, 0.89))
 
     child = parent.make_child(base, diff1, diff2)
 
@@ -44,5 +76,6 @@ def test_make_child(monkeypatch):
             "history_days": 4,
             "forecast_days": 4 + (6 - 3) * 0.8,
             "ticker_on": 3 + (3 - 1) * 0.8,
+            "day_of_year_on": 7 + (3 - 8) * 0.8,
         }
     }
