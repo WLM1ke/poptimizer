707013bc9f9c430b35318b0f3d067ad18541df92
WLM1ke <wlmike@gmail.com>
2020-08-22 13:38:38 +0300

    refactor: явная передача параметров обработчик запросов

--- poptimizer/data/core/app/requests.py
+++ poptimizer/data/core/app/requests.py
@@ -2,21 +2,39 @@
 import pandas as pd
 
 from poptimizer.data.core import ports
-from poptimizer.data.core.app import config
-from poptimizer.data.core.domain import factories, model, services
-
-uow_factory = config.get_uow_factory()
+from poptimizer.data.core.domain import factories, repo, services
 
 
-def get_table(table_name: ports.TableName) -> pd.DataFrame:
+class UnitOfWork:
+    """Группа операций с таблицами, в конце которой осуществляется сохранение изменных данных."""
+
+    def __init__(self, db_session: ports.AbstractDBSession) -> None:
+        """Создает изолированную сессию с базой данной и репо."""
+        self._db_session = db_session
+        self._repo = repo.Repo(db_session)
+
+    def __enter__(self) -> "UnitOfWork":
+        """Возвращает репо с таблицами."""
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:  # type: ignore
+        """Сохраняет изменные данные в базу данных."""
+        self._db_session.commit(self._repo.seen())
+
+    @property
+    def repo(self) -> repo.Repo:
+        """Репо, хранящее информацию о виденных в рамках UoW таблицах."""
+        return self._repo
+
+
+def get_table(table_name: ports.TableName,
+              db_session: ports.AbstractDBSession,
+              updater: ports.AbstractUpdater) -> pd.DataFrame:
     """Возвращает таблицу по наименованию."""
-    with uow_factory() as repo:
-        updater = model.registry.get_specs(table_name).updater
-        table = repo.get(table_name)
+    with UnitOfWork(db_session) as uow:
+        table = uow.repo.get(table_name)
         if table is None:
-            df = updater.get_update()
-            table = factories.create_table(table_name, df)
-            repo.add(table)
-        elif services.need_update(table):
-            table.df = updater.get_update()
+            table = factories.create_table(table_name)
+            uow.repo.add(table)
+        services.update_table(table, updater)
         return table.df
