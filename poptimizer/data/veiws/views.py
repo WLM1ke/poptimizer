64dd0b7174fcd277062e79b02a700b6d6b57f1b3
WLM1ke <wlmike@gmail.com>
2020-08-22 14:22:08 +0300

    refactor: переименование

--- poptimizer/data/veiws/views.py
+++ poptimizer/data/veiws/views.py
@@ -2,12 +2,12 @@
 import pandas as pd
 
 from poptimizer.data.core import ports
-from poptimizer.data.core.app import config, requests
+from poptimizer.data.core.app import config, handlers
 
 
 def last_history_date() -> pd.Timestamp:
     """Последняя доступная дата исторических котировок."""
     table_name = ports.TableName("trading_dates", "trading_dates")
     app_config = config.get()
-    df = requests.get_table(table_name, app_config)
+    df = handlers.get_table(table_name, app_config)
     return pd.Timestamp(df.loc[0, "till"])
