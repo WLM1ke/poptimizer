0cef79b7687e7284b18d642a0df121946d43991a
WLM1ke <wlmike@gmail.com>
2020-08-23 10:15:59 +0300

    feature: загрузчик данных для conomy.ru

---
+++ poptimizer/data/infrastructure/updaters/conomy.py
@@ -0,0 +1,118 @@
+"""Загрузка данных с https://www.conomy.ru/."""
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+from typing import List, cast
+
+import pandas as pd
+import pyppeteer
+from pyppeteer.browser import Browser
+from pyppeteer.page import Page
+
+from poptimizer.data.core import ports
+from poptimizer.data.infrastructure.updaters import names, parser
+
+logger = logging.getLogger(__name__)
+
+# Параметры поиска страницы эмитента
+SEARCH_URL = "https://www.conomy.ru/search"
+SEARCH_FIELD = '//*[@id="issuer_search"]'
+
+# Параметры поиска данных по дивидендам
+DIVIDENDS_MENU = '//*[@id="page-wrapper"]/div/nav/ul/li[5]/a'
+DIVIDENDS_TABLE = '//*[@id="page-container"]/div[2]/div/div[1]'
+
+# Номер таблицы на html-странице и строки с заголовком
+TABLE_INDEX = 1
+
+# Параметры проверки обыкновенная акция или привилегированная
+COMMON_TICKER_LENGTH = 4
+PREFERRED_TICKER_ENDING = "P"
+
+
+@asynccontextmanager
+async def get_browser() -> Browser:
+    """Асинхронный браузер с автоматическим закрытием после использования."""
+    browser = await pyppeteer.launch()
+    try:
+        yield browser
+    finally:
+        await browser.close()
+
+
+async def load_ticker_page(page: Page, ticker: str) -> None:
+    """Вводит в поле поиска тикер и переходит на страницу с информацией по эмитенту."""
+    await page.goto(SEARCH_URL)
+    await page.waitForXPath(SEARCH_FIELD)
+    element, *_ = await page.xpath(SEARCH_FIELD)
+    await element.type(ticker)
+    await element.press("Enter")
+
+
+async def load_dividends_table(page: Page) -> None:
+    """Выбирает на странице эмитента меню дивиденды и дожидается загрузки таблиц с ними."""
+    await page.waitForXPath(DIVIDENDS_MENU)
+    element, *_ = await page.xpath(DIVIDENDS_MENU)
+    await element.click()
+    await page.waitForXPath(DIVIDENDS_TABLE)
+
+
+async def get_html(ticker: str) -> str:
+    """Возвращает html-код страницы с данными по дивидендам с сайта https://www.conomy.ru/."""
+    async with get_browser() as browser:
+        page = await browser.newPage()
+        await load_ticker_page(page, ticker)
+        await load_dividends_table(page)
+        html = await page.content()
+        return cast(str, html)
+
+
+def is_common(ticker: str) -> bool:
+    """Определяет является ли акция обыкновенной."""
+    if len(ticker) == COMMON_TICKER_LENGTH:
+        return True
+    elif len(ticker) == COMMON_TICKER_LENGTH + 1:
+        if ticker[COMMON_TICKER_LENGTH] == PREFERRED_TICKER_ENDING:
+            return False
+    raise ports.DataError(f"Некорректный тикер {ticker}")
+
+
+def get_col_desc(ticker: str) -> List[parser.ColDesc]:
+    """Формирует список с описанием необходимых столбцов."""
+    date = parser.ColDesc(
+        num=5,
+        raw_name=("E", "Дата закрытия реестра акционеров", "Под выплату дивидендов"),
+        name=names.DATE,
+        parser_func=parser.date_parser,
+    )
+    columns = [date]
+    if is_common(ticker):
+        common = parser.ColDesc(
+            num=7,
+            raw_name=("G", "Размер дивидендов", "АОИ"),
+            name=ticker,
+            parser_func=parser.div_parser,
+        )
+        columns.append(common)
+        return columns
+    preferred = parser.ColDesc(
+        num=8, raw_name=("H", "Размер дивидендов", "АПИ"), name=ticker, parser_func=parser.div_parser,
+    )
+    columns.append(preferred)
+    return columns
+
+
+class ConomyUpdater(ports.AbstractUpdater):
+    """Обновление для таблиц с дивидендами на https://www.conomy.ru/."""
+
+    def get_update(self, name: ports.TableName) -> pd.DataFrame:
+        """Получение дивидендов для заданного тикера."""
+        group, ticker = name
+        if group != ports.CONOMY:
+            raise ports.DataError(f"Некорректное имя таблицы для обновления {name}")
+        logger.info(f"Загрузка данных: {name}")
+
+        html = asyncio.run(get_html(ticker))
+        cols_desc = get_col_desc(ticker)
+        table = parser.HTMLTable(html, TABLE_INDEX, cols_desc)
+        return table.get_df()
---
+++ poptimizer/data/infrastructure/updaters/parser.py
@@ -0,0 +1,105 @@
+"""Парсеры для html-таблиц."""
+import re
+from datetime import datetime
+from typing import Callable, List, NamedTuple, Optional, Tuple, Union
+
+import bs4
+import pandas as pd
+
+from poptimizer.data.core import ports
+
+DIV_PATTERN = r".*\d"
+DATE_PATTERN = r"\d{2}\.\d{2}\.\d{4}"
+
+
+ParserFunc = Callable[[str], Union[None, float, datetime]]
+
+
+class ColDesc(NamedTuple):
+    """Описание столбца с данными.
+
+    Используется для выбора определенных столбцов из html-таблицы, проверки ожидаемых значений в
+    заголовках и преобразования из строк в нужный формат.
+
+    - num: номер столбца
+    - raw_name: часть исходного наименования
+    - name: целевое наименование столбца
+    - parser_func: функция для парсинга значений
+    """
+
+    num: int
+    raw_name: Union[str, Tuple[str, ...]]
+    name: str
+    parser_func: ParserFunc
+
+
+def date_parser(date: str) -> Optional[datetime]:
+    """Функция парсинга значений в столбце с датами закрытия реестра."""
+    re_date = re.search(DATE_PATTERN, date)
+    if re_date:
+        date_string = re_date.group(0)
+        return datetime.strptime(date_string, "%d.%m.%Y")  # noqa: WPS323
+    return None
+
+
+def div_parser(div: str) -> Optional[float]:
+    """Функция парсинга значений в столбце с дивидендами."""
+    re_div = re.search(DIV_PATTERN, div)
+    if re_div:
+        date_string = re_div.group(0)
+        date_string = date_string.replace(",", ".")
+        date_string = date_string.replace(" ", "")
+        return float(date_string)
+    return None
+
+
+class HTMLTable:
+    """Извлекает таблицу из html-страницы."""
+
+    def __init__(self, html: str, table_num: int, cols_desc: List[ColDesc]) -> None:
+        """Проверяет наличие таблицы на html-странице."""
+        soup = bs4.BeautifulSoup(html, "lxml")
+        try:
+            table = soup.find_all("table")[table_num]
+        except IndexError:
+            raise ports.DataError(f"На странице нет таблицы {table_num}")
+        self._table = f"html{table}/html"
+        self._cols_desc = cols_desc
+
+    def get_df(self) -> pd.DataFrame:
+        """Получает таблицу из html-документа и форматирует ее в соответствии с описанием."""
+        converters = {desc.num: desc.parser_func for desc in self._cols_desc}
+        header_nums = self._get_header_nums()
+        df, *_ = pd.read_html(self._table, header=header_nums, converters=converters)
+        self._validate_header(df.columns)
+        return self._get_selected_col(df)
+
+    def _get_header_nums(self) -> List[int]:
+        """Формирует список строк, содержащих заголовки."""
+        raw_name = self._cols_desc[0].raw_name
+        if isinstance(raw_name, str):
+            headers = [0]
+        else:
+            num_of_headers = len(raw_name)
+            headers = list(range(num_of_headers))
+        return headers
+
+    def _validate_header(self, columns: pd.Index) -> None:
+        """Проверяет, что заголовки соответствуют описанию."""
+        for desc in self._cols_desc:
+            header = columns[desc.num]
+            raw_name = desc.raw_name
+            if isinstance(raw_name, str) and raw_name in header:
+                continue
+            if all(part in name for part, name in zip(raw_name, header)):
+                continue
+            raise ports.DataError(f"Неверный заголовок: {desc.raw_name} не входит в {header}")
+
+    def _get_selected_col(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Выбирает столбцы в соответствии с описанием."""
+        cols_desc = self._cols_desc
+        selected_col = [desc.num for desc in cols_desc]
+        df = df.iloc[:, selected_col]
+        df.columns = [desc.name for desc in cols_desc]
+        index_name = cols_desc[0].name
+        return df.set_index(index_name)
