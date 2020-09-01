f995a270d294ec187c0d3adeffd74f08e44cd07d
WLM1ke <wlmike@gmail.com>
2020-09-01 09:52:39 +0300

    refactor: обработчики под новый репо

--- poptimizer/data/app/handlers.py
+++ poptimizer/data/app/handlers.py
@@ -1,44 +1,44 @@
 """Запросы таблиц."""
 import pandas as pd
 
-from poptimizer.data.domain import repo
+from poptimizer.data.app.uow import UnitOfWork
+from poptimizer.data.domain import factories, model
 from poptimizer.data.domain.services import tables
-from poptimizer.data.ports import app, base, outer
+from poptimizer.data.ports import app, base
 
 
-class UnitOfWork:
-    """Группа операций с таблицами, в конце которой осуществляется сохранение изменных данных."""
-
-    def __init__(self, db_session: outer.AbstractDBSession) -> None:
-        """Создает изолированную сессию с базой данной и репо."""
-        self._db_session = db_session
-        self._repo = repo.Repo(db_session)
-
-    def __enter__(self) -> "UnitOfWork":
-        """Возвращает репо с таблицами."""
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback) -> None:  # type: ignore
-        """Сохраняет изменные данные в базу данных."""
-        self._db_session.commit(self._repo.seen())
-
-    @property
-    def repo(self) -> repo.Repo:
-        """Репо, хранящее информацию о виденных в рамках UoW таблицах."""
-        return self._repo
+def _load_or_create_table(
+    table_name: base.TableName,
+    app_config: app.Config,
+    uow: UnitOfWork,
+) -> model.Table:
+    table = uow.repo.get(table_name)
+    if table is None:
+        desc = app_config.description_registry[table_name.group]
+        table = factories.create_table(table_name, desc)
+        uow.repo.add(table)
+    return table
 
 
-def get_df(table_name: base.TableName, app_config: app.Config) -> pd.DataFrame:
+def get_df(
+    table_name: base.TableName,
+    app_config: app.Config,
+    force_update: bool = False,
+) -> pd.DataFrame:
     """Возвращает таблицу по наименованию."""
-    with UnitOfWork(app_config.db_session) as uow:
-        table = uow.repo.get(table_name)
-        tables.update(table, app_config.description_registry)
+    with UnitOfWork(app_config.description_registry, app_config.db_session) as uow:
+        helper_name = tables.get_helper_name(table_name)
+        helper = None
+        if helper_name is not None:
+            helper = _load_or_create_table(helper_name, app_config, uow)
+            tables.update(helper, helper=None)
+
+        table = _load_or_create_table(table_name, app_config, uow)
+        tables.update(table, helper, force_update)
+
         return table.df
 
 
 def get_df_force_update(table_name: base.TableName, app_config: app.Config) -> pd.DataFrame:
     """Возвращает таблицу по наименованию с принудительным обновлением."""
-    with UnitOfWork(app_config.db_session) as uow:
-        table = uow.repo.get(table_name)
-        tables.force_update_table(table, app_config.description_registry)
-        return table.df
+    return get_df(table_name, app_config, force_update=True)
--- poptimizer/data/domain/services/tables.py
+++ poptimizer/data/domain/services/tables.py
@@ -22,11 +22,3 @@ def update(
     end_of_trading_day = trading_day.get_end(helper)
     if force or table.need_update(end_of_trading_day):
         table.update()
-
-
-def force_update(
-    table: model.Table,
-    helper: Optional[model.Table],
-) -> None:
-    """Принудительно обновляет таблицу."""
-    update(table, helper, force=True)
