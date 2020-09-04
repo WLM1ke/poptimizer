fb3f1bc1c00504f75984fdc6a347009c6810d894
WLM1ke <wlmike@gmail.com>
2020-09-04 15:58:56 +0300

    feat: функция загрузки цен закрытия

--- poptimizer/data/views/moex.py
+++ poptimizer/data/views/moex.py
@@ -1,10 +1,13 @@
 """Функции предоставления данных по котировкам акций."""
+import functools
 from typing import Tuple
 
+import numpy as np
 import pandas as pd
 
 from poptimizer.data.config import bootstrap
 from poptimizer.data.ports import base, col
+from poptimizer.data.views import crop
 
 
 def last_history_date() -> pd.Timestamp:
@@ -35,3 +38,26 @@ def lot_size(tickers: Tuple[str, ...]) -> pd.Series:
     requests_handler = bootstrap.get_handler()
     df = requests_handler.get_df(table_name)
     return df.loc[list(tickers), col.LOT_SIZE]
+
+
+@functools.lru_cache(maxsize=1)
+def prices(tickers: Tuple[str, ...], last_date: pd.Timestamp) -> pd.DataFrame:
+    """Дневные цены закрытия для указанных тикеров до указанной даты включительно.
+
+    Пропуски заполнены предыдущими значениями.
+
+    :param tickers:
+        Тикеры, для которых нужна информация.
+    :param last_date:
+        Последняя дата цен закрытия.
+    :return:
+        Цены закрытия.
+    """
+    quotes_list = crop.quotes(tickers)
+    df = pd.concat(
+        [df[col.CLOSE] for df in quotes_list],
+        axis=1,
+    )
+    df = df.loc[:last_date]
+    df.columns = tickers
+    return df.replace(to_replace=[np.nan, 0], method="ffill")
