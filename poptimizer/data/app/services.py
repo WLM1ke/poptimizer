4730441849856c176f5eb9c58a3595837a0801d4
WLM1ke <wlmike@gmail.com>
2020-09-04 10:18:31 +0300

    feat: добавлена обработка событий в несколько потоков

--- poptimizer/data/app/services.py
+++ poptimizer/data/app/services.py
@@ -1,8 +1,11 @@
 """Группа операций с таблицами, в конце которой осуществляется сохранение изменных данных."""
-from typing import Callable
+import functools
+import itertools
+from typing import Callable, List
 
 import pandas as pd
 
+from poptimizer.data.config import resources
 from poptimizer.data.domain import factories, model, repo
 from poptimizer.data.ports import base, outer
 
@@ -17,6 +20,15 @@ def _load_or_create_table(
     return table
 
 
+def _handle_one_event(event: outer.AbstractEvent, store: repo.Repo) -> List[outer.AbstractEvent]:
+    """Обрабатывает одно событие и возвращает  дочерние."""
+    tables_dict = {
+        table_name: _load_or_create_table(table_name, store) for table_name in event.tables_required
+    }
+    event.handle_event(tables_dict)
+    return event.new_events
+
+
 class EventsBus(outer.AbstractEventsBus):
     """Шина для обработки сообщений."""
 
@@ -24,19 +36,21 @@ class EventsBus(outer.AbstractEventsBus):
         """Сохраняет параметры для создания изолированных репо для каждой обработки события."""
         self._repo_factory = repo_factory
 
-    def handle_event(self, message: outer.AbstractEvent) -> None:
-        """Обработка сообщения и следующих за ним."""
-        messages = [message]
-        repo_factory = self._repo_factory
-        while messages:
-            message = messages.pop()
-            with repo_factory() as store:
-                tables_dict = {
-                    table_name: _load_or_create_table(table_name, store)
-                    for table_name in message.tables_required
-                }
-                message.handle_event(tables_dict)
-            messages.extend(message.new_events)
+    def handle_event(self, event: outer.AbstractEvent) -> None:
+        """Обработка сообщения и следующих за ним.
+
+        Обработка сообщений идет поколениями - первое поколение генерирует поколение сообщений
+        потомков и т.д. В рамках поколения обработка сообщений идет в несколько потоков с одной
+        версией репо, что обеспечивает согласованность данных во всех потоках. После обработки
+        поколения изменения сохраняются и создается новая версии репо для следующего поколения.
+        """
+        events = [event]
+        thread_pool = resources.get_thread_pool()
+        while events:
+            with self._repo_factory() as store:
+                one_event_handler = functools.partial(_handle_one_event, store=store)
+                future_events = thread_pool.map(one_event_handler, events)
+            events = list(itertools.chain.from_iterable(future_events))
 
 
 class Viewer(outer.AbstractViewer):
@@ -44,11 +58,11 @@ class Viewer(outer.AbstractViewer):
 
     def __init__(self, repo_factory: Callable[[], repo.Repo]) -> None:
         """Сохраняет репо для просмотра данных."""
-        self._repo = repo_factory()
+        self._repo_factory = repo_factory
 
     def get_df(self, table_name: base.TableName) -> pd.DataFrame:
         """Возвращает DataFrame по имени таблицы."""
-        with self._repo as store:
+        with self._repo_factory() as store:
             if (table := store.get_table(table_name)) is None:
                 raise base.DataError(f"Таблицы {table_name} нет в хранилище")
             return table.df
--- poptimizer/data/config/resources.py
+++ poptimizer/data/config/resources.py
@@ -1,4 +1,6 @@
 """Конфигурация внешних ресурсов приложения."""
+from concurrent import futures
+
 import pymongo
 import requests
 from requests import adapters
@@ -20,7 +22,7 @@ HTTP_SESSION = start_http_session()
 
 
 def get_http_session() -> requests.Session:
-    """Возвращает сессию для http соединений."""
+    """Сессия  http-соединений."""
     return HTTP_SESSION
 
 
@@ -32,3 +34,12 @@ CLIENT = pymongo.MongoClient("localhost", PORT, tz_aware=False)
 def get_mongo_client() -> pymongo.MongoClient:
     """Клиентское соединение с MongoDB."""
     return CLIENT
+
+
+# Настройки пула потоков - количество потоков равно размеру пула http-соединений
+THREAD_POOL = futures.ThreadPoolExecutor(max_workers=MAX_POOL_SIZE)
+
+
+def get_thread_pool() -> futures.ThreadPoolExecutor:
+    """Пул потоков."""
+    return THREAD_POOL
