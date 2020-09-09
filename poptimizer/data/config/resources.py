94296ed548e7b5d8dce54322d5de2076983fa522
WLM1ke <wlmike@gmail.com>
2020-09-09 13:32:48 +0300

    refactor: удалена синхронная сессия

--- poptimizer/data/config/resources.py
+++ poptimizer/data/config/resources.py
@@ -3,29 +3,7 @@ import asyncio
 import atexit
 
 import aiohttp
-import requests
 from motor import motor_asyncio
-from requests import adapters
-
-# Настройки http-соединения
-MAX_POOL_SIZE = 20
-MAX_RETRIES = 3
-
-
-def start_http_session() -> requests.Session:
-    """Открытие пула соединений с интернетом."""
-    session = requests.Session()
-    adapter = adapters.HTTPAdapter(pool_maxsize=MAX_POOL_SIZE, max_retries=MAX_RETRIES, pool_block=True)
-    session.mount("https://", adapter)
-    return session
-
-
-HTTP_SESSION = start_http_session()
-
-
-def get_http_session() -> requests.Session:
-    """Сессия  http-соединений."""
-    return HTTP_SESSION
 
 
 # Пул с асинхронными http-соединениями
