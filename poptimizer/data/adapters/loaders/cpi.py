e758169f529cb283c41f93c25e12ea81f63c8b9b
WLM1ke <wlmike@gmail.com>
2020-09-09 10:24:27 +0300

    fix: обработка изменения в названии файла

--- poptimizer/data/adapters/loaders/cpi.py
+++ poptimizer/data/adapters/loaders/cpi.py
@@ -1,4 +1,7 @@
 """Загрузка данных по потребительской инфляции."""
+import re
+
+import aiohttp
 import pandas as pd
 
 from poptimizer.data.adapters import logger
@@ -6,7 +9,9 @@ from poptimizer.data.config import resources
 from poptimizer.data.ports import base, col
 
 # Параметры загрузки валидации данных
-URL_CPI = "https://rosstat.gov.ru/storage/mediabank/chSxGUk7/i_ipc.xlsx"
+URL_CORE = "https://rosstat.gov.ru/storage/mediabank/pcRcsWuc/"
+URL_END = "Индексы%20потребительских%20цен%20по%20Российской%20Федерации.html"
+FILE_PATTERN = re.compile("https://rosstat.gov.ru/storage/mediabank/[a-zA-Z0-9]+/i_ipc.xlsx")
 END_OF_JAN = 31
 PARSING_PARAMETERS = dict(sheet_name="ИПЦ", header=3, skiprows=[4], skipfooter=3, index_col=0)
 NUM_OF_MONTH = 12
@@ -14,10 +19,20 @@ FIRST_YEAR = 1991
 FIRST_MONTH = "январь"
 
 
+async def _get_xlsx_url(session: aiohttp.ClientSession) -> str:
+    """Получить url для файла с инфляцией."""
+    resp = await session.get(URL_CORE + URL_END)
+    html = await resp.text()
+    if match := re.search(FILE_PATTERN, html):
+        return match.group(0)
+    raise base.DataError("На странице отсутствует URL файла с инфляцией")
+
+
 async def _load_xlsx() -> pd.DataFrame:
     """Загрузка Excel-файла с данными по инфляции."""
     session = resources.get_aiohttp_session()
-    resp = await session.get(URL_CPI)
+    file_url = await _get_xlsx_url(session)
+    resp = await session.get(file_url)
     xls_file = await resp.read()
     return pd.read_excel(xls_file, **PARSING_PARAMETERS)
 
