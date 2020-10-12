51b937d485a61bb4d9d2e97a5f16d5127305147b
WLM1ke <wlmike@gmail.com>
2020-10-12 10:20:51 +0300

    test: добавлены тесты для сборки всех дивидендов

--- poptimizer/data/views/tests/test_crop.py
+++ poptimizer/data/views/tests/test_crop.py
@@ -4,6 +4,7 @@ import datetime
 import pandas as pd
 import pytest
 
+from poptimizer import config
 from poptimizer.data.config import bootstrap
 from poptimizer.data.ports import col
 from poptimizer.data.views import crop
@@ -56,6 +57,31 @@ def test_dividends(ticker, div_data):
 
     date, div = div_data
     assert df.loc[date, ticker] == div
+
+
+DIVIDENDS_CASES = (
+    ("2018-06-19", "CHMF", 38.32 + 27.72),
+    ("2017-12-05", "CHMF", 35.61),
+    ("2018-07-17", "GMKN", 607.98),
+    ("2017-10-19", "GMKN", 224.2),
+    ("2018-06-19", "GMKN", 0),
+    ("2018-07-17", "CHMF", 0),
+)
+
+
+@pytest.mark.parametrize("date, ticker, div", DIVIDENDS_CASES)
+def test_dividends_all(date, ticker, div):
+    """Тесты на размер результата и для выборочных значений и заполнение пропусков."""
+    df = crop.dividends_all(("CHMF", "GMKN"))
+
+    assert len(df) > 30
+    assert list(df.columns) == ["CHMF", "GMKN"]
+
+    assert df.index[0] == pd.Timestamp("2015-05-25")
+    assert df.index[-1] >= pd.Timestamp("2020-09-08")
+
+    div_after_tax = div * config.AFTER_TAX
+    assert df.loc[date, ticker] == pytest.approx(div_after_tax)
 
 
 CPI_CASES = (
