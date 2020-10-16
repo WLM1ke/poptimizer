0b48a5e47424dfcdb1d2da50855523d2b8805649
WLM1ke <wlmike@gmail.com>
2020-10-16 16:49:45 +0300

    style(mypy): добавлена аннотация типов

--- poptimizer/data/conftest.py
+++ poptimizer/data/conftest.py
@@ -1,10 +1,11 @@
 import asyncio
+from typing import Iterator
 
 import pytest
 
 
 @pytest.fixture(scope="session")
-def event_loop():
+def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
