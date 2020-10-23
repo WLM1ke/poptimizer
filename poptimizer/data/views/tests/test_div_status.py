8cc563ec537896a0c971210e5716049b6c92bd8a
WLM1ke <wlmike@gmail.com>
2020-10-23 16:10:03 +0300

    test: дополнен и исправлены тесты под новое сравнение дивидендов

--- poptimizer/data/views/tests/test_div_status.py
+++ poptimizer/data/views/tests/test_div_status.py
@@ -6,9 +6,9 @@ from poptimizer.data.views import div_status
 from poptimizer.data.views.crop import div
 
 
-def test_smart_lab():
+def test_smart_lab_all():
     """Проверка типа и структуры результата."""
-    df = div_status.smart_lab()
+    df = div_status._smart_lab_all()
 
     assert isinstance(df, pd.DataFrame)
     assert df.columns.tolist() == [col.DATE, col.DIVIDENDS]
@@ -31,7 +31,7 @@ PLZL_DF = pd.DataFrame([4], columns=["PLZL"], index=["2020-10-02"])
 
 def test_new_on_smart_lab(mocker, capsys):
     """Различные варианты включения и не включения в статус."""
-    mocker.patch.object(div_status, "smart_lab", return_value=SMART_LAB_DF)
+    mocker.patch.object(div_status, "_smart_lab_all", return_value=SMART_LAB_DF)
     mocker.patch.object(div, "dividends", side_effect=[PLZL_DF, KZOS_DF, TTLK_DF])
 
     assert div_status.new_on_smart_lab(("TTLK", "KZOS", "PLZL")) == ["PLZL", "KZOS"]
@@ -48,46 +48,49 @@ def test_new_on_smart_lab_no_output(mocker, capsys):
     assert not captured.out
 
 
+def test_smart_lab(mocker):
+    """Проверка типа и значения результата."""
+    mocker.patch.object(div_status, "_smart_lab_all", return_value=SMART_LAB_DF)
+
+    df = div_status.smart_lab("TTLK")
+
+    assert isinstance(df, pd.DataFrame)
+    pd.testing.assert_frame_equal(df, TTLK_DF, check_names=False)
+
+
 def test_compare(capsys):
     """Сравнение и распечатка результатов."""
     df = div_status._compare(
-        "test_name",
-        pd.DataFrame([1, 2]),
-        pd.DataFrame([1, 3]),
+        pd.DataFrame([1, 2], columns=["a"]),
+        pd.DataFrame([1, 3], columns=["b"]),
     )
 
     pd.testing.assert_frame_equal(
         df,
         pd.DataFrame(
             [[1, 1, ""], [2, 3, "ERROR"]],
-            columns=["LOCAL", "SOURCE", "STATUS"],
+            columns=["a", "b", "STATUS"],
         ),
     )
-    captured = capsys.readouterr()
-    assert "test_name" in captured.out
 
 
 def test_compare_all_empty(capsys):
     """Регрессионный тест на ошибку в сравнении пустых DataFrame."""
     df = div_status._compare(
-        "all_empty",
         pd.DataFrame(columns=[1]),
         pd.DataFrame(columns=[2]),
     )
 
     pd.testing.assert_frame_equal(
         df,
-        pd.DataFrame(columns=["LOCAL", "SOURCE", "STATUS"]),
+        pd.DataFrame(columns=[1, 2, "STATUS"]),
     )
-    captured = capsys.readouterr()
-    assert "all_empty" in captured.out
 
 
 def test_compare_with_empty(capsys):
     """Регрессионный тест на ошибку в сравнении с пустым DataFrame."""
     df = div_status._compare(
-        "with_empty",
-        pd.DataFrame([1, 2]),
+        pd.DataFrame([1, 2], columns=[3]),
         pd.DataFrame(columns=[1]),
     )
 
@@ -95,17 +98,16 @@ def test_compare_with_empty(capsys):
         df,
         pd.DataFrame(
             [[1, None, "ERROR"], [2, None, "ERROR"]],
-            columns=["LOCAL", "SOURCE", "STATUS"],
+            columns=[3, 1, "STATUS"],
             index=[0, 1],
         ),
         check_index_type=False,
     )
-    captured = capsys.readouterr()
-    assert "with_empty" in captured.out
 
 
 def test_dividends_validation(mocker):
     """Проверка количества запросов необходимой информации."""
+    mocker.patch.object(div_status, "pd")
     fake_dividends = mocker.patch.object(div, "dividends")
     fake_compare = mocker.patch.object(div_status, "_compare")
     fake_sources = [
@@ -114,11 +116,13 @@ def test_dividends_validation(mocker):
         mocker.patch.object(div, "bcs"),
         mocker.patch.object(div_status, "smart_lab"),
     ]
+    for source in fake_sources:
+        source.__name__ = ""  # noqa: WPS125
 
-    div_status.dividends_validation("TEST")
+    div_status.dividends_validation("TEST", fake_sources)
 
     assert fake_dividends.call_count == 1
-    assert fake_compare.call_count == len(fake_sources)
+    assert fake_compare.call_count == 1
 
-    for source in fake_sources:
-        assert source.call_count == 1
+    for test_source in fake_sources:
+        assert test_source.call_count == 1
