a9351a7be6d20e258cd96a8198aac892470cdf7b
WLM1ke <wlmike@gmail.com>
2020-10-25 19:19:35 +0300

    feat: базовый класс таблицы с дженериком

--- poptimizer/data_di/ports/entity.py
+++ poptimizer/data_di/ports/entity.py
@@ -1,7 +1,8 @@
 """Доменные сущности."""
 import abc
+import asyncio
 from datetime import datetime
-from typing import Iterator, List, Optional, TypeVar
+from typing import Generic, Iterator, List, Optional, TypeVar
 
 import pandas as pd
 
@@ -63,10 +64,6 @@ class AbstractEntity(abc.ABC):
         while self._events:
             yield self._events.pop()
 
-    def _record_event(self, event: events.AbstractEvent) -> None:
-        """Добавляет новое событие."""
-        self._events.append(event)
-
 
 class TableID(BaseID):
     """Идентификатор таблиц."""
@@ -76,7 +73,10 @@ class TableID(BaseID):
         super().__init__(DB, group, name)
 
 
-class BaseTable(AbstractEntity):
+Event = TypeVar("Event", bound=events.AbstractEvent)
+
+
+class BaseTable(Generic[Event], AbstractEntity):
     """Базовая таблица.
 
     Хранит время последнего обновления и DataFrame.
@@ -92,6 +92,7 @@ class BaseTable(AbstractEntity):
         super().__init__(id_)
         self._df = df
         self._timestamp = timestamp
+        self._df_lock = asyncio.Lock()
 
     @property
     def df(self) -> Optional[pd.DataFrame]:
@@ -100,12 +101,31 @@ class BaseTable(AbstractEntity):
             return None
         return df.copy()
 
-    @property
-    def timestamp(self) -> Optional[datetime]:
-        """Момент последнего обновления таблицы."""
-        return self._timestamp
+    async def handle_event(self, event: Event) -> None:
+        """Обновляет значение, изменяет текущую дату и добавляет связанные с этим события."""
+        async with self._df_lock:
+            if self._update_cond(event):
+                df_new = await self._prepare_df(event)
 
-    def _update(self, df_new: pd.DataFrame) -> None:
-        """Обновляет значение и изменяет текущую дату."""
-        self._timestamp = datetime.utcnow()
-        self._df = df_new
+                self._validate_data_and_index(df_new)
+
+                self._timestamp = datetime.utcnow()
+                self._df = df_new
+                self._events.extend(self._new_events())
+
+    @abc.abstractmethod
+    def _update_cond(self, event: Event) -> bool:
+        """Условие обновления."""
+
+    @abc.abstractmethod
+    async def _prepare_df(self, event: Event) -> pd.DataFrame:
+        """Новое значение DataFrame."""
+
+    @abc.abstractmethod
+    def _validate_data_and_index(self, df_new: pd.DataFrame) -> None:
+        """Проверка корректности новых данных в сравнении со старыми."""
+
+    @abc.abstractmethod
+    def _new_events(self) -> List[events.AbstractEvent]:
+        """События, которые нужно создать по результатам обновления."""
+        return []
