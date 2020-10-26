9f0804faf9aa09b3d1fdbf90a9ba3d64b0b9719f
WLM1ke <wlmike@gmail.com>
2020-10-26 09:31:28 +0300

    feat: интерфейс гейтвея для загрузки данных

---
+++ poptimizer/data_di/domain/ports.py
@@ -0,0 +1,12 @@
+"""Внешние интерфейсы, используемые доменной моделью."""
+import abc
+
+import pandas as pd
+
+
+class AbstractGateway(abc.ABC):
+    """Загружает данные."""
+
+    @abc.abstractmethod
+    async def get(self) -> pd.DataFrame:
+        """Загружает данные."""
