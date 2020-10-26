81a1282a4c62b00c9f05e4cb8a12c839cd8c541a
WLM1ke <wlmike@gmail.com>
2020-10-26 12:56:31 +0300

    style(flake8): добавлены докстринги

--- poptimizer/data_di/adapters/adapters.py
+++ poptimizer/data_di/adapters/adapters.py
@@ -1,16 +1,19 @@
 """Конфигурация внешней инфраструктуры."""
+from typing import Final
+
 import aiohttp
 import injector
 
 from poptimizer.data.adapters import logger
 from poptimizer.data_di.adapters import http
 
-POOL_SIZE = 20
+POOL_SIZE: Final = 20
 
 
 class Adapters(injector.Module):
     """Конфигурация внешней инфраструктуры."""
 
     def configure(self, binder: injector.Binder) -> None:
+        """Построение конфигурация внешней инфраструктуры."""
         binder.bind(aiohttp.ClientSession, to=http.http_session_factory(POOL_SIZE))
         binder.bind(logger.AsyncLogger, to=logger.AsyncLogger("GateWays"))
