3449baeb451ba641c4fea62d9abca13284508eb8
WLM1ke <wlmike@gmail.com>
2020-10-26 12:15:08 +0300

    feat: http-сессия и ее закрытие

---
+++ poptimizer/data_di/adapters/http.py
@@ -0,0 +1,19 @@
+"""Фабрика для асинхронного http-соединения."""
+import asyncio
+import atexit
+
+import aiohttp
+
+
+def _clean_up(session: aiohttp.ClientSession) -> None:
+    """Закрывает клиентскую сессию aiohttp."""
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(session.close())
+
+
+def http_session_factory(pool_size: int) -> aiohttp.ClientSession:
+    """Клиентская сессия aiohttp."""
+    connector = aiohttp.TCPConnector(limit=pool_size)
+    session = aiohttp.ClientSession(connector=connector)
+    atexit.register(_clean_up, session)
+    return session
