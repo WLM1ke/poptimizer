19566a518461508f67e0d68aa64e2a55af9c9203
WLM1ke <wlmike@gmail.com>
2020-10-28 09:39:24 +0300

    refactor: изменены типы в фабрике

--- poptimizer/data_di/domain/factories.py
+++ poptimizer/data_di/domain/factories.py
@@ -6,9 +6,8 @@ import pandas as pd
 from injector import Inject
 
 from poptimizer.data_di.adapters.gateways.trading_dates import TradingDatesGateway
-from poptimizer.data_di.ports import tables
 from poptimizer.data_di.domain.trading_dates import TradingDates
-from poptimizer.data_di.shared import entity
+from poptimizer.data_di.ports import tables
 
 
 class TablesFactory:
@@ -19,17 +18,13 @@ class TablesFactory:
         trading_dates_gateway: Inject[TradingDatesGateway],
     ) -> None:
         """Создает mapping между наименованием группы и настройками для создания таблицы."""
-        self._table_types_mapping = {
-            "trading_dates": (TradingDates, trading_dates_gateway),
-        }
+        self._gateway = trading_dates_gateway
 
     def create_table(
         self,
-        table_id: entity.ID,
+        table_id: tables.TableID,
         df: Optional[pd.DataFrame] = None,
         timestamp: Optional[datetime] = None,
-    ) -> tables.AbstractTable:
+    ) -> TradingDates:
         """Создает таблицу на основе данных и обновляет ее."""
-        group = table_id.group
-        table_type, *gateways = self._table_types_mapping[group]
-        return table_type(table_id, df, timestamp, *gateways)
+        return TradingDates(table_id, df, timestamp, self._gateway)
