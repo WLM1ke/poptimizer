dc05f14df0ab8f8c341b56e368733e99b3ec60a3
WLM1ke <wlmike@gmail.com>
2020-10-28 13:33:38 +0300

    refactor: скорректирована инъекция типов с учетом общего модуля

--- poptimizer/data_di/adapters/adapters.py
+++ poptimizer/data_di/adapters/adapters.py
@@ -3,9 +3,11 @@ from typing import Final, Type
 
 import aiohttp
 import injector
+import pandas as pd
 from motor import motor_asyncio
 
-from poptimizer.data_di.adapters import connection, logger, mapper
+from poptimizer.data_di.adapters import connection
+from poptimizer.data_di.shared import logger, mapper
 
 # Размер пула http-соединений - при большем размере многие сайты ругаются
 POOL_SIZE: Final = 20
@@ -14,19 +16,43 @@ POOL_SIZE: Final = 20
 MONGO_URI: Final = "mongodb://localhost:27017"
 
 
+# Описание мэппинга данных в MongoDB
+DATA_MAPPING: Final = (
+    mapper.Desc(
+        field_name="_df",
+        doc_name="data",
+        factory_name="df",
+        encoder=lambda df: df.to_dict("split"),
+        decoder=lambda doc_df: pd.DataFrame(**doc_df),
+    ),
+    mapper.Desc(
+        field_name="_timestamp",
+        doc_name="timestamp",
+        factory_name="timestamp",
+    ),
+)
+
+
 class Adapters(injector.Module):
     """Конфигурация внешней инфраструктуры."""
-
-    def configure(self, binder: injector.Binder) -> None:
-        """Построение конфигурация внешней инфраструктуры."""
-        binder.bind(aiohttp.ClientSession, to=connection.session_factory(POOL_SIZE))
-        binder.bind(
-            motor_asyncio.AsyncIOMotorClient,
-            to=motor_asyncio.AsyncIOMotorClient(MONGO_URI, tz_aware=False),
-        )
-        binder.bind(mapper.Mapper, to=mapper.Mapper(mapper.DATA_MAPPING))
 
     @injector.provider
     def logger_type(self) -> Type[logger.AsyncLogger]:
         """Предоставляет класс асинхронного логгера."""
         return logger.AsyncLogger
+
+    @injector.provider
+    @injector.singleton
+    def http_session(self) -> aiohttp.ClientSession:
+        """Предоставляет единственную http-сессию."""
+        return connection.session_factory(POOL_SIZE)
+
+    @injector.provider
+    @injector.singleton
+    def db_session(self, logger_type: Type[logger.AsyncLogger]) -> mapper.MongoDBSession:
+        """Предоставляет единственную сессию MongoDB."""
+        return mapper.MongoDBSession(
+            motor_asyncio.AsyncIOMotorClient(MONGO_URI, tz_aware=False),
+            mapper.Mapper(DATA_MAPPING),
+            logger_type,
+        )
