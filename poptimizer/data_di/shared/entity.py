7dec81bec8599452b1785b8de7d3d7fa2df5ec18
WLM1ke <wlmike@gmail.com>
2020-10-29 10:54:16 +0300

    refactor: возврат новых событий перемещен в в обработчик

--- poptimizer/data_di/shared/entity.py
+++ poptimizer/data_di/shared/entity.py
@@ -16,8 +16,8 @@ class AbstractHandler(Generic[Event], abc.ABC):
     """Абстрактный тип обработчика событий."""
 
     @abc.abstractmethod
-    async def handle_event(self, event: Event) -> None:
-        """Обрабатывает событие."""
+    async def handle_event(self, event: Event) -> List[AbstractEvent]:
+        """Обрабатывает событие и возвращает список новых порожденных событий."""
 
 
 @dataclasses.dataclass(frozen=True)
@@ -35,14 +35,12 @@ StateDict = Dict[str, Any]  # type: ignore
 class BaseEntity:
     """Абстрактный класс сущности.
 
-    Обязательно имеет поле и идентификатором, механизм сохранения и извлечения событий и
-    автоматического статуса изменений.
+    Обязательно имеет поле с идентификатором и автоматическим отслеживанием статуса изменений.
     """
 
     def __init__(self, id_: ID) -> None:
         """Формирует список событий и отметку об изменениях."""
         self._id = id_
-        self._events: List[AbstractEvent] = []
         self._changed_state: StateDict = {}
 
     def __setattr__(self, key: str, attr_value: Any) -> None:  # type: ignore
@@ -63,8 +61,3 @@ class BaseEntity:
     def clear(self) -> None:
         """Сбрасывает изменения."""
         self._changed_state.clear()
-
-    def pop_event(self) -> Iterator[AbstractEvent]:
-        """Возвращает события возникшие в за время существования сущности."""
-        while self._events:
-            yield self._events.pop()
