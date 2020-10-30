f1c89867f9708eb633626d5b3867534a12a4df71
WLM1ke <wlmike@gmail.com>
2020-10-30 16:34:21 +0300

    refactor: уточнены импорты и нотации типов

--- poptimizer/data_di/domain/handlers.py
+++ poptimizer/data_di/domain/handlers.py
@@ -1,21 +1,19 @@
 """Обработчики доменных событий."""
 from typing import List
 
-from injector import Inject
-
-from poptimizer.data_di.domain import events, repos
+from poptimizer.data_di.domain import events, repos, tables
 from poptimizer.data_di.shared import entities
 
 
 class AppStartedHandler(entities.AbstractHandler[events.AppStarted]):
     """Запускает проверку окончания торгового дня."""
 
-    def __init__(self, repo: Inject[repos.Repo]):
+    def __init__(self, repo: repos.Repo[events.AppStarted]):
         """Сохраняет репо."""
         self._repo = repo
 
     async def handle_event(self, event: events.AppStarted) -> List[entities.AbstractEvent]:
         """Обновляет таблицу с торговыми днями."""
-        table_id = repos.create_id("trading_date")
+        table_id = tables.create_id("trading_date")
         table = await self._repo.get_table(table_id)
         return await table.handle_event(event)
