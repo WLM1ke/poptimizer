6ff9740205f02e4d1fbe9c46a411f6fa1bce6bf0
WLM1ke <wlmike@gmail.com>
2020-10-31 18:19:46 +0300

    feat: логгер дескриптор

--- poptimizer/data_di/shared/logger.py
+++ poptimizer/data_di/shared/logger.py
@@ -1,14 +1,29 @@
 """Асинхронное логирование."""
 import asyncio
 import logging
+from typing import Generic, Type, TypeVar
+
+ObjectType = TypeVar("ObjectType")
 
 
-class AsyncLogger:
-    """Асинхронное логирование в отдельном потоке."""
+class AsyncLogger(Generic[ObjectType]):
+    """Асинхронное логирование в отдельном потоке.
 
-    def __init__(self, name: str) -> None:
+    Поддерживает протокол дескриптора для автоматического определения имени класса, в котором он
+    является атрибутом.
+    """
+
+    def __init__(self) -> None:
         """Инициализация логгера."""
-        self._logger = logging.getLogger(name)
+        self._logger: logging.Logger = logging.getLogger()
+
+    def __set_name__(self, owner: Type[ObjectType], name: str) -> None:
+        """Создает логгер с именем класса, где он является атрибутом."""
+        self._logger = logging.getLogger(owner.__name__)
+
+    def __get__(self, instance: ObjectType, owner: Type[ObjectType]) -> "AsyncLogger[ObjectType]":
+        """Возвращает себя при обращении к атрибуту."""
+        return self
 
     def log(self, message: str) -> None:
         """Создает асинхронную задачу по логгированию."""
