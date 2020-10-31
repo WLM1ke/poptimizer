65f57dfca9fb4742ceb0f7f2b52441c4e10aa1c7
WLM1ke <wlmike@gmail.com>
2020-10-31 09:28:22 +0300

    refactor: универсальный обработчик для всех событий - singledispatch

--- poptimizer/data_di/domain/handlers.py
+++ poptimizer/data_di/domain/handlers.py
@@ -1,18 +1,30 @@
 """Обработчики доменных событий."""
+import functools
 from typing import List
 
+from poptimizer import config
 from poptimizer.data_di.domain import events, repos, tables
 from poptimizer.data_di.shared import entities
 
 
-class AppStartedHandler(entities.AbstractHandler[events.AppStarted]):
-    """Запускает проверку окончания торгового дня."""
+class UnknownEventError(config.POptimizerError):
+    """Для события не зарегистрирован обработчик."""
 
-    def __init__(self, repo: repos.Repo[events.AppStarted]):
+
+class EventHandlersDispatcher(entities.AbstractHandler[entities.AbstractEvent]):
+    """Обеспечивает запуск обработчика в соответствии с типом события."""
+
+    def __init__(self, repo: repos.Repo):
         """Сохраняет репо."""
         self._repo = repo
 
-    async def handle_event(self, event: events.AppStarted) -> List[entities.AbstractEvent]:
+    @functools.singledispatchmethod
+    async def handle_event(self, event: entities.AbstractEvent) -> List[entities.AbstractEvent]:
+        """Обработчик для отсутствующих событий."""
+        raise UnknownEventError(event)
+
+    @handle_event.register
+    async def _(self, event: events.AppStarted) -> List[entities.AbstractEvent]:
         """Обновляет таблицу с торговыми днями."""
         table_id = tables.create_id("trading_date")
         table = await self._repo.get_table(table_id)
