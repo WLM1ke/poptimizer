b8134bf1cfe6dd168faf778f36213db8d1d5f737
WLM1ke <wlmike@gmail.com>
2020-11-12 15:10:39 +0300

    feat: загрузка данных с dohod.ru

---
+++ poptimizer/data_di/adapters/gateways/dohod.py
@@ -0,0 +1,45 @@
+"""Обновление данных с https://dohod.ru."""
+import pandas as pd
+
+from poptimizer.data_di.adapters.html import description, parser
+from poptimizer.data_di.shared import adapters, col
+
+# Параметры парсинга сайта
+URL = "https://www.dohod.ru/ik/analytics/dividend/"
+TABLE_INDEX = 2
+
+
+def get_col_desc(ticker: str) -> parser.Descriptions:
+    """Формирует список с описанием нужных столбцов."""
+    date_col = description.ColDesc(
+        num=0,
+        raw_name=("Дата закрытия реестра",),
+        name=col.DATE,
+        parser_func=description.date_parser,
+    )
+    div_col = description.ColDesc(
+        num=2,
+        raw_name=("Дивиденд (руб.)",),
+        name=ticker,
+        parser_func=description.div_parser,
+    )
+    return [date_col, div_col]
+
+
+class DohodGateway:
+    """Обновление данных с https://dohod.ru."""
+
+    _logger = adapters.AsyncLogger()
+
+    async def get(self, ticker: str) -> pd.DataFrame:
+        """Получение дивидендов для заданного тикера."""
+        self._logger(ticker)
+
+        cols_desc = get_col_desc(ticker)
+        url = f"{URL}{ticker.lower()}"
+        try:
+            df = await parser.get_df_from_url(url, TABLE_INDEX, cols_desc)
+        except description.ParserError:
+            return pd.DataFrame(columns=[ticker])
+        df = df.sort_index(axis=0)
+        return df.groupby(lambda date: date).sum()
