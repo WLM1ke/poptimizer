d9278ccf5f853c60264d93bdafb8394c5919a3f4
WLM1ke <wlmike@gmail.com>
2020-11-17 13:20:18 +0300

    refactor: данные из нового модуля

--- poptimizer/data/views/crop/not_div.py
+++ poptimizer/data/views/crop/not_div.py
@@ -4,40 +4,32 @@ from typing import List, Tuple
 
 import pandas as pd
 
-from poptimizer.data.config import bootstrap
-from poptimizer.data.ports import outer
+from poptimizer.data_di.app import bootstrap, viewers
+from poptimizer.data_di.domain.tables import base
 from poptimizer.data_di.shared import col
 
 
-def cpi() -> pd.Series:
+def cpi(viewer: viewers.Viewer = bootstrap.VIEWER) -> pd.Series:
     """Потребительская инфляция."""
-    table_name = outer.TableName(outer.CPI, outer.CPI)
-    requests_handler = bootstrap.get_handler()
-    df = requests_handler.get_df(table_name)
-    start_date = bootstrap.get_start_date()
-    return df.loc[start_date:, col.CPI]  # type: ignore
+    df = viewer.get_df(base.CPI, base.CPI)
+    return df.loc[bootstrap.START_DATE :, col.CPI]
 
 
-def index(ticker: str = "MCFTRR") -> pd.Series:
+def index(
+    ticker: str = "MCFTRR",
+    viewer: viewers.Viewer = bootstrap.VIEWER,
+) -> pd.Series:
     """Загрузка данных по индексу полной доходности с учетом российских налогов - MCFTRR."""
-    table_name = outer.TableName(outer.INDEX, ticker)
-    requests_handler = bootstrap.get_handler()
-    df = requests_handler.get_df(table_name)
-    start_date = bootstrap.get_start_date()
-    return df.loc[start_date:, col.CLOSE]  # type: ignore
+    df = viewer.get_df(base.INDEX, ticker)
+    return df.loc[bootstrap.START_DATE :, col.CLOSE]
 
 
 @functools.lru_cache(maxsize=1)
-def quotes(tickers: Tuple[str, ...]) -> List[pd.DataFrame]:
-    """Информация о котировках для заданных тикеров.
-
-    :param tickers:
-        Перечень тикеров, для которых нужна информация.
-    :return:
-        Список с котировками.
-    """
-    group: outer.GroupName = outer.QUOTES
-    requests_handler = bootstrap.get_handler()
-    dfs = requests_handler.get_dfs(group, tickers)
-    start_date = bootstrap.get_start_date()
-    return [df.loc[start_date:] for df in dfs]  # type: ignore
+def quotes(
+    tickers: Tuple[str, ...],
+    viewer: viewers.Viewer = bootstrap.VIEWER,
+) -> List[pd.DataFrame]:
+    """Информация о котировках для заданных тикеров."""
+    dfs = viewer.get_dfs(base.QUOTES, tickers)
+    start_date = bootstrap.START_DATE
+    return [df.loc[start_date:] for df in dfs]
