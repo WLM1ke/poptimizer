7c2ff4554c1b46f2fc8bb5e8d01a308357cd94fa
WLM1ke <wlmike@gmail.com>
2020-12-06 10:48:12 +0300

    refactor: перемещение тестов соединения

--- poptimizer/shared/tests/test_connections.py
+++ poptimizer/shared/tests/test_connections.py
@@ -1,19 +1,18 @@
-"""Тесты для создания http-соединения."""
+"""Тесты для общих соединений http и MongoDB."""
 import aiohttp
 
-import poptimizer.shared.connections
-from poptimizer.data_di.adapters.gateways import base
+from poptimizer.shared import connections
 
 
 def test_session_factory():
     """Проверка, что http-сессия является асинхронной."""
-    assert isinstance(poptimizer.shared.connections._session_factory(10), aiohttp.ClientSession)
+    assert isinstance(connections._session_factory(10), aiohttp.ClientSession)
 
 
 def test_clean_up(mocker):
     """Проверка закрытия http-сессии."""
     fake_session = mocker.AsyncMock()
 
-    poptimizer.shared.connections._clean_up(fake_session)
+    connections._clean_up(fake_session)
 
     fake_session.close.assert_called_once()
