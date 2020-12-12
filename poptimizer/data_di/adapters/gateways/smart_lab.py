ed2b19f8a00477ba4ff473a97a03025325899b1b
WLM1ke <wlmike@gmail.com>
2020-12-12 13:11:57 +0300

    refactor: заменен тип ошибки

--- poptimizer/data_di/adapters/gateways/smart_lab.py
+++ poptimizer/data_di/adapters/gateways/smart_lab.py
@@ -1,7 +1,6 @@
 """Загрузка данных со https://www.smart-lab.ru."""
 import pandas as pd
 
-from poptimizer import config
 from poptimizer.data_di.adapters.html import description, parser
 from poptimizer.shared import adapters, col
 
@@ -14,29 +13,14 @@ FOOTER = "+добавить дивиденды"
 
 def get_col_desc() -> parser.Descriptions:
     """Формирует список с описанием нужных столбцов."""
-    ticker = description.ColDesc(
-        num=1,
-        raw_name=("Тикер",),
-        name=col.TICKER,
-        parser_func=None,
-    )
+    ticker = description.ColDesc(num=1, raw_name=("Тикер",), name=col.TICKER, parser_func=None)
     date = description.ColDesc(
-        num=9,
-        raw_name=("дата отсечки",),
-        name=col.DATE,
-        parser_func=description.date_parser,
+        num=9, raw_name=("дата отсечки",), name=col.DATE, parser_func=description.date_parser,
     )
     div = description.ColDesc(
-        num=5,
-        raw_name=("дивиденд,руб",),
-        name=col.DIVIDENDS,
-        parser_func=description.div_parser,
+        num=5, raw_name=("дивиденд,руб",), name=col.DIVIDENDS, parser_func=description.div_parser,
     )
     return [ticker, date, div]
-
-
-class SmartLabGatewayError(config.POptimizerError):
-    """Ошибки при загрузке данных со https://www.smart-lab.ru."""
 
 
 class SmartLabGateway:
@@ -51,5 +35,5 @@ class SmartLabGateway:
         cols_desc = get_col_desc()
         df = await parser.get_df_from_url(URL, TABLE_INDEX, cols_desc)
         if FOOTER not in df.index[-1]:
-            raise SmartLabGatewayError("Некорректная html-таблица")
+            raise description.ParserError("Некорректная html-таблица")
         return df.dropna()
