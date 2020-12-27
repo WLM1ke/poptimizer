83227ec83c6fde539e4fc3ecf1e218ad7e9d8a78
WLM1ke <wlmike@gmail.com>
2020-12-27 11:29:40 +0300

    fix: tasks в разных event loops

---
+++ poptimizer/conftest.py
@@ -0,0 +1,13 @@
+"""Общий Event Loop для тестов."""
+import asyncio
+from typing import Iterator
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Iterator[asyncio.AbstractEventLoop]:
+    """Event loop для тестирования с помощью pytest."""
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
