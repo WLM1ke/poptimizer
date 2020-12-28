79066df77e39a335fb084546346127e8af9899fc
WLM1ke <wlmike@gmail.com>
2020-12-28 10:08:26 +0300

    fix: новая схема упорядочивания оптимальных

--- poptimizer/portfolio/tests/test_optimizer.py
+++ poptimizer/portfolio/tests/test_optimizer.py
@@ -39,7 +39,7 @@ def test_best_combination(opt, monkeypatch):
     df = opt.best_combination
 
     assert isinstance(df, pd.DataFrame)
-    assert df.shape == (3, 7)
+    assert df.shape == (5, 7)
     assert list(df.columns) == ["SELL", "Q_SELL", "BUY", "Q_BUY", "GRAD_DIFF", "TURNOVER", "P_VALUE"]
 
     wilcoxon = stats.wilcoxon([1] * 20, alternative="greater", correction=True)[1] * 17
@@ -48,11 +48,11 @@ def test_best_combination(opt, monkeypatch):
     assert df.loc[1, "BUY"] == "KZOS"
     assert df.loc[1, "P_VALUE"] == pytest.approx(wilcoxon)
 
-    assert df.loc[2, "SELL"] == "RTKMP"
-    assert df.loc[2, "BUY"] == "KZOS"
+    assert df.loc[2, "SELL"] == "MTSS"
+    assert df.loc[2, "BUY"] == "RTKMP"
     assert df.loc[2, "P_VALUE"] == pytest.approx(wilcoxon)
 
-    assert df.loc[3, "SELL"] == "TRCN"
+    assert df.loc[3, "SELL"] == "RTKMP"
     assert df.loc[3, "BUY"] == "KZOS"
     assert df.loc[3, "P_VALUE"] == pytest.approx(wilcoxon)
 
