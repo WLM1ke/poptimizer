d49380dccc11bf20bfe5c2706333676cccec78c5
WLM1ke <wlmike@gmail.com>
2021-01-15 11:19:33 +0300

    test: корректировка с учетом нового формата SmartLab

--- poptimizer/data/adapters/gateways/tests/test_smart_lab.py
+++ poptimizer/data/adapters/gateways/tests/test_smart_lab.py
@@ -3,13 +3,13 @@ import pandas as pd
 import pytest
 
 from poptimizer.data.adapters.gateways import smart_lab
-from poptimizer.data.adapters.html import description, parser
+from poptimizer.data.adapters.html import parser
 
 DF = pd.DataFrame(
-    [[4.0], [1.0], [2.0], [None]],
-    index=["2020-01-20", "2014-11-25", "2014-11-25", smart_lab.FOOTER],
+    [[4.0], [1.0], [None]],
+    index=["2020-01-20", "2014-11-25", "2014-11-25"],
 )
-DF_CASES = ((DF, DF.dropna()), (DF.dropna(), None))
+DF_CASES = ((DF, DF.dropna()),)
 
 
 @pytest.mark.parametrize("df_patch, df_res", DF_CASES)
@@ -18,9 +18,4 @@ async def test_conomy_gateway(mocker, df_patch, df_res):
     """Проверка нижних ячеек и отбрасывание предварительных данных."""
     mocker.patch.object(parser, "get_df_from_url", return_value=df_patch)
     loader = smart_lab.SmartLabGateway()
-
-    if df_res is None:
-        with pytest.raises(description.ParserError, match="Некорректная html-таблица"):
-            await loader.get()
-    else:
-        pd.testing.assert_frame_equal(await loader.get(), df_res)
+    pd.testing.assert_frame_equal(await loader.get(), df_res)
