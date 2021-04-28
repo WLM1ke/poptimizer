36c3101b014df0b7267bc7f775e8ecd02a6dc256
WLM1ke <wlmike@gmail.com>
2021-04-28 11:21:39 +0300

    feat: модификация под новую схему эволюции

--- poptimizer/evolve/population.py
+++ poptimizer/evolve/population.py
@@ -1,6 +1,6 @@
 """Класс организма и операции с популяцией организмов."""
 import time
-from typing import Iterable, Tuple, NoReturn, Optional
+from typing import Iterable, Optional
 
 import bson
 import numpy as np
@@ -8,9 +8,8 @@ import pandas as pd
 import pymongo
 
 from poptimizer.config import POptimizerError
-from poptimizer.dl import Model, Forecast
+from poptimizer.dl import Forecast, Model
 from poptimizer.evolve import store
-from poptimizer.evolve.chromosomes.chromosome import MUTATION_FACTOR
 from poptimizer.evolve.genotype import Genotype
 
 
@@ -30,33 +29,40 @@ class Organism:
         *,
         _id: Optional[bson.ObjectId] = None,
         genotype: Optional[Genotype] = None,
-    ):
-        self._data = store.Doc(id_=_id, genotype=genotype)
+    ) -> None:
+        """Загружает организм из базы данных."""
+        self._doc = store.Doc(id_=_id, genotype=genotype)
 
     def __str__(self) -> str:
-        return str(self._data.genotype)
+        """Текстовое представление генотипа организма."""
+        return str(self._doc.genotype)
 
     @property
     def id(self) -> bson.ObjectId:
         """ID организма."""
-        return self._data.id
+        return self._doc.id
 
     @property
     def genotype(self) -> Genotype:
         """Генотип организма."""
-        return self._data.genotype
+        return self._doc.genotype
 
     @property
     def timer(self) -> float:
         """Генотип организма."""
-        return self._data.timer
+        return self._doc.timer
 
     @property
     def wins(self) -> int:
         """Количество побед."""
-        return self._data.wins
+        return self._doc.wins
 
-    def evaluate_fitness(self, tickers: Tuple[str, ...], end: pd.Timestamp) -> float:
+    @property
+    def llh(self) -> float:
+        """Количество побед."""
+        return self._doc.llh
+
+    def evaluate_fitness(self, tickers: tuple[str, ...], end: pd.Timestamp) -> float:
         """Вычисляет качество организма.
 
         Если осуществлялась оценка для указанных тикеров и даты - используется сохраненное значение. Если
@@ -64,31 +70,22 @@ class Organism:
         В ином случае тренируется и оценивается с нуля.
         """
         tickers = list(tickers)
-        data = self._data
-        data.wins += 1
-
-        if data.date == end and data.tickers == tickers:
-            data.save()
-            return data.llh
-
-        pickled_model = data.model
-        if data.tickers != tickers:
-            pickled_model = None
+        doc = self._doc
+        doc.wins += 1
 
         timer = time.monotonic_ns()
-        model = Model(tuple(tickers), end, self.genotype.get_phenotype(), pickled_model)
+        model = Model(tuple(tickers), end, self.genotype.get_phenotype())
         llh = model.llh
         timer = time.monotonic_ns() - timer
 
-        data.llh = llh
-        data.model = bytes(model)
-        data.date = end
-        data.tickers = tickers
+        doc.llh = llh
+        doc.model = bytes(model)
+        doc.date = end
+        doc.tickers = tickers
 
-        if pickled_model is None:
-            data.timer = timer
+        doc.timer = timer
 
-        data.save()
+        doc.save()
         return llh
 
     def find_weaker(self) -> "Organism":
@@ -97,10 +94,10 @@ class Organism:
         В оборе участвуют только организмы с таким же набором тикеров и датой обновления. Может найти
         самого себя.
         """
-        data = self._data
+        doc = self._doc
         collection = store.get_collection()
 
-        filter_ = dict(timer={"$gte": data.timer}, tickers=data.tickers, date=data.date)
+        filter_ = {"timer": {"$gte": doc.timer}}
         id_dict = collection.find_one(
             filter=filter_,
             projection=["_id"],
@@ -110,7 +107,7 @@ class Organism:
         if self.id != org.id:
             return org
 
-        filter_ = dict(tickers=data.tickers, date=data.date)
+        filter_ = {}
         id_dict = collection.find_one(
             filter=filter_,
             projection=["_id"],
@@ -118,24 +115,24 @@ class Organism:
         )
         return Organism(**id_dict)
 
-    def die(self) -> NoReturn:
+    def die(self) -> None:
         """Организм удаляется из популяции."""
-        self._data.delete()
+        self._doc.delete()
 
-    def make_child(self, factor: float = MUTATION_FACTOR) -> "Organism":
+    def make_child(self, scale: float) -> "Organism":
         """Создает новый организм с помощью дифференциальной мутации."""
-        genotypes = [organism.genotype for organism in _sample_organism(3)]
-        child_genotype = self.genotype.make_child(*genotypes, factor)
+        parent, *_ = [organism.genotype for organism in _sample_organism(1)]
+        child_genotype = self.genotype.make_child(parent, scale)
         return Organism(genotype=child_genotype)
 
-    def forecast(self, tickers: Tuple[str, ...], end: pd.Timestamp) -> Forecast:
+    def forecast(self, tickers: tuple[str, ...], end: pd.Timestamp) -> Forecast:
         """Выдает прогноз для текущего организма.
 
-        При наличие натренированной модели, которая составлена на предыдущей статистике и для таких же
+        При наличии натренированной модели, которая составлена на предыдущей статистике и для таких же
         тикеров, будет использованы сохраненные веса сети, или выбрасывается исключение.
         """
-        data = self._data
-        if (pickled_model := data.model) is None or tickers != tuple(data.tickers):
+        doc = self._doc
+        if (pickled_model := doc.model) is None or tickers != tuple(doc.tickers):
             raise ForecastError
 
         model = Model(tickers, end, self.genotype.get_phenotype(), pickled_model)
@@ -145,9 +142,9 @@ class Organism:
             raise ForecastError
         return forecast
 
-    def save(self):
+    def save(self) -> None:
         """Сохраняет все изменения в организме."""
-        self._data.save()
+        self._doc.save()
 
 
 def _sample_organism(num: int) -> Iterable[Organism]:
@@ -180,11 +177,26 @@ def get_random_organism() -> Organism:
     return organism
 
 
+def get_parent() -> Organism:
+    """Получить лучший из популяции."""
+    collection = store.get_collection()
+    organism = collection.find_one(
+        filter={},
+        projection=["_id"],
+        sort=[
+            ("llh", pymongo.DESCENDING),
+        ],
+    )
+    return Organism(**organism)
+
+
 def get_all_organisms() -> Iterable[Organism]:
     """Получить все имеющиеся организмы."""
     collection = store.get_collection()
     id_dicts = collection.find(
-        filter={}, projection=["_id"], sort=[("date", pymongo.ASCENDING), ("llh", pymongo.DESCENDING)]
+        filter={},
+        projection=["_id"],
+        sort=[("date", pymongo.ASCENDING), ("llh", pymongo.DESCENDING)],
     )
     for id_dict in id_dicts:
         try:
@@ -193,39 +205,39 @@ def get_all_organisms() -> Iterable[Organism]:
             pass
 
 
-def print_stat() -> NoReturn:
-    """Статистика - минимальное и максимальное значение коэффициента Шарпа."""
+def print_stat() -> None:
+    """Статистика — минимальное и максимальное значение коэффициента Шарпа."""
     _print_llh_stats()
     _print_wins_stats()
 
 
-def _print_llh_stats() -> NoReturn:
+def _print_llh_stats() -> None:
     """Статистика по минимуму, медиане и максимуму llh."""
     collection = store.get_collection()
     db_find = collection.find
-    cursor = db_find(filter=dict(llh={"$exists": True}), projection=["llh"])
-    llhs = map(lambda x: x["llh"], cursor)
+    cursor = db_find(filter={"llh": {"$exists": True}}, projection=["llh"])
+    llhs = map(lambda doc: doc["llh"], cursor)
     llhs = tuple(llhs)
     if llhs:
-        quantiles = np.quantile(tuple(llhs), [0.0, 0.5, 1.0])
-        quantiles = map(lambda x: f"{x:.4f}", quantiles)
+        quantiles = np.quantile(tuple(llhs), [0, 0.5, 1.0])
+        quantiles = map(lambda quantile: f"{quantile:.4f}", quantiles)
         quantiles = tuple(quantiles)
     else:
-        quantiles = ["-"] * 3
+        quantiles = ["-" for _ in range(3)]
     print(f"LLH - ({', '.join(tuple(quantiles))})")
 
 
-def _print_wins_stats() -> NoReturn:
+def _print_wins_stats() -> None:
     """Статистика по максимуму побед."""
     collection = store.get_collection()
     db_find = collection.find
-    params = {
-        "filter": dict(wins={"$exists": True}),
+    request = {
+        "filter": {"wins": {"$exists": True}},
         "projection": ["wins"],
         "sort": [("wins", pymongo.DESCENDING)],
         "limit": 1,
     }
-    wins = list(db_find(**params))
+    wins = list(db_find(**request))
     max_wins = None
     if wins:
         max_wins, *_ = wins
