32dccb3ac5b76841c3d479612f0d075393d9a2ec
WLM1ke <wlmike@gmail.com>
2021-07-10 15:02:07 +0300

    settings: увеличена длинна прогноза

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -37,7 +37,7 @@ MAX_TRADE = 1 / 170
 MAX_POPULATION = 60
 
 # Длинна прогноза в торговых днях
-FORECAST_DAYS = 33
+FORECAST_DAYS = 34
 
 # Требуемая доходность, если None не используется
 MIN_RETURN = None
