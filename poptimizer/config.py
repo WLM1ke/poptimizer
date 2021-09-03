d175fb61f6cc4da92f9f31792dc9b600f612151b
WLM1ke <wlmike@gmail.com>
2021-09-03 07:50:16 +0300

    settings: увеличен минимальный размер истории

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -37,7 +37,7 @@ MIN_POPULATION = 60
 FORECAST_DAYS = 37
 
 # Минимальная количество дней истории котировок для прогнозов
-HISTORY_DAYS_MIN = 68
+HISTORY_DAYS_MIN = 70
 
 # Значимость отклонения градиента от нуля
 P_VALUE = 0.05
