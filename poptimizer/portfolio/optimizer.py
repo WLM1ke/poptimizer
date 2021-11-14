be6a54c86282741acf39a91851a23557568ee2b5
RomaKoks <1j2rsp26c3ak@mail.ru>
2021-11-14 18:04:29 +0300

    Расчет конкретных сделок. Сохранение рекомендаций в excel

--- poptimizer/portfolio/optimizer.py
+++ poptimizer/portfolio/optimizer.py
@@ -1,10 +1,14 @@
 """Оптимизатор портфеля."""
+import os
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from scipy import stats
+from sklearn.preprocessing import quantile_transform
 
 from poptimizer import config
 from poptimizer.portfolio import metrics
+from poptimizer.portfolio.excel_format import save_to_excel
 from poptimizer.portfolio.portfolio import CASH, Portfolio
 
 
@@ -25,13 +29,15 @@ class Optimizer:
 
     def __str__(self) -> str:
         """Информация о позициях, градиенты которых значимо отличны от 0."""
-        df = self._for_trade()
+        rec = self._for_trade()
         forecasts = self.metrics.count
         blocks = [
             "\nОПТИМИЗАЦИЯ ПОРТФЕЛЯ",
             f"\nforecasts = {forecasts}",
             f"p-value = {self._p_value:.2%}",
-            f"\n{df}",
+            f"\n{rec['SELL']}",
+            f"\n{rec['BUY']}",
+            f"\n{rec['new_port_summary']}",
         ]
         return "\n".join(blocks)
 
@@ -45,35 +51,104 @@ class Optimizer:
         """Метрики портфеля."""
         return self._metrics
 
-    def _for_trade(self) -> pd.DataFrame:
-        """Осуществляет расчет доверительного интервала для среднего."""
-        p_value = self._p_value / (len(self._portfolio.index) - 2)
+    def _update_portfolio(self, rec, cash):
+        # создаём новый портфель с учётом этой транзакции
+        rec['SHARES'] = rec['lots'] * rec['lot_size']
+        cur_prot = Portfolio(name=self.portfolio.name,
+                             date=self.portfolio.date,
+                             cash=cash,
+                             positions=rec['SHARES'].to_dict())
+        # Корректируем кэш (на всякий случай) чтобы оценка портфеля осталась той же
+        cur_prot._shares[CASH] = self.portfolio.value['PORTFOLIO'] - cur_prot.value['PORTFOLIO']
+        return cur_prot
 
-        conf_int = self.metrics.all_gradients.iloc[:-2]
-        conf_int = conf_int.apply(
-            lambda grad: _grad_conf_int(grad, p_value),
-            axis=1,
-            result_type="expand",
-        )
-        conf_int.columns = ["LOWER", "UPPER"]
-        conf_int["COSTS"] = self._costs()
-        conf_int["PRIORITY"] = conf_int["LOWER"] - conf_int["COSTS"]
+    def _for_trade(self, serialize=True) -> pd.DataFrame:
+        """Осуществляет расчет рекомендуемых операций."""
+        cur_prot = self.portfolio
 
-        for_sale = conf_int["UPPER"] < 0
-        for_sale = for_sale & (self._portfolio.shares.iloc[:-2] > 0)  # noqa: WPS465
-        for_sale = conf_int[for_sale]
-        for_sale = for_sale.assign(PRIORITY=lambda df: df["UPPER"])
+        rec = None
+        iteration = 0
+        prev_op = None
 
-        good_purchase = conf_int["PRIORITY"] > 0  # noqa: WPS465
-        good_purchase = conf_int[good_purchase]
+        while True:
+            iteration += 1
+            cur_metrics = metrics.MetricsResample(cur_prot)
+            grads = cur_metrics.all_gradients.iloc[:-2]
+            p_value = self._p_value / (len(cur_prot.index) - 2)
+            conf_int = grads
+            # Воообще, этот блок не используется, можно убрать, но пусть пока будет >>>
+            conf_int = conf_int.apply(
+                lambda grad: _grad_conf_int(grad, p_value),
+                axis=1,
+                result_type="expand",
+            )
+            conf_int.columns = ["LOWER", "UPPER"]
+            # <<<
+            rec = conf_int.copy()
+            # гармоническое среднее квантилей градиентов вместо PRIORITY
+            rec['PRIORITY'] = stats.hmean(quantile_transform(grads, n_quantiles=grads.shape[0]), axis=1)
+            rec.sort_values(["PRIORITY"], ascending=[False], inplace=True)
+            rec['lot_size'] = cur_prot.lot_size.loc[rec.index]
+            rec['lots'] = (cur_prot.shares.loc[rec.index] / rec['lot_size']).fillna(0).astype(int)
+            rec['lot_price'] = (cur_prot.lot_size.loc[rec.index] * cur_prot.price.loc[rec.index]).round(2)
 
-        return pd.concat(
-            [
-                good_purchase,
-                for_sale,
-            ],
-            axis=0,
-        ).sort_values("PRIORITY", ascending=False)
+            top_share = rec.index[0]
+            bot_share = rec.loc[rec['lots'] > 0].index[-1]
+            cash = cur_prot.value[CASH]
+            if cash > rec.loc[top_share, 'lot_price']:
+                rec.loc[top_share, 'lots'] += 1
+                cash -= rec.loc[top_share, 'lot_price']
+                cur_prot = self._update_portfolio(rec, cash)
+                if prev_op == ('SELL', top_share):
+                    # цикл (сначала продали лот, а потом купили обратно) -> выходим
+                    break
+                prev_op = ('BUY', top_share)
+                print(prev_op)
+            else:
+                rec.loc[bot_share, 'lots'] -= 1
+                cash += rec.loc[bot_share, 'lot_price']
+                cur_prot = self._update_portfolio(rec, cash)
+                prev_op = ('SELL', bot_share)
+                print(prev_op)
+
+            if iteration % 1000 == 0:
+                print('ITERATION', iteration, self.portfolio.name)
+
+        rec['SUM'] = (rec['lots'] * rec['lot_price']).round(2)
+        rec.sort_values('SUM', ascending=False, inplace=True)
+        rec['lots_exists'] = (self.portfolio.shares.loc[rec.index] / rec['lot_size']).fillna(0).astype(int)
+        rec['SHARES_AFTER'] = rec['lots'] * rec['lot_size']
+        recommendations = dict()
+        recommendations['SELL'] = rec.loc[rec['lots'] < rec['lots_exists']].copy()
+        recommendations['SELL']['lots'] = recommendations['SELL']['lots_exists'] - recommendations['SELL']['lots']
+
+        recommendations['BUY'] = rec.loc[rec['lots'] > rec['lots_exists']].copy()
+        recommendations['BUY']['lots'] = recommendations['BUY']['lots'] - recommendations['BUY']['lots_exists']
+
+        for op in ['SELL', 'BUY']:
+            recommendations[op]['SHARES'] = recommendations[op]['lots'] * recommendations[op]['lot_size']
+            recommendations[op]['SHARES_exists'] = recommendations[op]['lots_exists'] * recommendations[op]['lot_size']
+            # корректируем сумму учитывая целое количество лотов
+            recommendations[op]['SUM'] = (recommendations[op]['lots'] * recommendations[op]['lot_price']).round(2)
+
+            recommendations[op] = recommendations[op][['lot_size', 'lot_price', 'SHARES_exists', 'lots_exists',
+                                                       'lots', 'SHARES', 'SUM', 'SHARES_AFTER']]
+            recommendations[op].rename({'lots': f'lots_to_{op}'}, inplace=True, axis='columns')
+            recommendations[op].rename({'SHARES': f'SHARES_to_{op}'}, inplace=True, axis='columns')
+            recommendations[op].rename({'SUM': f'SUM_to_{op}'}, inplace=True, axis='columns')
+
+        recommendations['new_port_summary'] = cur_prot._main_info_df()
+        if serialize:
+            path = f"{config.REPORTS_PATH}/{'/'.join(self.portfolio.name)}"
+            print(path)
+            os.makedirs(path, exist_ok=True)
+            save_to_excel(f"{path}/{str(datetime.today())[:10]}_{','.join(self.portfolio.name)}.xlsx",
+                          {'summary': self.portfolio._main_info_df(),
+                           'lots_to_sell': recommendations['SELL'],
+                           'lots_to_buy': recommendations['BUY'],
+                           'new_port_summary': recommendations['new_port_summary']}
+                          )
+        return recommendations
 
     def _costs(self) -> pd.DataFrame:
         """Удельные торговые издержки.
@@ -102,19 +177,19 @@ class Optimizer:
 
         return (
             # Размер рыночного воздействие в дневном СКО для дневного оборот
-            config.MARKET_IMPACT_FACTOR
-            # Дневное СКО
-            * (self.metrics.std / config.YEAR_IN_TRADING_DAYS ** 0.5)
-            # Зависимость общих издержек от воздействия пропорционален степени 1.5 от нормированного на
-            # дневной оборот объема. Совершается покупка на кэш сейчас и увеличиваются издержки на
-            # ликвидацию позиции
-            * (cash ** impact_scale + (weight_cash ** impact_scale - weight ** impact_scale))
-            # Делим на объем операции для получения удельных издержек
-            / cash
-            # Умножаем на коэффициент пересчета в годовые значения
-            * (config.YEAR_IN_TRADING_DAYS / config.FORECAST_DAYS)
-            # Обычные издержки в две стороны
-            + config.COSTS * 2
+                config.MARKET_IMPACT_FACTOR
+                # Дневное СКО
+                * (self.metrics.std / config.YEAR_IN_TRADING_DAYS ** 0.5)
+                # Зависимость общих издержек от воздействия пропорционален степени 1.5 от нормированного на
+                # дневной оборот объема. Совершается покупка на кэш сейчас и увеличиваются издержки на
+                # ликвидацию позиции
+                * (cash ** impact_scale + (weight_cash ** impact_scale - weight ** impact_scale))
+                # Делим на объем операции для получения удельных издержек
+                / cash
+                # Умножаем на коэффициент пересчета в годовые значения
+                * (config.YEAR_IN_TRADING_DAYS / config.FORECAST_DAYS)
+                # Обычные издержки в две стороны
+                + config.COSTS * 2
         )
 
 
