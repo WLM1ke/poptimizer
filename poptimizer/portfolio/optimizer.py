fc7f3203001ac562b745ab587de1d16a659ffc1d
RomaKoks <1j2rsp26c3ak@mail.ru>
2021-11-25 21:57:30 +0300

    Изменение подхода к рассчету приоритета тикеров. Оптимизационный цикл с рассчетом конечных сделок. Отчёт.

--- poptimizer/portfolio/optimizer.py
+++ poptimizer/portfolio/optimizer.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 from scipy import stats
+from sklearn.preprocessing import quantile_transform
 
 from poptimizer import config
 from poptimizer.portfolio import metrics
@@ -25,13 +26,15 @@ class Optimizer:
 
     def __str__(self) -> str:
         """Информация о позициях, градиенты которых значимо отличны от 0."""
-        df = self._for_trade()
+        rec = self._for_trade()
         forecasts = self.metrics.count
         blocks = [
             "\nОПТИМИЗАЦИЯ ПОРТФЕЛЯ",
             f"\nforecasts = {forecasts}",
             f"p-value = {self._p_value:.2%}",
-            f"\n{df}",
+            f"\n{rec['SELL']}",
+            f"\n{rec['BUY']}",
+            f"\n{rec['new_port_summary']}",
         ]
         return "\n".join(blocks)
 
@@ -45,35 +48,93 @@ class Optimizer:
         """Метрики портфеля."""
         return self._metrics
 
-    def _for_trade(self) -> pd.DataFrame:
-        """Осуществляет расчет доверительного интервала для среднего."""
-        p_value = self._p_value / (len(self._portfolio.index) - 2)
+    def _update_portfolio(self, rec, cash):
+        """ Создаёт новый портфель с учётом новой транзакции"""
+        rec['SHARES'] = rec['lots'] * rec['lot_size']
+        cur_prot = Portfolio(name=self.portfolio.name,
+                             date=self.portfolio.date,
+                             cash=cash,
+                             positions=rec['SHARES'].to_dict())
+        return cur_prot
 
-        conf_int = self.metrics.all_gradients.iloc[:-2]
-        conf_int = conf_int.apply(
-            lambda grad: _grad_conf_int(grad, p_value),
-            axis=1,
-            result_type="expand",
-        )
-        conf_int.columns = ["LOWER", "UPPER"]
-        conf_int["COSTS"] = self._costs()
-        conf_int["PRIORITY"] = conf_int["LOWER"] - conf_int["COSTS"]
+    def _for_trade(self, serialize=True) -> dict[str, pd.DataFrame]:
+        """Осуществляет расчет рекомендуемых операций."""
+        cur_prot = self.portfolio
+        rec, op = None, None
+        # используется для определения цикла в операциях (получение портфеля который был ранее)
+        ports_set = set()
+        while True:
+            cur_metrics = metrics.MetricsResample(cur_prot)
+            grads = cur_metrics.all_gradients.iloc[:-2]
+            # гармоническое среднее квантилей градиентов вместо бутстрапа
+            # вычислительно существенно быстрее
+            q_trans_grads = quantile_transform(grads, n_quantiles=grads.shape[0])
+            # обработка (маскировка) возможных NA от плохих моделей
+            q_trans_grads = np.ma.array(q_trans_grads, mask=~(q_trans_grads > 0), fill_value=0)
+            # гармоническое среднее сильнее штрафует за низкие значения (близкие к 0),
+            # но его использование не принципиально - можно заменить на просто среднее или медиану
+            hmean_q_trans_grads = stats.hmean(q_trans_grads, axis=1)
+            rec = pd.Series(data=hmean_q_trans_grads, index=grads.index).to_frame(name='PRIORITY')
+            rec.sort_values(["PRIORITY"], ascending=[False], inplace=True)
 
-        for_sale = conf_int["UPPER"] < 0
-        for_sale = for_sale & (self._portfolio.shares.iloc[:-2] > 0)  # noqa: WPS465
-        for_sale = conf_int[for_sale]
-        for_sale = for_sale.assign(PRIORITY=lambda df: df["UPPER"])
+            # так как все операции производятся в лотах, нужно знать стоимость лота и текущее количество лотов
+            rec['lot_size'] = cur_prot.lot_size.loc[rec.index]
+            rec['lots'] = (cur_prot.shares.loc[rec.index] / rec['lot_size']).fillna(0).astype(int)
+            rec['lot_price'] = (cur_prot.lot_size.loc[rec.index] * cur_prot.price.loc[rec.index]).round(2)
 
-        good_purchase = conf_int["PRIORITY"] > 0  # noqa: WPS465
-        good_purchase = conf_int[good_purchase]
+            cash = cur_prot.value[CASH] + self.portfolio.value['PORTFOLIO'] - cur_prot.value['PORTFOLIO']
 
-        return pd.concat(
-            [
-                good_purchase,
-                for_sale,
-            ],
-            axis=0,
-        ).sort_values("PRIORITY", ascending=False)
+            # определяем операцию:
+            # покупка, если на покупку лучшего тикера хватает CASH
+            # иначе - продажа худшго тикера из тех, что в наличии
+
+            top_share = rec.index[0]
+            bot_share = rec.loc[rec['lots'] > 0].index[-1]
+            if cash > rec.loc[top_share, 'lot_price']:
+                rec.loc[top_share, 'lots'] += 1
+                cash -= rec.loc[top_share, 'lot_price']
+                op = ('BUY', top_share)
+            else:
+                rec.loc[bot_share, 'lots'] -= 1
+                cash += rec.loc[bot_share, 'lot_price']
+                op = ('SELL', bot_share)
+            cur_prot = self._update_portfolio(rec, cash)
+            print(len(ports_set), op, cash)
+            # проверка цикла
+            port_tuple = tuple(cur_prot.shares.drop(CASH).tolist())
+            if port_tuple in ports_set:
+                break
+            ports_set.add(port_tuple)
+
+        # оптимизированный портфель получен
+        # сортируем по новому весу от портфеля для наглядности и приоритезации сделок на покупку
+        rec['SUM'] = (rec['lots'] * rec['lot_price']).round(2)
+        rec.sort_values('SUM', ascending=False, inplace=True)
+        # найдем разницу с текущим портфелем
+        rec['lots_exists'] = (self.portfolio.shares.loc[rec.index] / rec['lot_size']).fillna(0).astype(int)
+        rec['SHARES_AFTER'] = rec['lots'] * rec['lot_size']
+        report = dict()
+        report['SELL'] = rec.loc[rec['lots'] < rec['lots_exists']].copy()
+        report['SELL']['lots'] = report['SELL']['lots_exists'] - report['SELL']['lots']
+
+        report['BUY'] = rec.loc[rec['lots'] > rec['lots_exists']].copy()
+        report['BUY']['lots'] = report['BUY']['lots'] - report['BUY']['lots_exists']
+
+        report['new_port_summary'] = cur_prot._main_info_df()
+
+        for op in ['SELL', 'BUY']:
+            report[op]['SHARES'] = report[op]['lots'] * report[op]['lot_size']
+            report[op]['SHARES_exists'] = report[op]['lots_exists'] * report[op]['lot_size']
+            # корректируем сумму учитывая целое количество лотов
+            report[op]['SUM'] = (report[op]['lots'] * report[op]['lot_price']).round(2)
+            # изменение порядка столбцов
+            report[op] = report[op][['lot_size', 'lot_price', 'SHARES_exists', 'lots_exists',
+                                     'lots', 'SHARES', 'SUM', 'SHARES_AFTER']]
+            report[op].rename({'lots': f'lots_to_{op}',
+                               'SHARES': f'SHARES_to_{op}',
+                               'SUM': f'SUM_to_{op}'},
+                              inplace=True, axis='columns')
+        return report
 
     def _costs(self) -> pd.DataFrame:
         """Удельные торговые издержки.
