a36e5f472e8a4a4aac2162eeab90784eb451b8d8
WLM1ke <wlmike@gmail.com>
2022-01-07 18:20:14 +0300

    feat: реализация репозитория на основе MongoDB

---
+++ data/internal/repo/mongo.go
@@ -0,0 +1,115 @@
+package repo
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/WLM1ke/poptimizer/data/internal/domain"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"time"
+)
+
+type tableDAO [R]struct {
+	Name domain.Name `bson:"_id"`
+	Date time.Time   `bson:"date"`
+	Rows []R         `bson:"rows"`
+}
+
+func NewTableDAO[R comparable](table domain.Table[R]) tableDAO[R] {
+	return tableDAO{
+		Name: table.Name,
+		Date: table.Date,
+		Rows: table.Rows,
+	}
+}
+
+func (d tableDAO[R]) toTable(group domain.Group) domain.Table[R] {
+	return domain.Table{
+		Group: group,
+		Name:  d.Name,
+		Date:  d.Date,
+		Rows:  d.Rows,
+	}
+}
+
+// Mongo обеспечивает хранение и загрузку таблиц.
+type Mongo[R comparable] struct {
+	db *mongo.Database
+}
+
+// NewMongo - создает новый репозиторий на основе MongoDB.
+func NewMongo[R comparable](db *mongo.Database) *Mongo[R] {
+	return &Mongo[R]{
+		db: db,
+	}
+}
+
+// Get загружает таблицу.
+func (r *Mongo[R]) Get(ctx context.Context, id domain.ID) (domain.Table[R], error) {
+	var dao tableDAO[R]
+
+	collection := r.db.Collection(string(id.Group))
+	err := collection.FindOne(ctx, bson.M{"_id": string(id.Name)}).Decode(&dao)
+
+	switch {
+	case errors.Is(err, mongo.ErrNoDocuments):
+		return nil, fmt.Errorf(
+			"%w: %s", ErrTableNotFound, id)
+	case err != nil:
+		return nil, fmt.Errorf("%w: %s - %s", ErrInternal, id, err)
+	}
+
+	return dao.toTable(id.Group), nil
+}
+
+// Replace перезаписывает таблицу.
+func (r *Mongo[R]) Replace(ctx context.Context, table domain.Table[R]) error {
+	collection := r.db.Collection(string(table.Group))
+
+	filter := bson.M{"_id": table.Name}
+	update := bson.M{"$set": bson.M{"rows": table.Rows, "date": table.Date}}
+
+	if _, err := collection.UpdateOne(ctx, filter, update, options.Update().SetUpsert(true)); err != nil {
+		return fmt.Errorf("%w: %s - %s", ErrTableUpdate, table.ID, err)
+	}
+
+	return nil
+}
+
+// Append добавляет строки в конец таблицы.
+func (r *Mongo[R]) Append(ctx context.Context, table domain.Table[R]) error {
+	collection := r.db.Collection(string(table.Group))
+
+	filter := bson.M{"_id": table.Name}
+	update := bson.M{"$push": bson.M{"rows": bson.M{"$each": table.Rows}}}
+
+	if _, err := collection.UpdateOne(ctx, filter, update, options.Update().SetUpsert(true)); err != nil {
+		return fmt.Errorf("%w: %s - %s", ErrTableUpdate, err, table.ID)
+	}
+
+	return nil
+}
+
+// GetJSON загружает ExtendedJSON представление таблицы.
+func (r *Mongo[R]) GetJSON(ctx context.Context, id domain.ID) ([]byte, error) {
+	collection := r.db.Collection(string(id.Group))
+
+	projections := options.FindOne().SetProjection(bson.M{"_id": 0, "rows": 1, "date": 1})
+
+	raw, err := collection.FindOne(ctx, bson.M{"_id": id.Name}, projections).DecodeBytes()
+	switch {
+	case errors.Is(err, mongo.ErrNoDocuments):
+		return nil, fmt.Errorf("%w: %s", ErrTableNotFound, id)
+	case err != nil:
+		return nil, fmt.Errorf("%w: %s - %s", ErrInternal, id, err)
+	}
+
+	json, err := bson.MarshalExtJSON(raw, true, true)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s - %s", ErrInternal, id, err)
+	}
+
+	return json, nil
+}
---
+++ data/internal/repo/repo.go
@@ -0,0 +1,34 @@
+package repo
+
+import (
+	"context"
+	"errors"
+	"github.com/WLM1ke/poptimizer/data/internal/domain"
+)
+
+var (
+	ErrTableNotFound = errors.New("table not found")
+	ErrInternal      = errors.New("repo internal error")
+	ErrTableUpdate   = errors.New("table update error")
+)
+
+// Read осуществляет загрузку таблиц.
+type Read[R comparable] interface {
+	// Get загружает таблицу.
+	Get(ctx context.Context, id domain.ID) (domain.Table[R], error)
+}
+
+// ReadJSON осуществляет загрузку таблицы в виде ExtendedJSON.
+type ReadJSON[R comparable] interface {
+	// GetJSON загружает ExtendedJSON представление таблицы.
+	GetJSON(ctx context.Context, id domain.ID) ([]byte, error)
+}
+
+// ReadWrite осуществляет загрузку и сохранение таблиц.
+type ReadWrite[R comparable] interface {
+	Read[R]
+	// Replace перезаписывает таблицу.
+	Replace(ctx context.Context, table domain.Table[R]) error
+	// Append добавляет строки в конец таблицы.
+	Append(ctx context.Context, table domain.Table[R]) error
+}
