dfc23413f78ededa0d067c3ea0512c3801f3bf53
WLM1ke <wlmike@gmail.com>
2022-03-14 10:50:49 +0300

    style: ignore type check for task cancellation

--- poptimizer/shared/app.py
+++ poptimizer/shared/app.py
@@ -115,7 +115,7 @@ def _shutdown_tasks(loop: asyncio.AbstractEventLoop) -> None:
     for task in to_cancel:
         task.cancel()
 
-    loop.run_until_complete(asyncio.gather(*to_cancel, loop=loop, return_exceptions=True))
+    loop.run_until_complete(asyncio.gather(*to_cancel, loop=loop, return_exceptions=True))  # type: ignore
 
     for canceled_task in to_cancel:
         if canceled_task.cancelled():
