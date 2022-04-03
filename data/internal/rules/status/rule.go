08fa46f4667c21152a16ddd9261d5627cf5205a0
WLM1ke <wlmike@gmail.com>
2022-04-03 07:16:56 +0300

    refactor: уточнено название правила

--- data/internal/rules/status/rule.go
+++ data/internal/rules/status/rule.go
@@ -1,13 +1,14 @@
 package status
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/WLM1ke/poptimizer/data/internal/domain"
 	"github.com/WLM1ke/poptimizer/data/internal/repo"
 	"github.com/WLM1ke/poptimizer/data/internal/rules/template"
 	"github.com/WLM1ke/poptimizer/data/pkg/lgr"
 	"go.mongodb.org/mongo-driver/mongo"
-	"net/http"
-	"time"
 )
 
 // Возможные альтернативные источники:
@@ -15,10 +16,10 @@ import (
 // - https://закрытияреестров.рф
 // - https://dividend.company
 // Теоретически можно проверять коректность распознования тикеров, но в этом кажется нет необходимости
-// Тесты для VEON-RX, AKRN и T-RM
+// Тесты для VEON-RX, AKRN и T-RM.
 func New(logger *lgr.Logger, db *mongo.Database, client *http.Client, timeout time.Duration) domain.Rule {
 	return template.NewRule[domain.DivStatus](
-		"DivStatus",
+		"DivStatusRule",
 		logger,
 		repo.NewMongo[domain.DivStatus](db),
 		selector{},
