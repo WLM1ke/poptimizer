81d19fc44567a3edccb160d6f12881b0045ad389
WLM1ke <wlmike@gmail.com>
2022-04-06 15:17:33 +0300

    feat: id сессии передается через cookie

--- data/internal/handlers/edit/handler.go
+++ data/internal/handlers/edit/handler.go
@@ -1,6 +1,7 @@
 package edit
 
 import (
+	"errors"
 	"html/template"
 	"net/http"
 
@@ -8,6 +9,8 @@ import (
 	"github.com/WLM1ke/poptimizer/data/pkg/lgr"
 	"github.com/go-chi/chi"
 )
+
+const _sessionID = `SessionID`
 
 type handler struct {
 	logger *lgr.Logger
@@ -26,109 +29,108 @@ func (h *handler) handleIndex(w http.ResponseWriter, r *http.Request) {
 
 	model, err := h.service.GetByTicker(ctx, ticker)
 	if err != nil {
-		h.logger.Warnf("Server: can't load dividends %s -> %s", ticker, err)
-		w.WriteHeader(http.StatusInternalServerError)
+		h.logger.Warnf("Server: can't load dividends -> %s", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 
 		return
 	}
+
+	cookie := http.Cookie{
+		Name:     _sessionID,
+		Value:    model.SessionID,
+		Path:     r.RequestURI,
+		MaxAge:   0,
+		Secure:   false,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+	}
+
+	http.SetCookie(w, &cookie)
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 
 	if err := h.index.Execute(w, model); err != nil {
 		h.logger.Warnf("Server: can't render index template -> %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-
-		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 }
 
-func (h *handler) handleAddRow(w http.ResponseWriter, r *http.Request) {
-	id := r.Header.Get("X-Request-ID")
-	if id == "" {
-		h.logger.Warnf("Server: no id in request")
-		w.WriteHeader(http.StatusBadRequest)
+func (h *handler) handleAddRow(rw http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(_sessionID)
+	if errors.Is(err, http.ErrNoCookie) {
+		h.logger.Warnf("Server: no cookie in request")
+		http.Error(rw, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
 	if err := r.ParseForm(); err != nil {
 		h.logger.Warnf("Server: can't parse request form -> %s", err)
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(rw, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
 	row, err := h.service.AddRow(
-		id,
+		cookie.Value,
 		r.PostForm.Get("date"),
 		r.PostForm.Get("value"),
 		r.PostForm.Get("currency"),
 	)
 	if err != nil {
 		h.logger.Warnf("Server: can't add row -> %s", err)
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(rw, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
-	if err := h.add.Execute(w, row); err != nil {
+	rw.Header().Set("Content-Type", "text/html; charset=UTF-8")
+
+	if err := h.add.Execute(rw, row); err != nil {
 		h.logger.Warnf("Server: can't render add template -> %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-
-		return
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
-
-	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 }
 
-func (h *handler) handleReload(w http.ResponseWriter, r *http.Request) {
-	id := r.Header.Get("X-Request-ID")
-	if id == "" {
-		h.logger.Warnf("Server: no id in request")
-		w.WriteHeader(http.StatusBadRequest)
+func (h *handler) handleReload(rw http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(_sessionID)
+	if errors.Is(err, http.ErrNoCookie) {
+		h.logger.Warnf("Server: no cookie in request")
+		http.Error(rw, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
-	div, err := h.service.Reload(r.Context(), id)
+	div, err := h.service.Reload(r.Context(), cookie.Value)
 	if err != nil {
-		h.logger.Warnf("Server: can't reload page for session id %s -> %s", id, err)
-		w.WriteHeader(http.StatusBadRequest)
+		h.logger.Warnf("Server: can't reload page -> %s", err)
+		http.Error(rw, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
-	if err := h.reload.Execute(w, div); err != nil {
+	rw.Header().Set("Content-Type", "text/html; charset=UTF-8")
+
+	if err := h.reload.Execute(rw, div); err != nil {
 		h.logger.Warnf("Server: can't render reload template -> %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-
-		return
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
-
-	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 }
 
 func (h *handler) handleSave(w http.ResponseWriter, r *http.Request) {
-	id := r.Header.Get("X-Request-ID")
-	if id == "" {
-		h.logger.Warnf("Server: no id in request")
-		w.WriteHeader(http.StatusBadRequest)
+	cookie, err := r.Cookie(_sessionID)
+	if errors.Is(err, http.ErrNoCookie) {
+		h.logger.Warnf("Server: no cookie in request")
+		http.Error(w, err.Error(), http.StatusBadRequest)
 
 		return
 	}
 
-	status := h.service.Save(r.Context(), id)
+	status := h.service.Save(r.Context(), cookie.Value)
+
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
 
 	if err := h.save.Execute(w, status); err != nil {
 		h.logger.Warnf("Server: can't render save template -> %s", err)
-		w.WriteHeader(http.StatusInternalServerError)
-
-		return
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
-	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
 }
