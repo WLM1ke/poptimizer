daf37220e481990e5cb1a67633edf0508e93483e
WLM1ke <wlmike@gmail.com>
2022-06-04 13:09:24 +0300

    feat: поддержка gob для агрегатов

--- opt/internal/domain/aggregate.go
+++ opt/internal/domain/aggregate.go
@@ -1,6 +1,10 @@
 package domain
 
-import "time"
+import (
+	"bytes"
+	"encoding/gob"
+	"time"
+)
 
 // Aggregate представляет доменный объект с данными, актуальными на конкретную дату.
 type Aggregate[E any] struct {
@@ -25,4 +29,48 @@ func newAggregate[E any](id QualifiedID, ver int, timestamp time.Time, data E) A
 		Timestamp: timestamp,
 		Entity:    data,
 	}
+}
+
+// GobEncode поддержка кодирования скрытых полей.
+func (a Aggregate[E]) GobEncode() ([]byte, error) {
+	dao := &struct {
+		ID        QualifiedID
+		Ver       int
+		Timestamp time.Time
+		Entity    E
+	}{
+		ID:        a.id,
+		Ver:       a.ver,
+		Timestamp: a.Timestamp,
+		Entity:    a.Entity,
+	}
+
+	var b bytes.Buffer
+	if err := gob.NewEncoder(&b).Encode(&dao); err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+// GobDecode поддержка декодирования скрытых полей.
+func (a *Aggregate[E]) GobDecode(b []byte) error {
+	dao := &struct {
+		ID        QualifiedID
+		Ver       int
+		Timestamp time.Time
+		Entity    E
+	}{}
+
+	reader := bytes.NewReader(b)
+	if err := gob.NewDecoder(reader).Decode(&dao); err != nil {
+		return err
+	}
+
+	a.id = dao.ID
+	a.ver = dao.Ver
+	a.Timestamp = dao.Timestamp
+	a.Entity = dao.Entity
+
+	return nil
 }
