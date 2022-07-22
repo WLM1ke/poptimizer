a3f6a70ad53346226c7723be05dc06121b5ca95f
WLM1ke <wlmike@gmail.com>
2022-07-22 09:44:48 +0300

    feat: заготовка для новой версии эволюции

---
+++ evolve/evolve/__main__.py
@@ -0,0 +1,10 @@
+"""Основная точка входа для запуска приложения."""
+import asyncio
+
+
+async def main() -> None:
+    """Запускает эволюцию."""
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
