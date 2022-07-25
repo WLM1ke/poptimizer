e1c97d355dfa941de91649cd000cfabb72ed89ff
WLM1ke <wlmike@gmail.com>
2022-07-25 17:38:49 +0300

    feat: обертка над логером с посылкой ошибок в телеграмм

---
+++ opt/internal/app/logger.go
@@ -0,0 +1,52 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/WLM1ke/poptimizer/opt/pkg/clients"
+	"github.com/WLM1ke/poptimizer/opt/pkg/lgr"
+)
+
+const _sendTimeout = time.Second
+
+type logger struct {
+	logger lgr.Logger
+	telega *clients.Telegram
+}
+
+func (l logger) WithPrefix(prefix string) lgr.Logger {
+	return logger{
+		logger: l.logger.WithPrefix(prefix),
+		telega: l.telega,
+	}
+}
+
+func (l logger) Infof(format string, args ...any) {
+	l.logger.Infof(format, args...)
+}
+
+func (l logger) Warnf(format string, args ...any) {
+	ctx, cancel := context.WithTimeout(context.Background(), _sendTimeout)
+	defer cancel()
+
+	err := l.telega.Send(ctx, fmt.Sprintf(format, args...))
+	if err != nil {
+		l.logger.Warnf("can't send notification -> %s", err)
+	}
+
+	l.logger.Warnf(format, args...)
+}
+
+func (l logger) Panicf(format string, args ...any) {
+	ctx, cancel := context.WithTimeout(context.Background(), _sendTimeout)
+	defer cancel()
+
+	err := l.telega.Send(ctx, fmt.Sprintf(format, args...))
+	if err != nil {
+		l.logger.Warnf("can't send notification -> %s", err)
+	}
+
+	l.logger.Panicf(format, args...)
+}
