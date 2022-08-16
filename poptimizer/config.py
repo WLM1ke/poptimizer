e6565382909142d9d7b55cb79629d9c0f3ff156f
WLM1ke <wlmike@gmail.com>
2022-08-16 11:02:43 +0300

    settings: снято ограничение на минимальную историю
    Так как все теперь бумаги торгуются можно использовать минимальную
    историю 3

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -57,7 +57,7 @@ YEAR_IN_TRADING_DAYS = 12 * MONTH_IN_TRADING_DAYS
 # Загрузка конфигурации
 FORECAST_DAYS = cast(int, _cfg.get("FORECAST_DAYS", 21))
 FORECAST_DIV = cast(float, _cfg.get("FORECAST_DIV", 0))
-HISTORY_DAYS_MIN = cast(int, _cfg.get("HISTORY_DAYS_MIN", 126))
+HISTORY_DAYS_MIN = cast(int, _cfg.get("HISTORY_DAYS_MIN", 3))
 P_VALUE = cast(float, _cfg.get("P_VALUE", 0.05))
 COSTS = cast(float, _cfg.get("COSTS", 0.025)) / 100
 TRADING_INTERVAL = cast(int, _cfg.get("TRADING_INTERVAL", 1))
