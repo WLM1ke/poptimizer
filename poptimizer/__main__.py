f25e5b6080f91458c0c754f46baebeb4872a7173
WLM1ke <wlmike@gmail.com>
2022-08-23 22:22:44 +0300

    feat: запуск приложения с высвобождением ресурсов

--- poptimizer/__main__.py
+++ poptimizer/__main__.py
@@ -5,11 +5,18 @@ import asyncio
 import logging
 import signal
 import types
-from typing import Final
+from typing import Awaitable, Callable, Final
+
+import aiohttp
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from poptimizer.data import updater
+from poptimizer.data.repo import Repo
+from poptimizer.data.trading_day import DatesSrv
 
 _URI: Final = "mongodb://localhost:27017"
+_DB: Final = "data_new"
+_POOL_SIZE: Final = 20
 
 
 class App:
@@ -20,6 +27,7 @@ class App:
         logging.basicConfig(level=logging.INFO)
         self._logger = logging.getLogger("App")
         self._stop_event = asyncio.Event()
+        self._resources: list[Callable[[], None] | Awaitable[None]] = []
 
     async def __aenter__(self) -> App:
         """Организует перехват системных сигналов для корректного завершения работы."""
@@ -37,11 +45,33 @@ class App:
         exc_tb: types.TracebackType | None,
     ) -> None:
         """Логирует выход из контекстного менеджера."""
+        count = len(self._resources)
+        self._logger.info(f"closing {count} resources")
+
+        for res in reversed(self._resources):
+            if isinstance(res, Awaitable):
+                await res
+                continue
+
+            res()
+
         self._logger.info("shutdown finished")
 
     async def run(self) -> None:
         """Запускает приложение."""
-        await updater.Updater().run(self._stop_event)
+        mongo = AsyncIOMotorClient(_URI, tz_aware=False)
+        self._resources.append(mongo.close)
+
+        repo = Repo(mongo[_DB])
+
+        session = aiohttp.ClientSession(
+            connector=aiohttp.TCPConnector(limit=_POOL_SIZE),
+        )
+        self._resources.append(session.close())
+
+        dates_srv = DatesSrv(repo, session)
+
+        await updater.Updater(dates_srv).run(self._stop_event)
 
     def _signal_handler(self) -> None:
         self._logger.info("shutdown signal received")
