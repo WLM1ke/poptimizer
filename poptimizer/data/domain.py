b64aff1a60fe2d2a5df811924b33e0f7d1cbcd50
WLM1ke <wlmike@gmail.com>
2022-08-23 22:18:50 +0300

    feat: стандартизированы группы таблиц и столбцы

--- poptimizer/data/domain.py
+++ poptimizer/data/domain.py
@@ -1,12 +1,26 @@
 """Описание доменных объектов."""
 from dataclasses import dataclass
-from enum import unique, Enum
+from datetime import datetime
+from enum import Enum, unique
 
 import pandas as pd
 
 
 @unique
+class Columns(str, Enum):  # noqa: WPS600
+    """Наименования столбцов в таблицах."""
+
+    DATE = "DATE"
+
+    def __str__(self) -> str:
+        """Отображается в виде названия, а не стандартного описания."""
+        return self.name
+
+
+@unique
 class Group(Enum):
+    """Группы таблиц."""
+
     TRADING_DATE = "trading_date"
 
 
