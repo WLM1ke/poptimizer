e90ba956ee956168efe760f40656e9afe202d9a1
WLM1ke <wlmike@gmail.com>
2022-08-26 17:10:29 +0300

    feat: добавлены функции валидации DataFrame

--- poptimizer/data/domain.py
+++ poptimizer/data/domain.py
@@ -1,9 +1,13 @@
 """Описание доменных объектов."""
+from __future__ import annotations
+
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum, unique
 
 import pandas as pd
+
+from poptimizer import config
 
 
 @unique
@@ -11,6 +15,7 @@ class Columns(str, Enum):  # noqa: WPS600
     """Наименования столбцов в таблицах."""
 
     DATE = "DATE"
+    VALUE = "VALUE"  # noqa: WPS110
 
     def __str__(self) -> str:
         """Отображается в виде названия, а не стандартного описания."""
@@ -22,13 +27,54 @@ class Group(Enum):
     """Группы таблиц."""
 
     TRADING_DATE = "trading_date"
+    CPI = "cpi"
 
 
 @dataclass(slots=True, frozen=True, kw_only=True)
 class Table:
-    """Таблица с рыночными данными."""
+    """Таблица с рыночными данными.
+
+    Таблицы разбиты на группы, некоторые из которых содержат единственный элемент. В таком случае название элемента
+    не указывается.
+    """
 
     group: Group
-    name: str | None
-    timestamp: datetime | None
-    df: pd.DataFrame | None
+    name: str | None = None
+    timestamp: datetime | None = None
+    df: pd.DataFrame | None = None
+
+    def new_revision(self, timestamp: datetime, df: pd.DataFrame) -> Table:
+        """Создает новую версию таблицы."""
+        return Table(
+            group=self.group,
+            name=self.name,
+            timestamp=timestamp,
+            df=df,
+        )
+
+
+class DataError(config.POError):
+    """Ошибки, связанные с операциями над данными."""
+
+
+def raise_not_unique_increasing_index(df: pd.DataFrame) -> None:
+    """Тестирует индекс на уникальность и возрастание."""
+    index = df.index
+
+    if not index.is_monotonic_increasing:
+        raise DataError("index is not increasing")
+
+    if not index.is_unique:
+        raise DataError("index is not unique")
+
+
+def raise_dfs_mismatch(df: pd.DataFrame, df_old: pd.DataFrame) -> None:
+    """Сравнивает новые данные со старыми для старого индекса."""
+    if df_old is None:
+        return
+
+    df_new_val = df.reindex(df_old.index)
+    try:
+        pd.testing.assert_frame_equal(df_new_val, df_old, check_dtype=False)
+    except AssertionError as err:
+        raise DataError("dfs mismatch") from err
