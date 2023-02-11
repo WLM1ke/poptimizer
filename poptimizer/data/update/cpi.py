ef1a44eb475106a6372dcad63c0f43b7b4bb2596
WLM1ke <wlmike@gmail.com>
2023-02-11 10:09:11 +0300

    fox: изменен адрес инфляции

--- poptimizer/data/update/cpi.py
+++ poptimizer/data/update/cpi.py
@@ -14,7 +14,7 @@ from pydantic import Field, validator
 from poptimizer.core import domain, repository
 from poptimizer.data import exceptions, validate
 
-_URL: Final = "https://rosstat.gov.ru/storage/mediabank/Ipc_mes-11.xlsx"
+_URL: Final = "https://rosstat.gov.ru/storage/mediabank/ipc_mes-1.xlsx"
 _SHEET_NAME: Final = "01"
 
 _FIRST_MONTH_CELL: Final = "A6"
