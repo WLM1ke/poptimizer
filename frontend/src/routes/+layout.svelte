c5af3d0093aa4e04859c34eabfaf109bacd54f0b
WLM1ke <wlmike@gmail.com>
2024-01-05 13:50:01 +0300

    refactor: font as lib

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import "../app.css";
+	import "@fontsource/roboto-condensed";
 	import { pageTitle } from "$lib/stores";
 	import Logo from "$lib/components/Logo.svelte";
 	import Header from "$lib/components/Header.svelte";
@@ -12,7 +13,7 @@
 	<title>POptimizer - {$pageTitle}</title>
 </svelte:head>
 
-<section class="grid h-screen w-screen grid-cols-layout grid-rows-layout">
+<section class="grid-cols-layout grid-rows-layout grid h-screen w-screen">
 	<Logo />
 	<Header />
 	<Sidebar accounts={data.accounts} />
