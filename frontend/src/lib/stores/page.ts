1e9d2103fef35703259268272f4595adcdc3b5c8
WLM1ke <wlmike@gmail.com>
2024-01-14 12:11:01 +0300

    feat: summary page

--- frontend/src/lib/stores/page.ts
+++ frontend/src/lib/stores/page.ts
@@ -3,6 +3,10 @@ import { page } from "$app/stores";
 
 export const pageTitle = derived(page, (page) => {
 	const path = page.url.pathname;
+	if (path === "/") {
+		return "Summary";
+	}
+
 	const lastSlash = path.lastIndexOf("/");
 	return path[lastSlash + 1].toUpperCase() + path.substring(lastSlash + 2);
 });
