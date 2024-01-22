617f92a3c74ef874a0e261b5089306094218fb2b
WLM1ke <wlmike@gmail.com>
2024-01-22 21:35:10 +0300

    fix: for new pyright version

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -18,9 +18,9 @@ class Cfg(BaseSettings):
     log_level: int | str = "INFO"
     telegram_token: str = ""
     telegram_chat_id: str = ""
-    server_url: HttpUrl = HttpUrl("http://localhost:5000")
+    server_url: HttpUrl = Url("http://localhost:5000")
     mongo_db_uri: MongoDsn = MultiHostUrl("mongodb://localhost:27017")
-    nats_uri: NatsDsn = NatsDsn("nats://localhost:4222")
+    nats_uri: NatsDsn = Url("nats://localhost:4222")
 
     model_config = SettingsConfigDict(
         env_file=Path(".env"),
