2ea008d78256dfed4715b0950118ab2bc5928d99
WLM1ke <wlmike@gmail.com>
2024-02-04 10:42:11 +0300

    refactor: use NATS url from pydantic

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -1,17 +1,8 @@
 from pathlib import Path
-from typing import Annotated
 
-from pydantic import HttpUrl, MongoDsn, UrlConstraints
+from pydantic import HttpUrl, MongoDsn, NatsDsn
 from pydantic_core import MultiHostUrl, Url
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
-NatsDsn = Annotated[
-    Url,
-    UrlConstraints(
-        allowed_schemes=["nats"],
-        default_port=4222,
-    ),
-]
 
 
 class Cfg(BaseSettings):
@@ -20,7 +11,7 @@ class Cfg(BaseSettings):
     telegram_chat_id: str = ""
     server_url: HttpUrl = Url("http://localhost:5000")
     mongo_db_uri: MongoDsn = MultiHostUrl("mongodb://localhost:27017")
-    nats_uri: NatsDsn = Url("nats://localhost:4222")
+    nats_uri: NatsDsn = MultiHostUrl("nats://localhost:4222")
 
     model_config = SettingsConfigDict(
         env_file=Path(".env"),
