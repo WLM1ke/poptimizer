4f5a19ac75d40f7d54c4ff3e66977ef85cafd122
WLM1ke <wlmike@gmail.com>
2024-05-12 17:34:28 +0300

    refactor: logger

---
+++ poptimizer/adapter/lgr.py
@@ -0,0 +1,47 @@
+import logging
+import sys
+import types
+from copy import copy
+from typing import Final, Literal
+
+_LOGGER_NAME_SIZE: Final = 11
+
+
+class _ColorFormatter(logging.Formatter):
+    levels: Final = types.MappingProxyType(
+        {
+            logging.DEBUG: "\033[90mDBG\033[0m",
+            logging.INFO: "\033[34mINF\033[0m",
+            logging.WARNING: "\033[31mWRN\033[0m",
+            logging.ERROR: "\033[1;31mERR\033[0m",
+            logging.CRITICAL: "\033[1;91mCRT\033[0m",
+        },
+    )
+
+    def __init__(
+        self,
+        fmt: str = "{asctime} {levelname} {message}",
+        datefmt: str = "%Y-%m-%d %H:%M:%S",
+        style: Literal["%", "{", "$"] = "{",
+    ) -> None:
+        super().__init__(fmt=fmt, datefmt=datefmt, style=style)
+
+    def formatMessage(self, record: logging.LogRecord) -> str:  # noqa: N802
+        record = copy(record)
+        record.levelname = self.levels[record.levelno]
+        record.name = f"{record.name}:".ljust(_LOGGER_NAME_SIZE)
+
+        return super().formatMessage(record)
+
+
+def init(level: int | str = logging.INFO) -> logging.Logger:
+    color_handler = logging.StreamHandler(sys.stdout)
+    color_handler.setFormatter(_ColorFormatter())
+
+    logging.basicConfig(
+        level=level,
+        handlers=[color_handler],
+    )
+    logging.getLogger("pymongo").setLevel(logging.CRITICAL)
+
+    return logging.getLogger()
