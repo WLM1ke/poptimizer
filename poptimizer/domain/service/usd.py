275c7a2bfc0dd723ea7084d10919ab53985c07e2
WLM1ke <wlmike@gmail.com>
2024-05-12 11:35:45 +0300

    refactor: usd

---
+++ poptimizer/domain/service/usd.py
@@ -0,0 +1,42 @@
+from datetime import date
+
+import aiohttp
+import aiomoex
+from pydantic import TypeAdapter
+
+from poptimizer.domain import consts
+from poptimizer.domain.entity import entity, usd
+from poptimizer.domain.service import service
+
+
+class USDUpdater:
+    def __init__(self, http_client: aiohttp.ClientSession) -> None:
+        self._session = http_client
+
+    async def __call__(self, ctx: service.Ctx, update_day: entity.Day) -> None:
+        table = await ctx.get_for_update(usd.Table)
+
+        start_day = table.last_row_date()
+        rows = await self._download(start_day, update_day)
+
+        table.update(update_day, rows)
+
+    async def _download(
+        self,
+        start_day: date | None,
+        update_day: date,
+    ) -> list[usd.Row]:
+        json = await aiomoex.get_market_candles(
+            session=self._session,
+            start=start_day and str(start_day),
+            end=str(update_day),
+            interval=24,
+            security="USD000UTSTOM",
+            market="selt",
+            engine="currency",
+        )
+
+        try:
+            return TypeAdapter(list[usd.Row]).validate_python(json)
+        except ValueError as err:
+            raise consts.DomainError("can't validate usd data") from err
