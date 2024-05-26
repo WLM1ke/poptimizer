e4cf5c4e59b4aac5cfc00b35f4e48175012a912e
WLM1ke <wlmike@gmail.com>
2024-05-26 16:08:40 +0300

    feat: graceful shutdown

--- poptimizer/app.py
+++ poptimizer/app.py
@@ -1,5 +1,7 @@
 import asyncio
 import contextlib
+from collections.abc import Coroutine
+from typing import Any
 
 import uvloop
 
@@ -15,6 +17,7 @@ from poptimizer.ui import server
 async def _run() -> None:
     cfg = config.Cfg()
     logger = lgr.init()
+    err: Exception | None = None
 
     async with contextlib.AsyncExitStack() as stack:
         http_client = await stack.enter_async_context(http.Client())
@@ -28,35 +31,52 @@ async def _run() -> None:
         mongo_db: mongo.MongoDatabase = mongo_client[cfg.mongo_db_db]
         mongo_repo = mongo.Repo(mongo_db)
 
-        logging_service = await stack.enter_async_context(logging.Service(logger, telegram_client))
+        lgr_service = await stack.enter_async_context(logging.Service(logger, telegram_client))
 
         div_raw_collection: mongo.MongoCollection = mongo_db[adapter.get_component_name(raw.DivRaw)]
-        backup_srv = backup.Service(logging_service, div_raw_collection)
+        backup_srv = backup.Service(lgr_service, div_raw_collection)
         await backup_srv.restore()
 
         ctx_factory = uow.CtxFactory(
-            logging_service,
+            lgr_service,
             mongo_repo,
             view.Service(mongo_repo),
         )
 
-        tg = await stack.enter_async_context(asyncio.TaskGroup())
-
-        tg.create_task(
+        tg = asyncio.TaskGroup()
+        service_coro: tuple[Coroutine[Any, Any, None], ...] = (
             app.run(
-                logging_service,
+                lgr_service,
                 http_client,
                 ctx_factory,
-            )
-        )
-        tg.create_task(
+            ),
             server.run(
-                logging_service,
+                lgr_service,
                 ctx_factory,
                 cfg.server_url,
                 lambda: backup_srv.backup_action(tg),
-            )
+            ),
         )
+
+        try:
+            await _start_app(tg, service_coro)
+        except asyncio.CancelledError:
+            lgr_service.info("Shutdown finished")
+        except Exception as exc:  # noqa: BLE001
+            lgr_service.warn(f"Shutdown abnormally: {exc!r}")
+            err = exc
+
+    if err:
+        raise err
+
+
+async def _start_app(
+    tg: asyncio.TaskGroup,
+    service_coro: tuple[Coroutine[Any, Any, None], ...],
+) -> None:
+    async with tg:
+        for coro in service_coro:
+            tg.create_task(coro)
 
 
 def run() -> None:
