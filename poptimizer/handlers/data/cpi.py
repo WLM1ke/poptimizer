0e5d55093b2c9552b21107c6d633ba3d487dcc74
WLMike <wlmike@gmail.com>
2024-10-22 20:44:35 +0300

    feat: cpi handler

---
+++ poptimizer/handlers/data/cpi.py
@@ -0,0 +1,100 @@
+import io
+import re
+from datetime import date, timedelta
+from typing import Final, cast
+
+import aiohttp
+from openpyxl.reader import excel
+from openpyxl.worksheet import worksheet
+
+from poptimizer import errors
+from poptimizer.domain.data import cpi
+from poptimizer.handlers import handler
+
+_PRICES_PAGE: Final = "https://rosstat.gov.ru/statistics/price"
+_RE_FILE: Final = re.compile(r"/[iI]pc[\-_]mes[\-_][0-9]{1,2}-[0-9]{4}.xlsx")
+_URL_TMPL: Final = "https://rosstat.gov.ru/storage/mediabank/{}"
+_SHEET_NAME: Final = "01"
+
+_FIRST_MONTH_CELL: Final = "A6"
+_FIRST_MONTH_VALUE: Final = "январь"
+_FIRST_YEAR_CELL: Final = "B4"
+_FIRST_YEAR_VALUE: Final = 1991
+
+_MIN_ROW: Final = 6
+_MAX_ROW: Final = 17
+_MIN_COL: Final = 2
+
+_JANUARY_LAST_DAY: Final = 31
+
+
+class CPIHandler:
+    def __init__(self, http_session: aiohttp.ClientSession) -> None:
+        self._http_session = http_session
+
+    async def __call__(self, ctx: handler.Ctx, msg: handler.DataPublished) -> None:
+        table = await ctx.get_for_update(cpi.CPI)
+
+        try:
+            xlsx_file = await self._download()
+        except (TimeoutError, aiohttp.ClientError) as err:
+            raise errors.HandlerError("MOEX IIS") from err
+
+        row = _parse_rows(xlsx_file)
+
+        table.update(msg.day, row)
+
+    async def _download(self) -> io.BytesIO:
+        async with self._http_session.get(_PRICES_PAGE) as resp:
+            if not resp.ok:
+                raise errors.HandlerError(f"bad CPI respond status {resp.reason}")
+
+            html = await resp.text()
+
+        if (file_name := _RE_FILE.search(html)) is None:
+            raise errors.HandlerError("can't find file with CPI")
+
+        cpi_url = _URL_TMPL.format(file_name.group(0))
+
+        async with self._http_session.get(cpi_url) as resp:
+            if not resp.ok:
+                raise errors.HandlerError(f"bad CPI respond status {resp.reason}")
+
+            return io.BytesIO(await resp.read())
+
+
+def _parse_rows(xlsx: io.BytesIO) -> list[cpi.Row]:
+    wb = excel.load_workbook(xlsx)
+    ws = cast(worksheet.Worksheet, wb[_SHEET_NAME])
+
+    _validate_data_position(ws)
+
+    day = date(year=_FIRST_YEAR_VALUE, month=1, day=_JANUARY_LAST_DAY)
+    rows: list[cpi.Row] = []
+
+    for row in ws.iter_cols(min_row=_MIN_ROW, max_row=_MAX_ROW, min_col=_MIN_COL, values_only=True):
+        for cell in row:
+            match cell:
+                case float() | int():
+                    rows.append(cpi.Row(day=day, cpi=cell / 100))
+                case None:
+                    return rows
+                case _:
+                    raise errors.HandlerError(f"strange CPI value {cell}")
+
+            day = _get_next_month_end(day)
+
+    return rows
+
+
+def _validate_data_position(ws: worksheet.Worksheet) -> None:
+    if (first_year := ws[_FIRST_YEAR_CELL].value) != _FIRST_YEAR_VALUE:
+        raise errors.HandlerError(f"first year {first_year}")
+    if (first_month := ws[_FIRST_MONTH_CELL].value) != _FIRST_MONTH_VALUE:
+        raise errors.HandlerError(f"wrong first month {first_month}")
+
+
+def _get_next_month_end(day: date) -> date:
+    skip_month = day + timedelta(days=_JANUARY_LAST_DAY + 1)
+
+    return skip_month - timedelta(days=skip_month.day)
--- poptimizer/service/bus.py
+++ poptimizer/service/bus.py
@@ -2,7 +2,7 @@ import aiohttp
 
 from poptimizer.adapters import msg
 from poptimizer.handlers import handler
-from poptimizer.handlers.data import trading_day
+from poptimizer.handlers.data import cpi, trading_day
 from poptimizer.handlers.evolve import evolve
 
 
@@ -10,6 +10,8 @@ def run(bus: msg.Bus, http_client: aiohttp.ClientSession) -> None:
     trading_day_handler = trading_day.TradingDayHandler(http_client)
     bus.register_handler(trading_day_handler.check, msg.IndefiniteRetryPolicy)
     bus.register_handler(trading_day_handler.update, msg.IndefiniteRetryPolicy)
+
+    bus.register_handler(cpi.CPIHandler(http_client), msg.IgnoreErrorsPolicy)
 
     bus.register_handler(evolve.EvolutionHandler(), msg.IndefiniteRetryPolicy)
     bus.publish(handler.AppStarted())
