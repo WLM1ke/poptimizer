a30f537315c61770a182e3d22e00a375af7c2fed
WLMike <wlmike@gmail.com>
2024-11-17 20:13:13 +0300

    fix: mongo quiry

--- poptimizer/adapters/mongo.py
+++ poptimizer/adapters/mongo.py
@@ -45,10 +45,10 @@ class Repo:
             {
                 "$project": {
                     "day": True,
-                    "_MONGO_ID": True,
+                    _MONGO_ID: True,
                 },
             },
-            {"$sort": {"day": pymongo.ASCENDING, "_MONGO_ID": pymongo.DESCENDING}},
+            {"$sort": {"day": pymongo.ASCENDING, _MONGO_ID: pymongo.DESCENDING}},
             {"$limit": 1},
         ]
 
