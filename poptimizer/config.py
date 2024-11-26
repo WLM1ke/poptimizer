59de34cdc25775b6e46f9f98cb142a3aad7d00b2
WLMike <wlmike@gmail.com>
2024-11-26 07:28:33 +0300

    fix: fix for new linting rules

--- poptimizer/config.py
+++ poptimizer/config.py
@@ -1,15 +1,14 @@
 from pathlib import Path
 
 from pydantic import HttpUrl, MongoDsn
-from pydantic_core import MultiHostUrl, Url
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Cfg(BaseSettings):
     telegram_token: str = ""
     telegram_chat_id: str = ""
-    server_url: HttpUrl = Url("http://localhost:5000")
-    mongo_db_uri: MongoDsn = MultiHostUrl("mongodb://localhost:27017")
+    server_url: HttpUrl = HttpUrl("http://localhost:5000")
+    mongo_db_uri: MongoDsn = MongoDsn("mongodb://localhost:27017")
     mongo_db_db: str = "poptimizer"
 
     model_config = SettingsConfigDict(
