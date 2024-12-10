4e7f7f1c8c62b8b2e04aa6ee76566f3650a3b89e
WLMike <wlmike@gmail.com>
2024-12-10 21:52:31 +0300

    feat: calc of mean forecasts

--- poptimizer/use_cases/portfolio/forecasts.py
+++ poptimizer/use_cases/portfolio/forecasts.py
@@ -1,5 +1,14 @@
-from poptimizer.domain.portfolio import forecasts
+from typing import TYPE_CHECKING
+
+import numpy as np
+
+from poptimizer.domain import domain
+from poptimizer.domain.evolve import evolve
+from poptimizer.domain.portfolio import forecasts, portfolio
 from poptimizer.use_cases import handler
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
 
 
 class ForecastHandler:
@@ -19,8 +28,39 @@ class ForecastHandler:
                 forecast.models.add(msg.uid)
 
         if len(forecast.models) ** 0.5 - forecast.forecasts**0.5 >= 1:
-            await self._update_forecast(ctx, forecast)
+            await self._update_forecast(ctx, msg.day, forecast)
 
         return handler.ForecastsAnalyzed(day=msg.day)
 
-    async def _update_forecast(self, ctx: handler.Ctx, forecast: forecasts.Forecast) -> None: ...
+    async def _update_forecast(self, ctx: handler.Ctx, day: domain.Day, forecast: forecasts.Forecast) -> None:
+        port = await ctx.get(portfolio.Portfolio)
+
+        weights = np.array(port.weights()).reshape(-1, 1)
+        tickers = port.tickers()
+
+        means: list[NDArray[np.double]] = []
+
+        for uid in forecast.models:
+            model = await ctx.get(evolve.Model, uid)
+            if model.day != day or model.tickers != tickers:
+                forecast.models.remove(uid)
+                continue
+            mean: NDArray[np.double] = np.array(model.mean)
+            means.append(mean)
+
+        stacked_mean = np.hstack(means)
+        median_mean = np.median(stacked_mean, axis=1)
+        port_means = np.sum(weights * stacked_mean, axis=0)
+        forecast.mean = np.median(port_means)
+
+        forecast.positions = []
+        for n, ticker in enumerate(tickers):
+            forecast.positions.append(
+                forecasts.Position(
+                    ticker=ticker,
+                    mean=median_mean[n],
+                )
+            )
+
+        forecast.forecasts = len(means)
+        forecast.portfolio_ver = port.ver
