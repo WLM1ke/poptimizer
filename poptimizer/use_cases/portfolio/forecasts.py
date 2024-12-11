f29c170b8fb1c9275734866f0452ec255f5b1e45
WLMike <wlmike@gmail.com>
2024-12-11 20:01:09 +0300

    feat: model.risk_tolerance

--- poptimizer/use_cases/portfolio/forecasts.py
+++ poptimizer/use_cases/portfolio/forecasts.py
@@ -43,10 +43,13 @@ class ForecastHandler:
         tickers = port.tickers()
 
         means: list[NDArray[np.double]] = []
-        port_mean: list[float] = []
+        port_means: list[float] = []
 
         stds: list[NDArray[np.double]] = []
-        port_std: list[float] = []
+        port_stds: list[float] = []
+
+        betas: list[NDArray[np.double]] = []
+        grads: list[NDArray[np.double]] = []
 
         for uid in forecast.models:
             model = await ctx.get(evolve.Model, uid)
@@ -56,20 +59,37 @@ class ForecastHandler:
 
             mean: NDArray[np.double] = np.array(model.mean)
             means.append(mean)
-            port_mean.append(np.sum(mean * weights).item())
+            port_mean = weights.reshape(1, -1) @ mean
+            port_means.append(port_mean.item())
 
             cov: NDArray[np.double] = np.array(model.cov)
             stds.append(np.diag(cov).reshape(-1, 1) ** 0.5)
-            port_std.append(((weights.reshape(1, -1) @ cov @ weights) ** 0.5).item())
+            covs = cov @ weights
+            port_var = weights.reshape(1, -1) @ covs
+            port_std = port_var**0.5
+            port_stds.append(port_std.item())
+            beta = covs / port_var
+            betas.append(beta)
 
-        forecast.mean = np.median(port_mean).item()
-        forecast.std = np.median(port_std).item()
+            # U = risk_tolerance * (mp - sp ** 2 / 2) - (1 - risk_tolerance) * sp  # noqa: ERA001
+            grad_log_ret = (mean - port_mean) - port_var * (beta - 1)
+            grad_err = port_std * (beta - 1)
+            grads.append(model.risk_tolerance * grad_log_ret - (1 - model.risk_tolerance) * grad_err)
+
+        forecast.mean = np.median(port_means).item()
+        forecast.std = np.median(port_stds).item()
 
         stacked_mean = np.hstack(means)
         median_mean = np.median(stacked_mean, axis=1)
 
         stacked_stds = np.hstack(stds)
         median_std = np.median(stacked_stds, axis=1)
+
+        stacked_betas = np.hstack(betas)
+        median_betas = np.median(stacked_betas, axis=1)
+
+        stacked_grads = np.hstack(grads)
+        median_grads = np.median(stacked_grads, axis=1)
 
         forecast.positions = []
         for n, ticker in enumerate(tickers):
@@ -78,6 +98,8 @@ class ForecastHandler:
                     ticker=ticker,
                     mean=median_mean[n],
                     std=median_std[n],
+                    beta=median_betas[n],
+                    grad=median_grads[n],
                 )
             )
 
