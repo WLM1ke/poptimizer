36827d800280ed86d064bb8b2552c38e5960af3f
WLMike <wlmike@gmail.com>
2024-12-23 20:56:16 +0300

    refactor: button with runes

--- frontend/src/lib/components/Button.svelte
+++ frontend/src/lib/components/Button.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	export let label: string;
+	let { label, onclick }: { label: string; onclick: () => void } = $props();
 </script>
 
 <button
-	on:click
+	{onclick}
 	type="button"
-	class="my-2 w-32 rounded-lg bg-link-base px-5 py-2.5 font-medium text-white hover:bg-link-hover">{label}</button
+	class="bg-link-base hover:bg-link-hover my-2 w-32 rounded-lg px-5 py-2.5 font-medium text-white">{label}</button
 >
