ad53ee95f5dd62f1b1a1a96572794ff42c87e5e3
WLMike <wlmike@gmail.com>
2024-12-28 21:25:58 +0300

    style: card format

--- frontend/src/lib/components/Card.svelte
+++ frontend/src/lib/components/Card.svelte
@@ -4,12 +4,12 @@
 	let { upper, main, lower }: { upper: string; main: string; lower: string } = $props();
 </script>
 
-<section class="bg-bg-accent w-80 rounded-lg p-4">
-	<div class="text-text-muted italic">
+<section class="w-80 rounded-lg bg-bg-accent p-4">
+	<div class="italic text-text-muted">
 		{upper}
 	</div>
 	<H2 text={main} />
-	<div class="text-text-muted italic">
+	<div class="italic text-text-muted">
 		{lower}
 	</div>
 </section>
