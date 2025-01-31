f030f48f6d446bb139d9553b4604cde00a92fef7
WLMike <wlmike@gmail.com>
2025-01-31 21:28:06 +0300

    fix: sorted accounts on update

--- poptimizer/use_cases/requests/portfolio.py
+++ poptimizer/use_cases/requests/portfolio.py
@@ -49,7 +49,7 @@ class Portfolio(handler.DTO):
         return cls(
             day=port.day,
             ver=port.ver + 1,
-            account_names=list(port.account_names),
+            account_names=sorted(port.account_names),
             cash=port.cash,
             positions=port.positions,
         )
