d56ff36dbfe4226448606e54b2b264165f53cae9
WLMike <wlmike@gmail.com>
2025-02-06 22:58:30 +0300

    feat: add cert for rosstat

--- poptimizer/adapters/http_session.py
+++ poptimizer/adapters/http_session.py
@@ -1,3 +1,5 @@
+import ssl
+from pathlib import Path
 from typing import Final
 
 import aiohttp
@@ -7,10 +9,16 @@ _HEADERS: Final = {
     "User-Agent": "POptimizer",
     "Connection": "keep-alive",
 }
+_ROS_STAT_CERTS: Final = Path(__file__).parent / "certs" / "_.rosstat.gov.ru.pem"
 
 
 def client(on_per_host: int = _MAX_ISS_CON) -> aiohttp.ClientSession:
+    ctx = ssl.create_default_context()
+    ctx.load_verify_locations(_ROS_STAT_CERTS)
     return aiohttp.ClientSession(
-        connector=aiohttp.TCPConnector(limit_per_host=on_per_host),
+        connector=aiohttp.TCPConnector(
+            ssl_context=ctx,
+            limit_per_host=on_per_host,
+        ),
         headers=_HEADERS,
     )
