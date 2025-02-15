da5089e7c7fd770f9f1f90c747419efd1396dd3f
WLMike <wlmike@gmail.com>
2025-02-15 19:12:19 +0300

    test: phenotype match dl config

---
+++ poptimizer/use_cases/dl/tests/test_trainer.py
@@ -0,0 +1,16 @@
+from poptimizer.domain.evolve import genotype
+from poptimizer.use_cases.dl import trainer
+
+
+def _check_keys(phenotype, cfg):
+    for k, v in phenotype.items():
+        assert k in cfg
+
+        if isinstance(v, dict):
+            _check_keys(v, cfg[k])
+
+
+def test_cfg_match_phenotype():
+    phenotype = genotype.Genotype().phenotype
+    cfg = trainer.Cfg.model_validate(phenotype).model_dump()
+    _check_keys(phenotype, cfg)
