7a4e116a84879611778ef6dd790eed11b8fed200
WLMike <wlmike@gmail.com>
2025-07-30 06:34:03 +0300

    fix: wide card

--- frontend/src/lib/components/Card.svelte
+++ frontend/src/lib/components/Card.svelte
@@ -4,12 +4,12 @@
 	let { upper, main, lower }: { upper: string; main: string; lower: string } = $props();
 </script>
 
-<section class="w-80 rounded-lg bg-bg-accent p-4">
-	<div class="italic text-text-muted">
+<section class="bg-bg-accent w-96 rounded-lg p-4">
+	<div class="text-text-muted italic">
 		{upper}
 	</div>
 	<H2 text={main} />
-	<div class="italic text-text-muted">
+	<div class="text-text-muted italic">
 		{lower}
 	</div>
 </section>
