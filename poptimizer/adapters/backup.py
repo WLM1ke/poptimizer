01ab0f2b0940879fcb8f90396d751f7f965a2df3
WLMike <wlmike@gmail.com>
2025-10-12 16:28:11 +0300

    fix: old virsion detection

--- poptimizer/adapters/backup.py
+++ poptimizer/adapters/backup.py
@@ -58,7 +58,7 @@ class BackupHandler:
             await self._mongo_repo.drop(div.Dividends)
 
         # 2025-10-12
-        if current_ver < "3.0.0b12":
+        if len(current_ver) < len("3.0.0b12") or current_ver < "3.0.0b12":
             self._lgr.warning("Dropping features data due to new format")
             await self._mongo_repo.drop(features.Features)
 
