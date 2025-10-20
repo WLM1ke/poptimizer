f19ac38ca8e0d4b921908adf6fe6aa3e8ca168c0
WLMike <wlmike@gmail.com>
2025-10-20 19:45:32 +0300

    feat: check on old index data

--- poptimizer/use_cases/moex/index.py
+++ poptimizer/use_cases/moex/index.py
@@ -1,5 +1,7 @@
 import asyncio
+import logging
 from datetime import date, timedelta
+from typing import Final
 
 import aiohttp
 import aiomoex
@@ -9,10 +11,13 @@ from poptimizer.domain import domain
 from poptimizer.domain.moex import index
 from poptimizer.use_cases import handler
 
+_MAX_INDEX_LAG: Final = timedelta(days=7)
+
 
 class IndexesHandler:
     def __init__(self, http_client: aiohttp.ClientSession) -> None:
         self._http_client = http_client
+        self._lgr = logging.getLogger()
 
     async def __call__(self, ctx: handler.Ctx, msg: handler.QuotesUpdated) -> handler.IndexesUpdated:
         async with asyncio.TaskGroup() as tg:
@@ -32,6 +37,10 @@ class IndexesHandler:
             rows = first_rows + rows
 
         table.update(update_day, rows)
+
+        last_date = table.last_row_date()
+        if last_date is not None and (update_day - last_date) > _MAX_INDEX_LAG:
+            self._lgr.warning("Index %s last value is too old: %s", ticker, last_date)
 
     async def _download(
         self,
