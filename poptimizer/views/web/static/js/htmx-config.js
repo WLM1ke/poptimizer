8f32a8d98d906daab429ab9cf79685bd4a417890
WLMike <wlmike@gmail.com>
2025-10-29 19:19:32 +0300

    feat: switch theme

--- poptimizer/views/web/static/js/htmx-config.js
+++ poptimizer/views/web/static/js/htmx-config.js
@@ -1,1 +1,21 @@
 htmx.config.defaultSwapStyle = "outerHTML";
+
+function setTheme(event) {
+  if (!event?.detail?.successful) {
+    return;
+  }
+
+  const requestPath = event.detail.pathInfo?.requestPath;
+  if (!requestPath) {
+    return;
+  }
+
+  const themeName = requestPath.split("/").pop();
+
+  const allowedThemes = ["light", "dark", "system"];
+  if (!allowedThemes.includes(themeName)) {
+    return;
+  }
+
+  document.documentElement.setAttribute("data-theme", themeName);
+}
