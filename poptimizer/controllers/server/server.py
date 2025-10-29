186eb71266af121b1a6f22ad11ccc12205c46f21
WLMike <wlmike@gmail.com>
2025-10-29 21:21:46 +0300

    refactor: remove old handlers

--- poptimizer/controllers/server/server.py
+++ poptimizer/controllers/server/server.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 from aiohttp import web
 
 from poptimizer.controllers.bus import msg
-from poptimizer.controllers.server import api, http_server, middleware
+from poptimizer.controllers.server import http_server, middleware
 from poptimizer.views.web import portfolio
 
 if TYPE_CHECKING:
@@ -15,10 +15,9 @@ def build(
     url: HttpUrl,
 ) -> http_server.Server:
     sub_app = web.Application()
-    api.Handlers(sub_app, bus)
 
     app = web.Application(middlewares=[middleware.RequestErrorMiddleware()])
-    portfolio.Handlers(app)
+    portfolio.Handlers(app, bus)
     app.add_subapp("/api/", sub_app)
 
     return http_server.Server(
