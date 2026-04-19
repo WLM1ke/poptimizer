6fa8bd42f1cb21a06081b57590c64a964af1764b
WLMike <wlmike@gmail.com>
2026-04-19 20:45:58 +0300

    feat: new income report logic

--- poptimizer/reports/income.py
+++ poptimizer/reports/income.py
@@ -20,32 +20,59 @@ async def report(
     investor: funds.Investor,
     months: int,
 ) -> None:
-    fund = await repo.get(funds.Fund)
-    cpi_table = await repo.get(cpi.CPI)
-
-    if len(fund.rows) <= months or len(cpi_table.df) <= months:
-        raise errors.DomainError("too many months")
-
-    value = fund.rows[-months - 1].get_value(investor)
-    shortfall = 0
-
-    for lag in reversed(range(1, months + 1)):
-        value *= cpi_table.df[-lag].cpi
-        value += fund.rows[-lag].get_inflow(investor)
-
-        shortfall *= cpi_table.df[-lag].cpi
-        shortfall = max(shortfall, value - fund.rows[-lag].get_value(investor))
+    deflator = await _deflator(repo, months)
+    income = await _real_income(repo, investor, deflator)
+    monthly_income, shortfall = _monthly_income_and_shortfall(income)
 
     params = sorted([*_ROWS_PARAMS, (months, f"{months}M")], key=lambda x: x[0], reverse=True)
-    income = fund.rows[-1].get_value(investor) - value
-    income /= months
 
     label_align = len(str(months)) + 1
     max_factor = params[0][0]
-    income_align = len(f"{round(max_factor * income, -3):_.0f}")
+    income_align = len(f"{round(max_factor * monthly_income, -3):_.0f}")
 
     lgr.info("CPI-adjusted report for %s", investor)
     for factor, label in params:
-        lgr.info(f"{label:<{label_align}}: {round(income * factor, -3):>{income_align}_.0f}")
+        lgr.info(f"{label:<{label_align}}: {round(monthly_income * factor, -3):>{income_align}_.0f}")
 
     lgr.info(f"Real shortfall - {round(shortfall, -5):_.0f}")
+
+
+async def _deflator(repo: uow.UOW, months: int) -> list[float]:
+    cpi_table = await repo.get(cpi.CPI)
+
+    if len(cpi_table.df) < months:
+        raise errors.DomainError("too many months")
+
+    deflator = [1.0]
+
+    for i in range(1, months + 1):
+        deflator.append(deflator[-1] * cpi_table.df[-i].cpi)
+
+    return list(reversed(deflator))
+
+
+async def _real_income(repo: uow.UOW, investor: funds.Investor, deflator: list[float]) -> list[float]:
+    fund = await repo.get(funds.Fund)
+
+    months = len(deflator) - 1
+
+    if len(fund.rows) <= months:
+        raise errors.DomainError("too many months")
+
+    return [
+        (fund.rows[-i].get_value(investor) - fund.rows[-i].get_inflow(investor)) * deflator[-i]
+        - fund.rows[-i - 1].get_value(investor) * deflator[-i - 1]
+        for i in range(months, 0, -1)
+    ]
+
+
+def _monthly_income_and_shortfall(income: list[float]) -> tuple[float, float]:
+    monthly_income = sum(income) / len(income)
+
+    shortfall_max = 0
+    reserve = 0
+    for inc in income:
+        reserve += inc - monthly_income
+        shortfall_max = max(shortfall_max, -reserve)
+
+    return monthly_income, shortfall_max
