2cea96224008eeee49cd2062af61bd9c80885c05
WLMike <wlmike@gmail.com>
2026-06-11 20:20:40 +0300

    feat: more info about dividends reset

--- poptimizer/cli/div.py
+++ poptimizer/cli/div.py
@@ -10,6 +10,9 @@ class Reset(config.Cfg):
 
     async def cli_cmd(self) -> None:
         async with contextlib.AsyncExitStack() as stack:
+            lgr = logger.init()
             mongo_db = await stack.enter_async_context(mongo.db(self.mongo.uri, self.mongo.db))
 
             await safe.run(logger.init(), mongo.Repo(mongo_db).drop(raw.DivRaw))
+
+            lgr.info("Raw dividends are deleted and will be restored from backup on next run")
