ab18e50153671328eebc1ffd9fed9d3b63bbeb21
WLMike <wlmike@gmail.com>
2026-06-12 17:21:51 +0300

    feat: new turnover logic

--- poptimizer/portfolio/actions.py
+++ poptimizer/portfolio/actions.py
@@ -96,9 +96,9 @@ async def _prepare_sec_cache(
 
         turnover = 0
         if len(df) >= minimal_candles:
-            turnover_data = [row.turnover for row in df[-forecast_days:] if row.day in turnover_days]
-            if turnover_data:
-                turnover = statistics.median(turnover_data)
+            turnover_data = [row.turnover for row in df[-minimal_candles:] if row.day in turnover_days]
+            turnover_data_short = [row.turnover for row in df[-forecast_days:] if row.day in turnover_days]
+            turnover = min(statistics.median(turnover_data), statistics.median(turnover_data_short))
 
         cache[sec.ticker] = portfolio.Position(
             ticker=sec.ticker,
